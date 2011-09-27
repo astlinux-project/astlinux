6de6787392dcd298ef44c85bc7b0d24f8f8a2f36
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-09-27 22:16:55 +0000

    support our files.astlinux.org toolchain/getter_script.sh
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5058 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ toolchain/getter_script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# getter_better script from gumstix
+# what a great idea...
+SITE=files.astlinux.org
+
+WGET_ARGS="--passive-ftp --timeout=30 -c -t 2"
+
+wget ${WGET_ARGS} $@ || (
+	echo Retrying from astlinux alternate site...
+	index=$#-1
+	# Copy all params into an array
+	for (( i=0; $?==0; i++ ));do a[$i]=$1; shift; done
+	# Chop all but filename from last param and prepend out URL
+	a[$index]=${a[index]/*\//http:\/\/$SITE/}
+	# Now wget that from our server
+	wget ${WGET_ARGS} ${a[@]}
+)
+
+
+for i in $@
+do
+URL="$i"
+done
+
+FILE=`basename $URL`
+
+wget ${WGET_ARGS} -nv -P dl "$SITE"/"$FILE".sha1
+
+if `sha1sum -c --status dl/"$FILE".sha1`
+then
+echo "$FILE verified"
+exit 0
+else
+if `grep -q $FILE toolchain/file_exclude`
+then
+echo "Skipping file verification abort"
+exit 0
+else
+echo "$FILE failed verification - exiting"
+exit 1
+fi
+fi
