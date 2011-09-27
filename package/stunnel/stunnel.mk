b1e6c7c9f5546e0036f07aa1a39161af1501c551
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-09-27 01:40:57 +0000

    fix openssl and stunnel
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5051 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -7,17 +7,16 @@
 STUNNEL_VERSION = 4.36
 STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel/obsolete/4.x/
 STUNNEL_DEPENDENCIES = openssl
+STUNNEL_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) --version
 
 STUNNEL_CONF_OPT += \
 	--with-ssl=$(STAGING_DIR)/usr \
 	--with-threads=fork
 
 define STUNNEL_INSTALL_CONF_SCRIPT
-	$(INSTALL) -m 0755 -D package/stunnel/S50stunnel $(TARGET_DIR)/etc/init.d/S50stunnel
-	[ -f $(TARGET_DIR)/etc/stunnel/stunnel.conf ] || \
-		$(INSTALL) -m 0644 -D $(@D)/tools/stunnel.conf \
-			$(TARGET_DIR)/etc/stunnel/stunnel.conf
-	rm -f $(TARGET_DIR)/etc/stunnel/stunnel.conf-sample
+	ln -sf /tmp/etc/stunnel.conf $(TARGET_DIR)/etc/stunnel.conf
+	$(INSTALL) -m 0755 -D package/stunnel/stunnel.init $(TARGET_DIR)/etc/init.d/stunnel
+	$(INSTALL) -m 0755 -D $(@D)/src/stunnel $(TARGET_DIR)/usr/sbin/stunnel
 endef
 
 STUNNEL_POST_INSTALL_TARGET_HOOKS += STUNNEL_INSTALL_CONF_SCRIPT
