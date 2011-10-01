c80f602927b0fa76f394367d01f3b8359ddfcc70
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-01 00:23:51 +0000

    generate AstLinux releases, usage: ./scripts/build [ board ]
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5074 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ boot/runnix/runnix.mk
@@ -0,0 +1,52 @@
+#############################################################
+#
+# runnix
+#
+#############################################################
+RUNNIX_VER:=0.4-170
+RUNNIX_SOURCE:=runnix-$(RUNNIX_VER).tar.gz
+RUNNIX_SITE:=http://mirror.astlinux.org/runnix4
+RUNNIX_DIR:=$(BUILD_DIR)/runnix-$(RUNNIX_VER)
+RUNNIX_CAT:=zcat
+RUNFS_DIR:=$(BUILD_DIR)/runfs
+
+RUNNIX_NDEV:=$(patsubst "%",%,$(BR2_TARGET_RUNNIX_NDEV))
+
+$(DL_DIR)/$(RUNNIX_SOURCE):
+	$(WGET) -P $(DL_DIR) $(RUNNIX_SITE)/$(RUNNIX_SOURCE)
+
+$(RUNNIX_DIR)/.unpacked: $(DL_DIR)/$(RUNNIX_SOURCE)
+	$(RUNNIX_CAT) $(DL_DIR)/$(RUNNIX_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $(RUNNIX_DIR)/.unpacked
+
+$(RUNFS_DIR)/runnix: $(RUNNIX_DIR)/.unpacked
+	mkdir -p $(RUNFS_DIR)
+	cp -af $(RUNNIX_DIR)/rootfs_vfat/* $(RUNFS_DIR)
+	rm -f $(RUNFS_DIR)/*.sample
+ifneq ($(RUNNIX_NDEV),)
+ ifneq ($(RUNNIX_NDEV),eth0)
+	$(SED) 's/^NDEV="eth[0-9]"$$/NDEV="$(RUNNIX_NDEV)"/' \
+		$(RUNFS_DIR)/os/default.conf
+ endif
+endif
+	touch -c $(RUNFS_DIR)/runnix
+
+runnix: $(RUNFS_DIR)/runnix
+
+runfs-dirclean:
+	rm -rf $(RUNFS_DIR)
+
+runnix-dirclean:
+	rm -rf $(RUNNIX_DIR)
+
+runfs: runnix
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_TARGET_RUNNIX)),y)
+TARGETS+=runnix
+endif
+
