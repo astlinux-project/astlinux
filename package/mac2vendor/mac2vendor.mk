78151ff1db78f386635f19bb2b630e6e061dfdd4
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-05 22:59:21 +0000

    mac2vendor added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5103 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/mac2vendor/mac2vendor.mk
@@ -0,0 +1,46 @@
+#############################################################
+#
+# mac2vendor
+#
+##############################################################
+MAC2VENDOR_VERSION := 2011-10-05
+MAC2VENDOR_DATA := oui-$(MAC2VENDOR_VERSION).txt
+MAC2VENDOR_DATA_ORIG := oui.txt
+MAC2VENDOR_SITE := http://standards.ieee.org/develop/regauth/oui
+MAC2VENDOR_DIR := $(BUILD_DIR)/mac2vendor
+MAC2VENDOR_BINARY := usr/sbin/mac2vendor
+
+$(DL_DIR)/$(MAC2VENDOR_DATA):
+	$(WGET) -P $(DL_DIR) $(MAC2VENDOR_SITE)/$(MAC2VENDOR_DATA_ORIG)
+	mv $(DL_DIR)/$(MAC2VENDOR_DATA_ORIG) $(DL_DIR)/$(MAC2VENDOR_DATA)
+
+$(MAC2VENDOR_DIR)/.data: $(DL_DIR)/$(MAC2VENDOR_DATA)
+	mkdir -p $(MAC2VENDOR_DIR)/oui-db
+	for i in 0 1 2 3 4 5 6 7 8 9 A B C D E F; do \
+	  grep "^[0-9A-F]\{5\}$$i " $(DL_DIR)/$(MAC2VENDOR_DATA) | \
+	  sed 's/ [^(]*.base 16.[^0-9a-zA-Z]*/~/' > $(MAC2VENDOR_DIR)/oui-db/xxxxx$$i ; \
+	  chmod a-w $(MAC2VENDOR_DIR)/oui-db/xxxxx$$i ; \
+	done
+	touch $@
+
+$(TARGET_DIR)/$(MAC2VENDOR_BINARY): $(MAC2VENDOR_DIR)/.data
+	$(INSTALL) -D -m 0755 package/mac2vendor/mac2vendor $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
+	cp -a $(MAC2VENDOR_DIR)/oui-db $(TARGET_DIR)/usr/share/oui-db
+
+mac2vendor: $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
+
+mac2vendor-clean:
+	rm -f $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
+	rm -rf $(TARGET_DIR)/usr/share/oui-db
+
+mac2vendor-dirclean:
+	rm -rf $(MAC2VENDOR_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_MAC2VENDOR)),y)
+TARGETS+=mac2vendor
+endif
