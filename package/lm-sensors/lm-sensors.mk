9c01d70c96c7c70c285e5555040ed189ba36e1e4
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-06 03:59:59 +0000

    lm-sensors added, switched to v3
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5105 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/lm-sensors/lm-sensors.mk
+++ package/lm-sensors/lm-sensors.mk
@@ -3,7 +3,7 @@
 # lm-sensors
 #
 #############################################################
-LM_SENSORS_VERSION = 3.2.0
+LM_SENSORS_VERSION = 3.3.1
 LM_SENSORS_SOURCE = lm_sensors-$(LM_SENSORS_VERSION).tar.bz2
 LM_SENSORS_SITE = http://dl.lm-sensors.org/lm-sensors/releases
 LM_SENSORS_INSTALL_STAGING = YES
@@ -31,7 +31,11 @@ endef
 
 define LM_SENSORS_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) install
+	$(INSTALL) -D -m 0755 package/lm-sensors/lm-sensors.init $(TARGET_DIR)/etc/init.d/lmsensors
+	mv $(TARGET_DIR)/etc/sensors3.conf $(TARGET_DIR)/stat/etc/sensors3.conf
+	ln -sf /tmp/etc/sensors3.conf $(TARGET_DIR)/etc/sensors3.conf
 	rm -f $(addprefix $(TARGET_DIR)/usr/,$(LM_SENSORS_BINS_))
+	rm -rf $(TARGET_DIR)/etc/sensors.d
 endef
 
 define LM_SENSORS_UNINSTALL_TARGET_CMDS
