98ebd1c7de599dc7075670043c39ffb5976cbf60
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-08 01:59:24 +0000

    minor ntp.mk fix
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5115 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ntp/ntp.mk
+++ package/ntp/ntp.mk
@@ -50,8 +50,7 @@ endef
 define NTP_UNINSTALL_TARGET_CMDS
 	rm $(TARGET_DIR)/usr/sbin/ntpd
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,$(NTP_INSTALL_FILES_y))
-	rm $(TARGET_DIR)/etc/init.d/S49ntp
-	rm $(TARGET_DIR)/etc/default/ntpd
+	rm $(TARGET_DIR)/etc/init.d/ntpd
 endef
 
 NTP_POST_PATCH_HOOKS += NTP_PATCH_FIXUPS
