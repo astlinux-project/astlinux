20cfae1f24824f44bafa31066dbe0cc15ac3d39d
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-09 23:22:01 +0000

    kexec added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5130 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -7,6 +7,8 @@ KEXEC_VERSION = 2.0.2
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.bz2
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 
+KEXEC_UNINSTALL_STAGING_OPT = --version
+
 ifeq ($(BR2_PACKAGE_KEXEC_ZLIB),y)
 KEXEC_CONF_OPT += --with-zlib
 KEXEC_DEPENDENCIES = zlib
@@ -14,10 +16,19 @@ else
 KEXEC_CONF_OPT += --without-zlib
 endif
 
-define KEXEC_REMOVE_LIB_TOOLS
-	rm -rf $(TARGET_DIR)/usr/lib/kexec-tools
+# Only install kexec
+define KEXEC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/build/sbin/kexec $(TARGET_DIR)/sbin/kexec
 endef
 
-KEXEC_POST_INSTALL_TARGET_HOOKS += KEXEC_REMOVE_LIB_TOOLS
+#define KEXEC_REMOVE_LIB_TOOLS
+#	rm -rf $(TARGET_DIR)/usr/lib/kexec-tools
+#endef
+#
+#KEXEC_POST_INSTALL_TARGET_HOOKS += KEXEC_REMOVE_LIB_TOOLS
+#
+define KEXEC_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/sbin/kexec
+endef
 
 $(eval $(call AUTOTARGETS,package,kexec))
