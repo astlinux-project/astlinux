511a64e05bf4f014e0368f039199d479a4a44fda
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-10 21:21:14 +0000

    nano, disable color and nanorc
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5141 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/nano/nano.mk
+++ package/nano/nano.mk
@@ -10,14 +10,12 @@ NANO_MAKE_ENV = CURSES_LIB="-lncurses"
 NANO_DEPENDENCIES = ncurses
 NANO_CONF_OPT = \
 	--without-slang \
-	--enable-tiny \
-	--enable-color \
-	--enable-nanorc
+	--enable-tiny
 
 define NANO_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/src/nano $(TARGET_DIR)/usr/bin/nano
-	$(INSTALL) -m 444 -D package/nano/etc.nanorc $(TARGET_DIR)/etc/nanorc
-	$(INSTALL) -m 444 -D package/nano/asterisk.nanorc $(TARGET_DIR)/usr/share/nano/asterisk.nanorc
+	#$(INSTALL) -m 444 -D package/nano/etc.nanorc $(TARGET_DIR)/etc/nanorc
+	#$(INSTALL) -m 444 -D package/nano/asterisk.nanorc $(TARGET_DIR)/usr/share/nano/asterisk.nanorc
 endef
 
 define NANO_UNINSTALL_TARGET_CMDS
