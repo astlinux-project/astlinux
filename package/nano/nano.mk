16263b880a27fb4462d9a7c4164dee63bd91688f
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-10 16:52:43 +0000

    nano added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5140 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/nano/nano.mk
+++ package/nano/nano.mk
@@ -7,11 +7,17 @@
 NANO_VERSION = 2.2.6
 NANO_SITE = http://www.nano-editor.org/dist/v2.2
 NANO_MAKE_ENV = CURSES_LIB="-lncurses"
-NANO_CONF_OPT = --without-slang --enable-tiny
 NANO_DEPENDENCIES = ncurses
+NANO_CONF_OPT = \
+	--without-slang \
+	--enable-tiny \
+	--enable-color \
+	--enable-nanorc
 
 define NANO_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/src/nano $(TARGET_DIR)/usr/bin/nano
+	$(INSTALL) -m 444 -D package/nano/etc.nanorc $(TARGET_DIR)/etc/nanorc
+	$(INSTALL) -m 444 -D package/nano/asterisk.nanorc $(TARGET_DIR)/usr/share/nano/asterisk.nanorc
 endef
 
 define NANO_UNINSTALL_TARGET_CMDS
