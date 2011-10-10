8b21ee5e926db5a7207fae12c8c9cd00dcd4ef75
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-10 22:14:34 +0000

    p910nd added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5142 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/p910nd/p910nd.mk
@@ -0,0 +1,33 @@
+#############################################################
+#
+# p910nd
+#
+#############################################################
+
+P910ND_VERSION = 0.94
+P910ND_SOURCE:=p910nd-$(P910ND_VERSION).tar.bz2
+P910ND_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/p910nd
+
+P910ND_MAKE_OPT = CC='$(TARGET_CC)' \
+		  LD='$(TARGET_LD)' \
+		  CFLAGS='$(TARGET_CFLAGS) $(TARGET_LDFLAGS) -DLOCKFILE_DIR=\"/var/lock\"' \
+		  USE_WRAP= \
+		  -C $(@D)
+
+P910ND_UNINSTALL_STAGING_OPT = --version
+
+define P910ND_CONFIGURE_CMDS
+        @echo "No configure"
+endef
+
+define P910ND_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/p910nd $(TARGET_DIR)/usr/sbin/p910nd
+	$(INSTALL) -m 0755 -D package/p910nd/p910nd.init $(TARGET_DIR)/etc/init.d/p910nd
+endef
+
+define P910ND_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/p910nd
+	rm -f $(TARGET_DIR)/etc/init.d/p910nd
+endef
+
+$(eval $(call AUTOTARGETS,package,p910nd))
