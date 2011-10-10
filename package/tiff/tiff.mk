370f7441b2ae14cc2272100ea13dedc517291e73
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-10 01:30:55 +0000

    libtiff (with tools) added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5133 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -9,13 +9,21 @@ TIFF_SOURCE:=tiff-$(TIFF_VERSION).tar.gz
 TIFF_INSTALL_STAGING = YES
 TIFF_INSTALL_TARGET = YES
 TIFF_CONF_OPT = \
+	--disable-debug \
 	--disable-cxx \
 	--without-x \
 
 TIFF_DEPENDENCIES = host-pkg-config zlib jpeg
 
 define TIFF_INSTALL_TARGET_CMDS
-	-cp -a $(@D)/libtiff/.libs/libtiff.so* $(TARGET_DIR)/usr/lib/
+	cp -a $(@D)/libtiff/.libs/libtiff.so* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/i586-linux-tiff2pdf $(TARGET_DIR)/usr/bin/tiff2pdf
+	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/i586-linux-tiffinfo $(TARGET_DIR)/usr/bin/tiffinfo
+endef
+
+define TIFF_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/tiff2pdf
+	rm -f $(TARGET_DIR)/usr/bin/tiffinfo
 endef
 
 $(eval $(call AUTOTARGETS,package,tiff))
