2434660bcf024d6eaaeb563a81554702487e9f3d
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-11 21:30:13 +0000

    uw-imap added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5155 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/uw-imap/uw-imap.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# uw-imap
+#
+#############################################################
+UW_IMAP_VERSION = 2007e
+UW_IMAP_SITE = ftp://ftp.cac.washington.edu/imap
+UW_IMAP_SOURCE = imap-$(UW_IMAP_VERSION).tar.gz
+UW_IMAP_INSTALL_STAGING = NO
+UW_IMAP_INSTALL_TARGET = YES
+
+UW_IMAP_DEPENDENCIES = openssl
+
+define UW_IMAP_CONFIGURE_CMDS
+        @echo "No configure"
+endef
+
+UW_IMAP_MAKE_OPT = slx CC='$(TARGET_CC)' \
+		   EXTRACFLAGS="-I$(STAGING_DIR)/usr/include/openssl" \
+		   SSLLIB=$(STAGING_DIR)/lib \
+		   SSLTYPE=nopwd \
+		   SHLIBBASE=c-client \
+		   SHLIBNAME=libc-client.so.1 \
+		   SSLDIR=$(TARGET_DIR)/usr/lib/ssl \
+		   LD='$(TARGET_LD)' \
+		   -C $(@D) c-client
+
+define UW_IMAP_INSTALL_TARGET_CMDS
+	@echo "Nothing to install"
+endef
+
+$(eval $(call AUTOTARGETS,package,uw-imap))
