676e67eb29f98a96af5675b3118d293650db0d4e
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-11 20:30:15 +0000

    bc added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5154 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/bc/bc.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# bc
+#
+#############################################################
+
+BC_VERSION = 1.06
+BC_SOURCE:=bc-$(BC_VERSION).tar.gz
+BC_SITE = http://ftp.gnu.org/gnu/bc
+BC_DEPENDENCIES = host-flex
+
+define BC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/bc/bc $(TARGET_DIR)/usr/bin/bc
+endef
+
+define BC_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/bc
+endef
+
+$(eval $(call AUTOTARGETS,package,bc))
