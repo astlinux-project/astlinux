41260448063c3d643de3c247848567631e5724b0
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-11 00:43:46 +0000

    rp-pppoe added, using rp-pppoe.so from pppd
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5145 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/rp-pppoe/rp-pppoe.mk
+++ package/rp-pppoe/rp-pppoe.mk
@@ -6,31 +6,42 @@
 
 RP_PPPOE_VERSION = 3.10
 RP_PPPOE_SITE = http://www.roaringpenguin.com/files/download
-RP_PPPOE_DEPENDENCIES = pppd
 RP_PPPOE_SUBDIR = src
+RP_PPPOE_DEPENDENCIES = pppd
 RP_PPPOE_TARGET_FILES = pppoe pppoe-server pppoe-relay pppoe-sniff
+RP_PPPOE_TARGET_SCRIPTS = pppoe-connect pppoe-start pppoe-stop pppoe-status
 RP_PPPOE_MAKE_OPT = PLUGIN_DIR=/usr/lib/pppd/$(PPPD_VERSION)
-RP_PPPOE_CONF_OPT = --disable-debugging
 RP_PPPOE_CONF_ENV = \
-	rpppoe_cv_pack_bitfields=normal \
+	ac_cv_func_setvbuf_reversed=no \
+	ac_cv_linux_kernel_pppoe=yes \
+	rpppoe_cv_pack_bitfields=rev \
 	PPPD_H=$(PPPD_DIR)/pppd/pppd.h
+
+RP_PPPOE_CONF_OPT = \
+	--disable-debugging
 
 define RP_PPPOE_INSTALL_TARGET_CMDS
 	for ff in $(RP_PPPOE_TARGET_FILES); do \
 		$(INSTALL) -m 0755 $(@D)/src/$$ff $(TARGET_DIR)/usr/sbin/$$ff; \
 	done
-	for ff in $(RP_PPPOE_TARGET_FILES); do \
-		$(INSTALL) -m 644 -D $(RP_PPPOE_DIR)/man/$$ff.8 $(TARGET_DIR)/usr/share/man/man8/$$ff.8; \
+	for ff in $(RP_PPPOE_TARGET_SCRIPTS); do \
+		$(INSTALL) -m 0755 $(@D)/scripts/$$ff $(TARGET_DIR)/usr/sbin/$$ff; \
 	done
+	$(INSTALL) -m 0755 package/rp-pppoe/pppoe-restart $(TARGET_DIR)/usr/sbin/pppoe-restart
+	# work-around so that we don't clobber the existing link
+	$(SED) 's@ln -s /etc/ppp/resolv.conf /etc/resolv.conf@: # ln -sf /etc/ppp/resolv.conf /tmp/etc/resolv.conf@' \
+		-e 's@rm -f /etc/resolv.conf@# &@' \
+		$(TARGET_DIR)/usr/sbin/pppoe-connect
 endef
 
 define RP_PPPOE_UNINSTALL_TARGET_CMDS
 	for ff in $(RP_PPPOE_TARGET_FILES); do \
 		rm -f $(TARGET_DIR)/usr/sbin/$$ff; \
 	done
-	for ff in $(RP_PPPOE_TARGET_FILES); do \
-		rm -f $(TARGET_DIR)/usr/share/man/man8/$$ff.8; \
+	for ff in $(RP_PPPOE_TARGET_SCRIPTS); do \
+		rm -f $(TARGET_DIR)/usr/sbin/$$ff; \
 	done
+	rm -f $(TARGET_DIR)/usr/sbin/pppoe-restart
 endef
 
 $(eval $(call AUTOTARGETS,package,rp-pppoe))
