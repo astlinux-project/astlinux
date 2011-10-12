be3c1beed64ebd0f43e9fd12bc67ade0919bdc8c
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-12 20:00:47 +0000

    ex-vi added, latest Busybox vi is less featured than before, a nice alternative for 180KB
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5162 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/ex-vi/ex-vi.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# ex-vi
+#
+############################################################
+
+EX_VI_VERSION = 050325
+EX_VI_SOURCE = ex-$(EX_VI_VERSION).tar.bz2
+EX_VI_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/ex-vi
+
+EX_VI_DEPENDENCIES = ncurses
+
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+EX_VI_DEPENDENCIES += busybox
+endif
+
+define EX_VI_BUILD_CMDS
+        $(MAKE) CC="$(TARGET_CC)" \
+		TERMLIB="ncurses" \
+		CHARSET="-DISO8859_1" \
+		FEATURES="-DCHDIR -DFASTTAG -DUCVISUAL -DBIT8" \
+		-C $(@D)
+endef
+
+define EX_VI_CONFIGURE_CMDS
+        @echo "No configure"
+endef
+
+define EX_VI_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/ex $(TARGET_DIR)/bin/vi
+endef
+
+define EX_VI_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/bin/vi
+endef
+
+$(eval $(call GENTARGETS,package,ex-vi))
