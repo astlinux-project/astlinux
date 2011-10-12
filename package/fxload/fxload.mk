ff9d942d493a898d22fdcf8995a8663634f18981
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-12 02:42:32 +0000

    fxload added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5159 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/fxload/fxload.mk
@@ -0,0 +1,45 @@
+#############################################################
+#
+# fxload
+#
+#############################################################
+FXLOAD_VERSION:=2008_10_13
+FXLOAD_SOURCE:=fxload-$(FXLOAD_VERSION).tar.gz
+FXLOAD_SITE:=http://$(SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/linux-hotplug/fxload/$(FXLOAD_VERSION)
+FXLOAD_DIR:=$(BUILD_DIR)/fxload-$(FXLOAD_VERSION)
+FXLOAD_CAT:=zcat
+FXLOAD_BINARY:=fxload
+FXLOAD_TARGET_BINARY:=usr/sbin/fxload
+
+$(DL_DIR)/$(FXLOAD_SOURCE):
+	$(WGET) -P $(DL_DIR) $(FXLOAD_SITE)/$(FXLOAD_SOURCE)
+
+$(FXLOAD_DIR)/.unpacked: $(DL_DIR)/$(FXLOAD_SOURCE)
+	$(FXLOAD_CAT) $(DL_DIR)/$(FXLOAD_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $@
+
+$(FXLOAD_DIR)/$(FXLOAD_BINARY): $(FXLOAD_DIR)/.unpacked
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(FXLOAD_DIR)
+
+$(TARGET_DIR)/$(FXLOAD_TARGET_BINARY): $(FXLOAD_DIR)/$(FXLOAD_BINARY)
+	$(INSTALL) -D $(FXLOAD_DIR)/$(FXLOAD_BINARY) $(TARGET_DIR)/$(FXLOAD_TARGET_BINARY)
+
+fxload: $(TARGET_DIR)/$(FXLOAD_TARGET_BINARY)
+
+fxload-source: $(FXLOAD_DIR)/.unpacked
+
+fxload-clean:
+	rm -f $(TARGET_DIR)/$(FXLOAD_TARGET_BINARY)
+	-$(MAKE) -C $(FXLOAD_DIR) clean
+
+fxload-dirclean:
+	rm -rf $(FXLOAD_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_FXLOAD)),y)
+TARGETS+=fxload
+endif
