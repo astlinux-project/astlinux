891236ec96a5248bf73e1570de8c7a37ec0c122a
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-13 14:22:18 +0000

    libpri added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5165 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/libpri/libpri.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# libpri
+#
+############################################################
+
+LIBPRI_VERSION = 1.4.12
+LIBPRI_SOURCE = libpri-$(LIBPRI_VERSION).tar.gz
+LIBPRI_SITE = http://downloads.digium.com/pub/libpri/releases
+LIBPRI_INSTALL_STAGING = YES
+LIBPRI_INSTALL_TARGET = YES
+
+define LIBPRI_CONFIGURE_CMDS
+        @echo "No configure"
+endef
+
+LIBPRI_MAKE_OPT = \
+	OSARCH=Linux \
+	INSTALL_BASE=/usr \
+	$(TARGET_CONFIGURE_OPTS)
+
+LIBPRI_INSTALL_STAGING_OPT = \
+	OSARCH=Linux \
+	INSTALL_BASE=/usr \
+	INSTALL_PREFIX=$(STAGING_DIR) \
+	$(TARGET_CONFIGURE_OPTS) \
+	install
+
+LIBPRI_INSTALL_TARGET_OPT = \
+	OSARCH=Linux \
+	INSTALL_BASE=/usr \
+	INSTALL_PREFIX=$(TARGET_DIR) \
+	$(TARGET_CONFIGURE_OPTS) \
+	install
+
+$(eval $(call AUTOTARGETS,package,libpri))
+
