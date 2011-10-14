c7ceb57eb365ea10aea34941b2d21db7e68a0049
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-14 05:49:56 +0000

    rhino added
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/br2011-08@5168 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/rhino/rhino.mk
@@ -0,0 +1,80 @@
+#############################################################
+#
+# rhino - Package for Rhino PCI drivers.
+#
+#############################################################
+
+RHINO_VERSION := 0.99.4rc1
+#RHINO_VERSION := 0.99.5b1
+RHINO_SOURCE := rhino-linux-$(RHINO_VERSION).tbz2
+#RHINO_SOURCE := rhino-linux-current.tbz2
+RHINO_SITE := http://files.astlinux.org
+#RHINO_SITE := http://downloads.rhinoequipment.com/Rhino%20Downloads/Drivers/DAHDI
+RHINO_DIR := $(BUILD_DIR)/rhino-linux-$(RHINO_VERSION)
+RHINO_CAT := bzcat
+RHINO_TARGET_BINARY := usr/sbin/rhino_ver
+RHINO_MODULES := r1t1 rxt1 rcbfx
+RHINO_LEGACY_MODULES := r4fxo
+
+RHINO_PREREQS := dahdi-linux
+RHINO_CONFIGURE := DAHDI_DIR=$(DAHDI_LINUX_DIR)
+
+$(DL_DIR)/$(RHINO_SOURCE):
+	$(WGET) -P $(DL_DIR) $(RHINO_SITE)/$(RHINO_SOURCE)
+
+$(RHINO_DIR)/.source: $(DL_DIR)/$(RHINO_SOURCE)
+	$(RHINO_CAT) $(DL_DIR)/$(RHINO_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $@
+
+$(RHINO_DIR)/.patched: $(RHINO_DIR)/.source | $(RHINO_PREREQS)
+	toolchain/patch-kernel.sh $(RHINO_DIR) package/rhino/ rhino-\*.patch
+	cp -a $(DAHDI_LINUX_DIR)/drivers/dahdi/Module.symvers $(RHINO_DIR)/drivers/rhino/Module.symvers
+	touch $@
+
+$(RHINO_DIR)/.built: $(RHINO_DIR)/.patched
+	$(MAKE) -C $(RHINO_DIR) \
+		HOSTCC=gcc CC=$(TARGET_CC) ARCH=$(KERNEL_ARCH) \
+		KVER=$(LINUX_VERSION_PROBED) PWD=$(RHINO_DIR)  \
+		KSRC=$(LINUX_DIR) LEGACY_MODULES="$(RHINO_LEGACY_MODULES)" \
+		MODULES="$(RHINO_MODULES)" KMOD=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED) \
+		KINCLUDES=$(STAGING_DIR)/include \
+		KINSTDIR=/lib/modules/$(LINUX_VERSION_PROBED)/kernel \
+		INSTALL_PREFIX=$(TARGET_DIR) \
+		$(RHINO_CONFIGURE) \
+		all
+	touch $@
+
+$(TARGET_DIR)/$(RHINO_TARGET_BINARY): $(RHINO_DIR)/.built
+	$(MAKE) -C $(RHINO_DIR) \
+		HOSTCC=gcc CC=$(TARGET_CC) ARCH=$(KERNEL_ARCH) \
+		KVER=$(LINUX_VERSION_PROBED) PWD=$(RHINO_DIR) \
+		KSRC=$(LINUX_DIR) LEGACY_MODULES="$(RHINO_LEGACY_MODULES)" \
+		MODULES="$(RHINO_MODULES)" KMOD=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED) \
+		KINCLUDES=$(STAGING_DIR)/include \
+		KINSTDIR=/lib/modules/$(LINUX_VERSION_PROBED)/kernel \
+		INSTALL_PREFIX=$(TARGET_DIR) \
+		$(RHINO_CONFIGURE) \
+		install
+	$(DEPMOD) -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+	echo -e "#!/bin/sh\necho \""$(RHINO_VERSION)"\"" > $(TARGET_DIR)/$(RHINO_TARGET_BINARY)
+	chmod 755 $(TARGET_DIR)/$(RHINO_TARGET_BINARY)
+
+rhino: $(TARGET_DIR)/$(RHINO_TARGET_BINARY)
+
+rhino-clean:
+	rm -f $(TARGET_DIR)/$(RHINO_TARGET_BINARY)
+	rm -f $(RHINO_DIR)/.built
+
+rhino-dirclean:
+	rm -rf $(RHINO_DIR)
+
+rhino-source: $(RHINO_DIR)/.patched
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_RHINO)),y)
+TARGETS+=rhino
+endif
