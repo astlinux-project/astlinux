891cfa81fd48968da61d91dd4a9e4822f3ce13af
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-22 18:49:05 +0000

    e1000 and e1000e packages added, override the linux kernel dirvers with newer, stable versions. Fixes an issue with the net5501 and e1000 driver in kernel 2.6.35
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5226 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/e1000e/e1000e.mk
@@ -0,0 +1,38 @@
+#############################################################
+#
+# e1000e
+#
+#############################################################
+
+E1000E_VERSION = 1.6.3
+E1000E_SOURCE:=e1000e-$(E1000E_VERSION).tar.gz
+E1000E_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/e1000/e1000e%20stable/$(E1000E_VERSION)
+E1000E_DEPENDENCIES = linux
+E1000E_SUBDIR = src
+
+E1000E_UNINSTALL_STAGING_OPT = --version
+
+E1000E_MAKE_OPT += \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	BUILD_KERNEL=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR) \
+	VERSION_FILE=$(LINUX_DIR)/include/generated/utsrelease.h \
+	CONFIG_FILE=$(LINUX_DIR)/include/generated/autoconf.h \
+	ARCH=$(KERNEL_ARCH)
+
+define E1000E_CONFIGURE_CMDS
+	@echo "No configure"
+endef
+
+define E1000E_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(@D)/src/e1000e.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/e1000e/e1000e.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+define E1000E_UNINSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/e1000e/e1000e.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call AUTOTARGETS,package,e1000e))
