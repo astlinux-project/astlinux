c3aaadcc0b5aae12ff0c78aec18cf63f5de0c88c
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-10-24 20:56:38 +0000

    r8168 package added, common RealTek PCI-e NIC's not found in linux 2.6.35
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5235 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/r8168/r8168.mk
@@ -0,0 +1,39 @@
+#############################################################
+#
+# r8168
+#
+#############################################################
+
+R8168_VERSION = 8.025.00
+R8168_SOURCE:=r8168-$(R8168_VERSION).tar.bz2
+R8168_SITE = http://files.astlinux.org
+R8168_DEPENDENCIES = linux
+R8168_SUBDIR = src
+
+R8168_UNINSTALL_STAGING_OPT = --version
+
+R8168_MAKE_OPT += \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KDIR=$(LINUX_DIR) \
+	PWD=$(@D) \
+	KEXT=ko \
+	KFLAG=26x \
+	modules
+
+define R8168_CONFIGURE_CMDS
+	@echo "No configure"
+endef
+
+define R8168_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(@D)/src/r8168.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/r8168.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+define R8168_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/r8168.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call AUTOTARGETS,package,r8168))
