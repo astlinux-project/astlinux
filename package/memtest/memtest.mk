29b65066063ee7631c7f008c7e048f1ce32027e0
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-11-17 22:53:17 +0000

    RUNNIX memtest, use TARGET_CC to compile, some makefile cleanup, Thanks Ingmar
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5259 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/memtest/memtest.mk
+++ package/memtest/memtest.mk
@@ -11,8 +11,6 @@ MEMTEST_DIR:=$(BUILD_DIR)/memtest86+-$(MEMTEST_VER)
 MEMTEST_CAT:=zcat
 MEMTEST_BIN:=memtest.bin
 
-MEMTEST_USE_TARGET_CC:=
-
 $(DL_DIR)/$(notdir $(MEMTEST_SOURCE)):
 	 $(WGET) -P $(DL_DIR) $(MEMTEST_SITE)/$(MEMTEST_SOURCE)
 
@@ -25,14 +23,7 @@ $(MEMTEST_DIR)/.configured: $(MEMTEST_DIR)/.unpacked
 	touch $@
 
 $(MEMTEST_DIR)/$(MEMTEST_BIN): $(MEMTEST_DIR)/.configured
-ifeq ($(MEMTEST_USE_TARGET_CC),y)
 	$(MAKE1) -C $(MEMTEST_DIR) CC=$(TARGET_CC)
-else
-	$(MAKE1) -C $(MEMTEST_DIR)
-endif
-
-#$(RUNFS_DIR)/memtest: $(MEMTEST_DIR)/$(MEMTEST_BIN) | runfs
-#	cp -p $(MEMTEST_DIR)/$(MEMTEST_BIN) $(RUNFS_DIR)/memtest
 
 memtest: $(MEMTEST_DIR)/$(MEMTEST_BIN)
 
@@ -40,9 +31,6 @@ memtest-source: $(MEMTEST_DIR)/.unpacked
 
 memtest-dirclean:
 	rm -rf $(MEMTEST_DIR)
-
-#memtest-clean:
-#	rm -f $(RUNFS_DIR)/memtest
 
 #############################################################
 #
