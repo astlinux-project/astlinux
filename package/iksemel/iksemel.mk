f357000e9c2c4c0d211f999ea754387feadadf83
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-11-28 17:44:31 +0000

    iksemel package added, required for Jabber/GTalk, disabled for now
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5275 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/iksemel/iksemel.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# iksemel
+#
+#############################################################
+IKSEMEL_VERSION = 1.5-pre1
+IKSEMEL_SOURCE = iksemel-$(IKSEMEL_VERSION).tar.gz
+#IKSEMEL_SITE = http://iksemel.googlecode.com/files
+IKSEMEL_SITE = http://files.astlinux.org
+IKSEMEL_DEPENDENCIES = openssl
+
+IKSEMEL_INSTALL_STAGING = YES
+
+IKSEMEL_CONF_OPT = \
+	--disable-python
+
+define IKSEMEL_CONFIGURE_PREFLIGHT
+	( cd $(@D) ; \
+	  ./autogen.sh ; \
+	)
+endef
+IKSEMEL_PRE_CONFIGURE_HOOKS += IKSEMEL_CONFIGURE_PREFLIGHT
+
+define IKSEMEL_INSTALL_TARGET_CMDS
+        cp -a $(STAGING_DIR)/usr/lib/libiksemel.so* $(TARGET_DIR)/usr/lib/
+endef
+
+define IKSEMEL_UNINSTALL_TARGET_CMDS
+        rm -f $(TARGET_DIR)/usr/lib/libiksemel.so*
+endef
+
+$(eval $(call AUTOTARGETS,package,iksemel))
