a68087697ab441fe6e39482dccd62bba88c9bf83
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-12-03 15:43:02 +0000

    openssh, use '/usr/libexec' for sftp-server, etc.
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5279 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -7,7 +7,7 @@
 OPENSSH_VERSION = 5.9p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
-OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
+OPENSSH_CONF_OPT = --libexecdir=/usr/libexec --disable-lastlog --disable-utmp \
 		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip \
 		--sysconfdir=/etc/ssh
 
