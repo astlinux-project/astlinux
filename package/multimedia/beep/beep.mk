959bbfa059f88be3bdc3a2b3b99b3b774149d0bc
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-12-07 00:55:44 +0000

    beep, a new package, the goal is to optionally play a ditty when AstLinux is up, removed old beep script
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5293 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/multimedia/beep/beep.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# beep
+#
+#############################################################
+
+BEEP_VERSION = 1.3
+BEEP_SOURCE:=beep-$(BEEP_VERSION).tar.gz
+BEEP_SITE = http://www.johnath.com/beep
+
+define BEEP_CONFIGURE_CMDS
+        @echo "No configure"
+endef
+
+BEEP_MAKE_OPT = CC='$(TARGET_CC)' LD='$(TARGET_LD)' -C $(@D) beep
+
+define BEEP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/beep $(TARGET_DIR)/usr/bin/beep
+endef
+
+define BEEP_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/beep
+endef
+
+$(eval $(call AUTOTARGETS,package,beep))
