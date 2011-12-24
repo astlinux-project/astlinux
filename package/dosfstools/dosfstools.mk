4c9f02cfba0aa4af022c52711a8f4cbbd3963a95
Lonnie Abelbeck <lonnie@abelbeck.com>
2011-12-24 03:45:01 +0000

    dosfstools, version bump to 3.0.12
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5338 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/dosfstools/dosfstools.mk
+++ package/dosfstools/dosfstools.mk
@@ -3,7 +3,7 @@
 # dosfstools
 #
 #############################################################
-DOSFSTOOLS_VERSION = 3.0.11
+DOSFSTOOLS_VERSION = 3.0.12
 DOSFSTOOLS_SOURCE = dosfstools-$(DOSFSTOOLS_VERSION).tar.gz
 DOSFSTOOLS_SITE = http://www.daniel-baumann.ch/software/dosfstools
 MKDOSFS_BINARY = mkdosfs
