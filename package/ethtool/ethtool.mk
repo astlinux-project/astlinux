dbab9db01ff08211248e5bc967dcbfc55f5fecc0
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-01-09 20:27:54 +0000

    ethtool, version bump to 3.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5363 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,8 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 3.0
-ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool/
+ETHTOOL_VERSION = 3.1
+ETHTOOL_SITE = http://www.kernel.org/pub/software/network/ethtool
+#ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 
 $(eval $(call AUTOTARGETS,package,ethtool))
