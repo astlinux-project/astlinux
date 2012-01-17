4142b624bd500bf92db2abf69a821b55eefdad27
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-01-17 16:24:11 +0000

    p910nd, version bump to 0.95
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5389 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/p910nd/p910nd.mk
+++ package/p910nd/p910nd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-P910ND_VERSION = 0.94
+P910ND_VERSION = 0.95
 P910ND_SOURCE:=p910nd-$(P910ND_VERSION).tar.bz2
 P910ND_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/p910nd
 
