08b872e80a75cbd7c1f1d90be1497c51368525d7
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-01-31 00:47:55 +0000

    testmail, add local interfaces to the email
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5431 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/msmtp/testmail.sh
+++ package/msmtp/testmail.sh
@@ -13,6 +13,18 @@ TO="$1"
 
 FROM="$2"
 
+dev_to_ipv4_address()
+{
+  ip -o addr show dev "$1" 2>/dev/null | \
+        awk '$3 == "inet" { split($4, field, "/"); print field[1]; }'
+}
+
+dev_to_ipv4_network()
+{
+  ip -o addr show dev "$1" 2>/dev/null | \
+        awk '$3 == "inet" { print $4; }'
+}
+
 if [ -z "$TO" ]; then
   echo "Usage: testmail TO_email_address [ FROM_email_address ]"
   exit 1
@@ -33,8 +45,19 @@ Test Email from '$HOSTNAME'
 "
   echo "Hostname:   $HOSTNAME"
   echo "System Time:   $(date)"
-  echo "IPv4 Address:   $(ip -o addr show dev "$EXTIF" 2>/dev/null | \
-                          awk '$3 == "inet" { split($4, field, "/"); print field[1]; }')"
+  echo "External IPv4 Address:   $(dev_to_ipv4_address $EXTIF)"
+  if [ -n "$INTIF" ]; then
+    echo "1st LAN IPv4 Network:   $(dev_to_ipv4_network $INTIF)"
+  fi
+  if [ -n "$INT2IF" ]; then
+    echo "2nd LAN IPv4 Network:   $(dev_to_ipv4_network $INT2IF)"
+  fi
+  if [ -n "$INT3IF" ]; then
+    echo "3rd LAN IPv4 Network:   $(dev_to_ipv4_network $INT3IF)"
+  fi
+  if [ -n "$DMZIF" ]; then
+    echo "The DMZ IPv4 Network:   $(dev_to_ipv4_network $DMZIF)"
+  fi
   if [ -f /etc/astlinux-release ]; then
     if [ -x /usr/sbin/asterisk ]; then
       echo "AstLinux Release:   $(cat /etc/astlinux-release) - $(/usr/sbin/asterisk -V)"
