1e824a7764839cd3de61d464001a2c659b6de077
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-05-22 23:27:30 +0000

    openssl, version bump to 0.9.8x, security fix CVE-2012-2333
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5548 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 0.9.8w
+OPENSSL_VERSION = 0.9.8x
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
