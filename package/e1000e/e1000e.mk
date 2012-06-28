c288c2f2c5406ae0c7662721aca37a328f8a5a3a
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-06-28 20:39:23 +0000

    e1000e, version bump to 2.0.0.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5580 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/e1000e/e1000e.mk
+++ package/e1000e/e1000e.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-E1000E_VERSION = 1.10.6
+E1000E_VERSION = 2.0.0.1
 E1000E_SOURCE:=e1000e-$(E1000E_VERSION).tar.gz
 E1000E_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/e1000/e1000e%20stable/$(E1000E_VERSION)
 E1000E_DEPENDENCIES = linux
