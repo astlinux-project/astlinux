3b9a524c047569b2b241d0bfee889e474e0512b3
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-08-25 18:40:45 +0000

    netsnmp, version bump to 5.7.1 (LTS), security fix CVE-2012-2141, and enabled for IPv6
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5638 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/netsnmp/netsnmp.mk
+++ package/netsnmp/netsnmp.mk
@@ -4,14 +4,14 @@
 #
 #############################################################
 
-NETSNMP_VERSION = 5.6.1
+NETSNMP_VERSION = 5.7.1
 NETSNMP_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/net-snmp
 NETSNMP_SOURCE = net-snmp-$(NETSNMP_VERSION).tar.gz
 NETSNMP_INSTALL_STAGING = YES
 NETSNMP_CONF_ENV = ac_cv_NETSNMP_CAN_USE_SYSCTL=yes
 NETSNMP_CONF_OPT = --with-persistent-directory=/var/lib/snmp --disable-static \
 		--with-defaults --enable-mini-agent --without-rpm \
-		--with-logfile=none --without-kmem-usage $(DISABLE_IPV6) \
+		--with-logfile=none --without-kmem-usage \
 		--enable-as-needed --disable-debugging --without-perl-modules \
 		--disable-embedded-perl --disable-perl-cc-checks \
 		--disable-scripts --with-default-snmp-version="1" \
@@ -21,6 +21,7 @@ NETSNMP_CONF_OPT = --with-persistent-directory=/var/lib/snmp --disable-static \
 		--with-mib-modules="host ucd-snmp/dlmod" \
 		--with-out-mib-modules="disman/event disman/schedule utilities" \
 		--with-out-transports="Unix"
+NETSNMP_MAKE = $(MAKE1)
 NETSNMP_BLOAT_MIBS = BRIDGE DISMAN-EVENT DISMAN-SCHEDULE DISMAN-SCRIPT EtherLike RFC-1215 RFC1155-SMI RFC1213 SCTP SMUX
 
 ifeq ($(BR2_ENDIAN),"BIG")
@@ -48,6 +49,8 @@ ifneq ($(BR2_INET_IPV6),y)
 define NETSNMP_REMOVE_MIBS_IPV6
 	rm -f $(TARGET_DIR)/usr/share/snmp/mibs/IPV6*
 endef
+else
+	NETSNMP_CONF_OPT += --enable-ipv6
 endif
 
 define NETSNMP_INSTALL_TARGET_CMDS
