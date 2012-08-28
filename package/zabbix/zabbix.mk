4cf4f0eb14f94fab62f3bc118d70e5c77a8f59b7
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-08-28 22:54:00 +0000

    zabbix, more build tweaks
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5648 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/zabbix/zabbix.mk
+++ package/zabbix/zabbix.mk
@@ -7,31 +7,29 @@
 ZABBIX_VERSION = 2.0.2
 ZABBIX_SOURCE = zabbix-$(ZABBIX_VERSION).tar.gz
 ZABBIX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/zabbix
+ZABBIX_AUTORECONF = YES
 
 ZABBIX_CONF_OPT = \
 	--enable-agent
 
 ifeq ($(strip $(BR2_PACKAGE_ZABBIX_PROXY)),y)
-ZABBIX_CONF_OPT+= \
-	--enable-proxy
+ZABBIX_DEPENDENCIES += sqlite
+ZABBIX_CONF_OPT += \
+	--enable-proxy \
+	--with-sqlite3="$(STAGING_DIR)/usr"
+
+ ifeq ($(strip $(BR2_PACKAGE_CURL)),y)
+ZABBIX_DEPENDENCIES += libcurl
+ZABBIX_CONF_OPT += \
+	--with-libcurl="$(STAGING_DIR)/usr/bin/curl-config"
+ endif
+
+ ifeq ($(strip $(BR2_PACKAGE_NETSNMP)),y)
+ZABBIX_DEPENDENCIES += netsnmp
+ZABBIX_CONF_OPT += \
+	--with-net-snmp="$(STAGING_DIR)/usr/bin/net-snmp-config"
+ endif
 endif
-
-ifeq ($(strip $(BR2_PACKAGE_CURL)),y)
-ZABBIX_CONF_OPT+= \
-                        --with-libcurl="$(STAGING_DIR)/usr/bin/curl-config"
-endif
-
-ifeq ($(strip $(BR2_PACKAGE_NETSNMP)),y)
-ZABBIX_CONF_OPT+= \
-                        --with-net-snmp="$(STAGING_DIR)/usr/bin/net-snmp-config"
-endif
-
-
-ifeq ($(strip $(BR2_PACKAGE_SQLITE)),y)
-ZABBIX_CONF_OPT+= \
-                        --with-sqlite3="$(STAGING_DIR)"
-endif
-
 
 define ZABBIX_CONFIGURE_CMDS
         (cd $(@D); \
@@ -50,7 +48,10 @@ endef
 
 define ZABBIX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/src/zabbix_agent/zabbix_agentd $(TARGET_DIR)/usr/bin/zabbix_agentd
-	$(INSTALL) -m 0755 -D $(@D)/src/zabbix_proxy/zabbix_proxy $(TARGET_DIR)/usr/bin/zabbix_proxy
+	if [ -f $(@D)/src/zabbix_proxy/zabbix_proxy ]; then \
+	  $(INSTALL) -m 0755 -D $(@D)/src/zabbix_proxy/zabbix_proxy $(TARGET_DIR)/usr/bin/zabbix_proxy ; \
+          ln -sf /tmp/etc/zabbix_proxy.conf $(TARGET_DIR)/etc/zabbix_proxy.conf ; \
+	fi
         $(INSTALL) -m 0755 -D package/zabbix/zabbix.init $(TARGET_DIR)/etc/init.d/zabbix
         ln -sf /tmp/etc/zabbix_agentd.conf $(TARGET_DIR)/etc/zabbix_agentd.conf
 endef
@@ -59,6 +60,8 @@ define ZABBIX_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/zabbix_agentd
 	rm -f $(TARGET_DIR)/usr/bin/zabbix_proxy
         rm -f $(TARGET_DIR)/etc/init.d/zabbix
+        rm -f $(TARGET_DIR)/etc/zabbix_agentd.conf
+        rm -f $(TARGET_DIR)/etc/zabbix_proxy.conf
 endef
 
-$(eval $(call AUTOTARGETS,package,zabbix,zabbix_proxy))
+$(eval $(call AUTOTARGETS,package,zabbix))
