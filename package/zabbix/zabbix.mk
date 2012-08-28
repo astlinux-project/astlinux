bf1c29f44f0519d6130b4987d681640e519c7b14
Darrick Hartman <dhartman@djhsolutions.com>
2012-08-28 05:24:19 +0000

    build zabbix_proxy support, needs more polish. Package also needs to be updated to 2.0.x release. May want to make the proxy optional as it adds 500K
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5645 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/zabbix/zabbix.mk
+++ package/zabbix/zabbix.mk
@@ -9,7 +9,24 @@ ZABBIX_SOURCE = zabbix-$(ZABBIX_VERSION).tar.gz
 ZABBIX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/zabbix
 
 ZABBIX_CONF_OPT = \
-	--enable-agent
+	--enable-agent --enable-proxy
+
+ifeq ($(strip $(BR2_PACKAGE_CURL)),y)
+ZABBIX_CONF_OPT+= \
+                        --with-libcurl="$(STAGING_DIR)/usr/bin/curl-config"
+endif
+
+ifeq ($(strip $(BR2_PACKAGE_NETSNMP)),y)
+ZABBIX_CONF_OPT+= \
+                        --with-net-snmp="$(STAGING_DIR)/usr/bin/net-snmp-config"
+endif
+
+
+ifeq ($(strip $(BR2_PACKAGE_SQLITE)),y)
+ZABBIX_CONF_OPT+= \
+                        --with-sqlite3="$(STAGING_DIR)"
+endif
+
 
 define ZABBIX_CONFIGURE_CMDS
         (cd $(@D); \
@@ -28,12 +45,14 @@ endef
 
 define ZABBIX_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/src/zabbix_agent/zabbix_agentd $(TARGET_DIR)/usr/bin/zabbix_agentd
+	$(INSTALL) -m 0755 -D $(@D)/src/zabbix_proxy/zabbix_proxy $(TARGET_DIR)/usr/bin/zabbix_proxy
         $(INSTALL) -m 0755 -D package/zabbix/zabbix.init $(TARGET_DIR)/etc/init.d/zabbix
         ln -sf /tmp/etc/zabbix_agentd.conf $(TARGET_DIR)/etc/zabbix_agentd.conf
 endef
 
 define ZABBIX_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/zabbix_agentd
+	rm -f $(TARGET_DIR)/usr/bin/zabbix_proxy
         rm -f $(TARGET_DIR)/etc/init.d/zabbix
 endef
 
