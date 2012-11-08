ae449216fb9fac24e8b555d883d562584b19740f
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-11-08 19:08:11 +0000

    asterisk, version bump to 1.8.18.0 and 11.0.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5761 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -10,9 +10,9 @@ else
 ASTERISK_VERSION := 1.6.2.21
  else
   ifeq ($(BR2_PACKAGE_ASTERISK_v1_8),y)
-ASTERISK_VERSION := 1.8.17.0
+ASTERISK_VERSION := 1.8.18.0
   else
-ASTERISK_VERSION := 11.0.0
+ASTERISK_VERSION := 11.0.1
   endif
  endif
 endif
