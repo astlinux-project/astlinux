870502906046a88be916de67d11dabaa37355452
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-12-12 23:35:33 +0000

    links, version bump to 2.7
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5802 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/links/links.mk
+++ package/links/links.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LINKS_VERSION = 2.5
+LINKS_VERSION = 2.7
 LINKS_SITE = http://links.twibright.com/download
 LINKS_CONF_OPT = --without-x
 
