2ec828ddd943b71211873d6b8c801dd64d9cf0e6
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-12-15 17:03:51 +0000

    zlib, version bump to 1.2.7
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5806 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/zlib/zlib.mk
+++ package/zlib/zlib.mk
@@ -3,10 +3,10 @@
 # zlib
 #
 #############################################################
-ZLIB_VERSION:=1.2.6
-ZLIB_SOURCE:=zlib-$(ZLIB_VERSION).tar.bz2
-ZLIB_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libpng
-ZLIB_INSTALL_STAGING=YES
+ZLIB_VERSION = 1.2.7
+ZLIB_SOURCE = zlib-$(ZLIB_VERSION).tar.bz2
+ZLIB_SITE = http://downloads.sourceforge.net/project/libpng/zlib/$(ZLIB_VERSION)
+ZLIB_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PREFER_STATIC_LIB),y)
 ZLIB_PIC :=
