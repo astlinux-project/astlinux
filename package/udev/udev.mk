9b698fb3059d206f45729e8873338dce7ea315f3
Lonnie Abelbeck <lonnie@abelbeck.com>
2012-12-17 00:59:21 +0000

    udev, stay with version 173 though enable use of usb.ids and pci.ids hwdb which is required for newer versions of udev and adds extra rules
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5813 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -6,11 +6,15 @@
 UDEV_VERSION = 173
 UDEV_SOURCE = udev-$(UDEV_VERSION).tar.bz2
 UDEV_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/hotplug/
+UDEV_INSTALL_STAGING = YES
 
 UDEV_CONF_OPT =			\
 	--sbindir=/sbin		\
 	--with-rootlibdir=/lib	\
 	--libexecdir=/lib/udev	\
+	--with-usb-ids-path=/usr/share/usb.ids \
+	--with-pci-ids-path=/usr/share/pci.ids \
+	--with-firmware-path=/lib/firmware \
 	--disable-introspection
 
 UDEV_DEPENDENCIES = host-gperf host-pkg-config
@@ -18,12 +22,9 @@ UDEV_DEPENDENCIES = host-gperf host-pkg-config
 ifeq ($(BR2_PACKAGE_UDEV_ALL_EXTRAS),y)
 UDEV_DEPENDENCIES += libusb libusb-compat acl usbutils hwdata libglib2
 UDEV_CONF_OPT +=							\
-	--with-pci-ids-path=$(TARGET_DIR)/usr/share/hwdata/pci.ids	\
-	--with-usb-ids-path=$(TARGET_DIR)/usr/share/hwdata/usb.ids	\
 	--enable-udev_acl
 else
 UDEV_CONF_OPT +=		\
-	--disable-hwdb		\
 	--disable-gudev
 endif
 
