be349fdbfdb3964ddc83dde68a9d470049f9aba1
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-01-06 04:57:51 +0000

    expat, version bump to 2.1.0
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5865 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-EXPAT_VERSION = 2.0.1
+EXPAT_VERSION = 2.1.0
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.gz
-EXPAT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/expat
+EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_INSTALL_STAGING = YES
 EXPAT_INSTALL_TARGET = YES
 EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
