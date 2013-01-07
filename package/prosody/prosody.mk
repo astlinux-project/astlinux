e4455f451b8c63c3a45ddd9e01095f26c84e8843
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-01-07 07:30:40 +0000

    prosody, add a prosody.init script and runlevels, script still needs some work luafilesystem, version bump to 1.6.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5871 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/prosody/prosody.mk
+++ package/prosody/prosody.mk
@@ -25,4 +25,29 @@ define PROSODY_CONFIGURE_CMDS
         )
 endef
 
+define PROSODY_POST_INSTALL
+	mkdir -p $(TARGET_DIR)/stat/etc/prosody
+	cp $(TARGET_DIR)/etc/prosody/prosody.cfg.lua $(TARGET_DIR)/stat/etc/prosody/prosody.cfg.lua
+	rm -rf $(TARGET_DIR)/etc/prosody
+	$(INSTALL) -m 0755 -D package/prosody/prosody.init $(TARGET_DIR)/etc/init.d/prosody
+	ln -s /tmp/etc/prosody $(TARGET_DIR)/etc/prosody
+	ln -sf ../../init.d/prosody $(TARGET_DIR)/etc/runlevels/default/S58prosody
+	ln -sf ../../init.d/prosody $(TARGET_DIR)/etc/runlevels/default/K02prosody
+endef
+
+PROSODY_POST_INSTALL_TARGET_HOOKS = PROSODY_POST_INSTALL
+
+PROSODY_UNINSTALL_STAGING_OPT = --version
+
+define PROSODY_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/prosody
+	rm -f $(TARGET_DIR)/usr/bin/prosodyctl
+	rm -rf $(TARGET_DIR)/usr/lib/prosody
+	rm -rf $(TARGET_DIR)/stat/etc/prosody
+	rm -f $(TARGET_DIR)/etc/prosody
+	rm -f $(TARGET_DIR)/etc/init.d/prosody
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S58prosody
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K02prosody
+endef
+
 $(eval $(call AUTOTARGETS,package,prosody))
