5f44223b54c7c58ea9091ed95db6a2d7bf8d4fa4
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-01-14 20:04:05 +0000

    libical, new package, not hooked to menuconfig yet
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5898 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/libical/libical.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# libical
+#
+#############################################################
+
+LIBICAL_VERSION = 0.48
+LIBICAL_SITE = http://downloads.sourceforge.net/project/freeassociation/libical/libical-$(LIBICAL_VERSION)
+LIBICAL_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,libical))
