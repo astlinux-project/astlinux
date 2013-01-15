4749491d0419b33a4d1e76f66435664e732e513f
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-01-15 19:29:44 +0000

    libical, add --with-builtintz to fix segfault, but we should not need it also use the latest libical from their trunk r1139
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5903 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libical/libical.mk
+++ package/libical/libical.mk
@@ -4,8 +4,15 @@
 #
 #############################################################
 
-LIBICAL_VERSION = 0.48
-LIBICAL_SITE = http://downloads.sourceforge.net/project/freeassociation/libical/libical-$(LIBICAL_VERSION)
+#LIBICAL_VERSION = 0.48
+#LIBICAL_SITE = http://downloads.sourceforge.net/project/freeassociation/libical/libical-$(LIBICAL_VERSION)
+LIBICAL_VERSION = r1139
+LIBICAL_SITE = http://files.astlinux.org
 LIBICAL_INSTALL_STAGING = YES
+LIBICAL_AUTORECONF = YES
+
+LIBICAL_CONF_OPT = \
+		--disable-static \
+		--with-builtintz
 
 $(eval $(call AUTOTARGETS,package,libical))
