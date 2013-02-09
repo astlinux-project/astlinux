2ad83895a255268db1c0bb5a92f96f9c3ef9667e
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-02-09 22:14:36 +0000

    openvpn, remove easy-rsa scripts
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5948 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -35,11 +35,9 @@ define OPENVPN_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -D package/openvpn/openvpnclient.init $(TARGET_DIR)/etc/init.d/openvpnclient
 	$(INSTALL) -m 755 -D package/openvpn/tls-verify.sh $(TARGET_DIR)/usr/sbin/openvpn-tls-verify
 	$(INSTALL) -m 755 -D package/openvpn/user-pass-verify.sh $(TARGET_DIR)/usr/sbin/openvpn-user-pass-verify
-	mkdir -p $(TARGET_DIR)/stat/etc/openvpn
 	ln -sf /tmp/etc/openvpn.conf $(TARGET_DIR)/etc/openvpn.conf
 	ln -sf /tmp/etc/openvpnclient.conf $(TARGET_DIR)/etc/openvpnclient.conf
 	ln -sf /tmp/etc/openvpn $(TARGET_DIR)/etc/openvpn
-	tar -C $(TARGET_DIR)/stat/etc/openvpn -xzf package/openvpn/easy-rsa.tar.gz
 endef
 
 define OPENVPN_UNINSTALL_TARGET_CMDS
