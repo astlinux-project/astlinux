c362526bcbf0d6b9bcbdfa3eecc94976073860d9
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-03-05 00:48:52 +0000

    unixodbc, new package - disabled by default, for now. When selected, asterisk now builds all related 'odbc' modules
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5976 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/unixodbc/unixodbc.mk
@@ -0,0 +1,42 @@
+#############################################################
+#
+# unixodbc
+#
+#############################################################
+UNIXODBC_VERSION = 2.3.1
+UNIXODBC_SOURCE = unixODBC-$(UNIXODBC_VERSION).tar.gz
+UNIXODBC_SITE = ftp://ftp.unixodbc.org/pub/unixODBC
+UNIXODBC_DEPENDENCIES = libtool
+
+UNIXODBC_INSTALL_STAGING = YES
+
+UNIXODBC_CONF_OPT = \
+	--disable-gui \
+	--with-pic \
+	--enable-drivers
+
+define UNIXODBC_INSTALL_TARGET_CMDS
+        cp -a $(STAGING_DIR)/usr/lib/libodbc[.ci]*so* $(TARGET_DIR)/usr/lib/
+        cp -a $(STAGING_DIR)/usr/lib/libnn.*so* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -m 0755 -D package/unixodbc/unixodbc.init $(TARGET_DIR)/etc/init.d/unixodbc
+	$(INSTALL) -m 0755 -D $(STAGING_DIR)/usr/bin/isql $(TARGET_DIR)/usr/bin/isql
+	$(INSTALL) -m 0755 -D $(STAGING_DIR)/usr/bin/odbcinst $(TARGET_DIR)/usr/bin/odbcinst
+	ln -sf /tmp/etc/odbc.ini $(TARGET_DIR)/etc/odbc.ini
+	ln -sf /tmp/etc/odbcinst.ini $(TARGET_DIR)/etc/odbcinst.ini
+	ln -sf /tmp/etc/ODBCDataSources $(TARGET_DIR)/etc/ODBCDataSources
+	ln -sf ../../init.d/unixodbc $(TARGET_DIR)/etc/runlevels/default/S00unixodbc
+endef
+
+define UNIXODBC_UNINSTALL_TARGET_CMDS
+        rm -f $(TARGET_DIR)/usr/lib/libodbc[.ci]*so*
+        rm -f $(TARGET_DIR)/usr/lib/libnn.*so*
+        rm -f $(TARGET_DIR)/etc/init.d/unixodbc
+        rm -f $(TARGET_DIR)/usr/bin/isql
+        rm -f $(TARGET_DIR)/usr/bin/odbcinst
+        rm -f $(TARGET_DIR)/etc/odbc.ini
+        rm -f $(TARGET_DIR)/etc/odbcinst.ini
+        rm -f $(TARGET_DIR)/etc/ODBCDataSources
+        rm -f $(TARGET_DIR)/etc/runlevels/default/S00unixodbc
+endef
+
+$(eval $(call AUTOTARGETS,package,unixodbc))
