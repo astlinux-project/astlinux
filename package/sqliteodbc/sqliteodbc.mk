a77c1e1fc9e46e2afe19e1a51b5faf217a912d52
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-03-05 18:28:32 +0000

    sqliteodbc, new package, disabled by default, for now. This lib allows a sqlite3 database to be used by asterisk, eg. ODBC dialplan functions can be created to read/write to a sqlite3 database
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@5979 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/sqliteodbc/sqliteodbc.mk
@@ -0,0 +1,31 @@
+#############################################################
+#
+# sqliteodbc
+#
+#############################################################
+SQLITEODBC_VERSION = 0.99
+SQLITEODBC_SOURCE = sqliteodbc-$(SQLITEODBC_VERSION).tar.gz
+SQLITEODBC_SITE = http://www.ch-werner.de/sqliteodbc
+SQLITEODBC_SUBDIR = sqliteodbc-$(SQLITEODBC_VERSION)
+SQLITEODBC_DEPENDENCIES = sqlite unixodbc
+
+SQLITEODBC_INSTALL_STAGING = YES
+
+SQLITEODBC_CONF_OPT = \
+	--disable-static \
+	--with-pic \
+	--with-sqlite="$(STAGING_DIR)/usr" \
+	--with-sqlite3="$(STAGING_DIR)/usr" \
+	--with-odbc="$(STAGING_DIR)/usr"
+
+define SQLITEODBC_INSTALL_TARGET_CMDS
+        cp -a $(STAGING_DIR)/usr/lib/libsqlite3odbc*.so $(TARGET_DIR)/usr/lib/
+        #cp -a $(STAGING_DIR)/usr/lib/libsqlite3_mod_*.so $(TARGET_DIR)/usr/lib/
+endef
+
+define SQLITEODBC_UNINSTALL_TARGET_CMDS
+        rm -f $(TARGET_DIR)/usr/lib/libsqlite3odbc*.so
+        #rm -f $(TARGET_DIR)/usr/lib/libsqlite3_mod_*.so
+endef
+
+$(eval $(call AUTOTARGETS,package,sqliteodbc))
