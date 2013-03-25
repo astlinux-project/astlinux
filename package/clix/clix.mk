5eb559b469d96d202748debc6f7d5c9cbac6f9a6
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-03-25 18:51:07 +0000

    sendxmpp, new command, users should use sendxmpp instead of directly calling clix More Info: sendxmpp --help
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6013 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/clix/clix.mk
+++ package/clix/clix.mk
@@ -9,10 +9,12 @@ CLIX_SITE = http://files.astlinux.org
 
 define CLIX_INSTALL_TARGET_CMDS
 	install -D -m 755 $(@D)/clix.bin $(TARGET_DIR)/usr/bin/clix
+	install -D -m 755 package/clix/sendxmpp $(TARGET_DIR)/usr/bin/sendxmpp
 endef
 
 define CLIX_UNINSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/usr/bin/clix
+	rm -rf $(TARGET_DIR)/usr/bin/sendxmpp
 endef
 
 $(eval $(call GENTARGETS,package,clix))
