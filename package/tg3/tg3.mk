1dc927dd9420d859605baa81c8c1b378761525c8
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-05-17 23:01:48 +0000

    tg3, new package, build NIC kernel driver from source, version 3.124c
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6089 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/tg3/tg3.mk
@@ -0,0 +1,38 @@
+#############################################################
+#
+# tg3
+#
+#############################################################
+
+TG3_VERSION = 3.124c
+TG3_SOURCE:=tg3-$(TG3_VERSION).tar.gz
+TG3_SITE = http://files.astlinux.org
+TG3_DEPENDENCIES = linux
+
+TG3_UNINSTALL_STAGING_OPT = --version
+
+TG3_MAKE_OPT += \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	KVER=$(LINUX_VERSION_PROBED) \
+	BCMPROC=$(KERNEL_ARCH) \
+	BCMCFGDIR=$(LINUX_DIR) \
+	BCMMODDIR=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net \
+	BCMSRCDIR=$(LINUX_DIR) \
+	ARCH=$(KERNEL_ARCH)
+
+define TG3_CONFIGURE_CMDS
+	@echo "No configure"
+endef
+
+define TG3_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(@D)/tg3.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/tg3.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+define TG3_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/tg3.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call AUTOTARGETS,package,tg3))
