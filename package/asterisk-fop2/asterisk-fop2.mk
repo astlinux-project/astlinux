b1136f2f764d6a6b51cb973facfee67c73ea897a
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-07-12 18:10:44 +0000

    New package 'asterisk-fop2', adds the framework for supporting the 'Flash Operator Panel 2' (free for 15 buttons, very reasonable for unlimited).  The actual 'fop2' code is downloaded on ASTURW as a 'package' using the new CLI command 'upgrade-package'
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6130 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/asterisk-fop2/asterisk-fop2.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# asterisk-fop2
+#
+#############################################################
+ASTERISK_FOP2_VERSION = 2.27
+ASTERISK_FOP2_SOURCE = fop2-$(ASTERISK_FOP2_VERSION)-debian-i386.tgz
+ASTERISK_FOP2_SITE = http://download.fop2.com
+# Note: be sure to edit "project/astlinux/target_skeleton/usr/sbin/upgrade-package" on version change
+
+define ASTERISK_FOP2_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 package/asterisk-fop2/fop2.init $(TARGET_DIR)/etc/init.d/fop2
+	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/fop2.cfg $(TARGET_DIR)/stat/etc/fop2/fop2.cfg
+	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/buttons.cfg $(TARGET_DIR)/stat/etc/fop2/buttons.cfg
+	ln -snf /tmp/etc/fop2 $(TARGET_DIR)/etc/fop2
+        ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/S82fop2
+        ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/K00fop2
+endef
+
+define ASTERISK_FOP2_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/etc/init.d/fop2
+	rm -rf $(TARGET_DIR)/stat/etc/fop2
+	rm -f $(TARGET_DIR)/etc/fop2
+        rm -f $(TARGET_DIR)/etc/runlevels/default/S82fop2
+        rm -f $(TARGET_DIR)/etc/runlevels/default/K00fop2
+endef
+
+$(eval $(call GENTARGETS,package,asterisk-fop2))
