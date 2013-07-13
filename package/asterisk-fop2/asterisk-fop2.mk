4e67ef05b5be525198a500fed5026707974df561
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-07-13 20:21:34 +0000

    fop2, add RAM check to disallow less than 512 MB systems, move to S98 runlevel
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6133 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/asterisk-fop2/asterisk-fop2.mk
+++ package/asterisk-fop2/asterisk-fop2.mk
@@ -13,16 +13,16 @@ define ASTERISK_FOP2_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/fop2.cfg $(TARGET_DIR)/stat/etc/fop2/fop2.cfg
 	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/buttons.cfg $(TARGET_DIR)/stat/etc/fop2/buttons.cfg
 	ln -snf /tmp/etc/fop2 $(TARGET_DIR)/etc/fop2
-        ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/S82fop2
-        ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/K00fop2
+	ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/S98fop2
+	ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/K00fop2
 endef
 
 define ASTERISK_FOP2_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/etc/init.d/fop2
 	rm -rf $(TARGET_DIR)/stat/etc/fop2
 	rm -f $(TARGET_DIR)/etc/fop2
-        rm -f $(TARGET_DIR)/etc/runlevels/default/S82fop2
-        rm -f $(TARGET_DIR)/etc/runlevels/default/K00fop2
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S98fop2
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K00fop2
 endef
 
 $(eval $(call GENTARGETS,package,asterisk-fop2))
