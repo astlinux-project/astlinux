fc26c1d8268b7907d91c575beb9468caa576b635
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-08-06 12:58:43 +0000

    logrotate, enable by default to manage non-syslog log files
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6165 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -3,9 +3,9 @@
 # logrotate
 #
 #############################################################
-LOGROTATE_VERSION = 3.7.9
+LOGROTATE_VERSION = 3.8.4
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.gz
-LOGROTATE_SITE = https://fedorahosted.org/releases/l/o/logrotate/
+LOGROTATE_SITE = https://www.fedorahosted.org/releases/l/o/logrotate
 
 LOGROTATE_DEPENDENCIES = popt
 
@@ -15,10 +15,10 @@ endef
 
 define LOGROTATE_INSTALL_TARGET_CMDS
 	$(MAKE) PREFIX=$(TARGET_DIR) -C $(@D) install
-	if [ ! -f $(TARGET_DIR)/etc/logrotate.conf ]; then \
-		$(INSTALL) -m 0644 package/logrotate/logrotate.conf $(TARGET_DIR)/etc/logrotate.conf; \
+	$(INSTALL) -m 0644 package/logrotate/logrotate.conf $(TARGET_DIR)/etc/logrotate.conf
+	if [ ! -d $(TARGET_DIR)/etc/logrotate.d ]; then \
+		$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/logrotate.d; \
 	fi
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/logrotate.d
 endef
 
 define LOGROTATE_UNINSTALL_TARGET_CMDS
@@ -26,7 +26,6 @@ define LOGROTATE_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/etc/logrotate.conf
 	rm -f $(TARGET_DIR)/usr/man/man5/logrotate.conf.5
 	rm -f $(TARGET_DIR)/usr/man/man8/logrotate.8
-	rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/etc/logrotate.d
 endef
 
 define LOGROTATE_CLEAN_CMDS
