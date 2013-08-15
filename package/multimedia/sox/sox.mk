c5565136d75685cb15b227da49c46603fe021439
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-08-15 01:35:01 +0000

    sox, version bump to 14.4.1, 8 YEARS newer from previous 12.17.9
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6172 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/multimedia/sox/sox.mk
+++ package/multimedia/sox/sox.mk
@@ -4,22 +4,38 @@
 #
 #############################################################
 
-SOX_VERSION = 12.17.9
+SOX_VERSION = 14.4.1
 SOX_SOURCE:=sox-$(SOX_VERSION).tar.gz
-SOX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/sox
+SOX_SITE = http://downloads.sourceforge.net/sourceforge/sox
+SOX_INSTALL_STAGING = YES
+
+SOX_CONF_OPT = \
+	--without-libltdl \
+	--without-flac \
+	--without-ffmpeg \
+	--without-ladspa \
+	--without-lame \
+	--without-id3tag
 
 ifeq ($(BR2_PACKAGE_LIBMAD),y)
-SOX_DEPENDENCIES = libmad
+	SOX_DEPENDENCIES += libmad
+	SOX_CONF_OPT += --with-mad
+else
+	SOX_CONF_OPT += --without-mad
 endif
 
 define SOX_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/src/sox $(TARGET_DIR)/usr/bin/sox
-	$(INSTALL) -m 0755 -D $(@D)/src/soxmix $(TARGET_DIR)/usr/bin/soxmix
+	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/sox $(TARGET_DIR)/usr/bin/sox
+	cp -a $(STAGING_DIR)/usr/bin/rec $(TARGET_DIR)/usr/bin/
+	cp -a $(STAGING_DIR)/usr/bin/play $(TARGET_DIR)/usr/bin/
+	cp -a $(STAGING_DIR)/usr/lib/libsox.so* $(TARGET_DIR)/usr/lib/
 endef
 
 define SOX_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/sox
-	rm -f $(TARGET_DIR)/usr/bin/soxmix
+	rm -f $(TARGET_DIR)/usr/bin/rec
+	rm -f $(TARGET_DIR)/usr/bin/play
+	rm -f $(TARGET_DIR)/usr/lib/libsox.so*
 endef
 
 $(eval $(call AUTOTARGETS,package,sox))
