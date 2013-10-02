171df263e81386b7cb3fb4e04d33ce13f8ff0427
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-10-02 19:21:33 +0000

    slapd LDAP server, initial check-in, not enabled in default configs, runtime enable with LDAP_SERVER=yes The goal is to setup an ODBC SQL(ite3) LDAP backend to the file /mnt/kd/ldap-odbc.sqlite3 which could store an address book that can be distributed via LDAP to IP Phones, mail app contacts, etc. and additionally be used within Asterisk using func_odbc.  Far more configuration and testing is required to prove this is useful. Also openldap debug is enabled for now on a temp basis.
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6212 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openldap/openldap.mk
+++ package/openldap/openldap.mk
@@ -15,19 +15,34 @@ OPENLDAP_UNINSTALL_STAGING_OPT = --version
 OPENLDAP_CONF_OPT = \
 	--enable-shared \
 	--disable-static \
-	--disable-debug \
+	--enable-debug \
 	--enable-syslog \
 	--enable-ipv6 \
 	--with-tls \
-	--with-yielding_select="yes" \
+	--with-odbc=unixodbc \
+	--with-yielding_select=yes \
 	--without-fetch \
 	--without-cyrus-sasl \
-	--disable-slapd \
+	--enable-slapd \
+	--enable-sql \
+	--enable-null \
 	--disable-local \
 	--disable-bdb \
 	--disable-hdb \
 	--disable-monitor \
 	--disable-relay
+
+ifeq ($(BR2_PACKAGE_OPENLDAP_SERVER),y)
+define OPENLDAP_INSTALL_TARGET_SERVER
+	cp -a $(STAGING_DIR)/etc/openldap/schema $(TARGET_DIR)/etc/openldap/
+	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/libexec/slapd $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/ldap* $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 -D package/openldap/slapd.init $(TARGET_DIR)/etc/init.d/slapd
+	ln -sf /tmp/etc/openldap/slapd.conf $(TARGET_DIR)/etc/openldap/slapd.conf
+	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/S45slapd
+	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/K16slapd
+endef
+endif
 
 define OPENLDAP_INSTALL_TARGET_CMDS
 	cp -a $(STAGING_DIR)/usr/lib/libldap*.so* $(TARGET_DIR)/usr/lib/
@@ -38,16 +53,20 @@ define OPENLDAP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/openldap
 	ln -sf /tmp/etc/openldap/ldap.conf $(TARGET_DIR)/etc/openldap/ldap.conf
 	ln -sf ../../init.d/ldap $(TARGET_DIR)/etc/runlevels/default/S00ldap
+	$(OPENLDAP_INSTALL_TARGET_SERVER)
 endef
 
 define OPENLDAP_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/lib/libldap*
 	rm -f $(TARGET_DIR)/usr/lib/liblber*
-	rm -f $(TARGET_DIR)/usr/bin/ldapsearch
-	rm -f $(TARGET_DIR)/usr/bin/ldapwhoami
+	rm -f $(TARGET_DIR)/usr/bin/ldap*
 	rm -f $(TARGET_DIR)/etc/init.d/ldap
+	rm -f $(TARGET_DIR)/etc/init.d/slapd
 	rm -rf $(TARGET_DIR)/etc/openldap
 	rm -f $(TARGET_DIR)/etc/runlevels/default/S00ldap
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S45slapd
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K16slapd
+	rm -f $(TARGET_DIR)/usr/sbin/slapd
 endef
 
 $(eval $(call AUTOTARGETS,package,openldap))
