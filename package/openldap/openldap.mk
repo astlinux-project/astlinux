86f8791ce5ad3fd12199fbe3d8302af39e017f04
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-10-09 04:39:01 +0000

    slapd, switch back to MDB backend, LDIF backend is too hackish ignoring access privileges
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6219 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openldap/openldap.mk
+++ package/openldap/openldap.mk
@@ -23,11 +23,11 @@ OPENLDAP_CONF_OPT = \
 	--without-fetch \
 	--without-cyrus-sasl \
 	--enable-slapd \
+	--enable-mdb \
 	--enable-null \
 	--disable-local \
 	--disable-bdb \
 	--disable-hdb \
-	--disable-mdb \
 	--disable-monitor \
 	--disable-relay
 
@@ -37,6 +37,7 @@ define OPENLDAP_INSTALL_TARGET_SERVER
 	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/libexec/slapd $(TARGET_DIR)/usr/sbin/
 	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/ldap* $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 -D package/openldap/slapd.init $(TARGET_DIR)/etc/init.d/slapd
+	$(INSTALL) -m 0444 -D package/openldap/schema/*.schema $(TARGET_DIR)/etc/openldap/schema/
 	ln -sf /tmp/etc/openldap/slapd.conf $(TARGET_DIR)/etc/openldap/slapd.conf
 	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/S45slapd
 	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/K16slapd
