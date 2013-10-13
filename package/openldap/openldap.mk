6c9a84008384e6e1dea946e063df83e421a9a4dc
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-10-13 13:55:28 +0000

    slapd, add 'vcard-export' script to generate ldif files from vcard files, suitable to 'ldapadd' into the addressbook
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6224 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openldap/openldap.mk
+++ package/openldap/openldap.mk
@@ -39,6 +39,7 @@ define OPENLDAP_INSTALL_TARGET_SERVER
 	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/bin/ldap* $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 -D package/openldap/slapd.init $(TARGET_DIR)/etc/init.d/slapd
 	$(INSTALL) -m 0444 -D package/openldap/schema/*.schema $(TARGET_DIR)/etc/openldap/schema/
+	$(INSTALL) -m 0755 -D package/openldap/vcard-export $(TARGET_DIR)/usr/sbin/
 	ln -sf /tmp/etc/openldap/slapd.conf $(TARGET_DIR)/etc/openldap/slapd.conf
 	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/S45slapd
 	ln -sf ../../init.d/slapd $(TARGET_DIR)/etc/runlevels/default/K16slapd
@@ -68,6 +69,7 @@ define OPENLDAP_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/etc/runlevels/default/S45slapd
 	rm -f $(TARGET_DIR)/etc/runlevels/default/K16slapd
 	rm -f $(TARGET_DIR)/usr/sbin/slapd
+	rm -f $(TARGET_DIR)/usr/sbin/vcard-export
 endef
 
 $(eval $(call AUTOTARGETS,package,openldap))
