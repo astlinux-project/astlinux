9753cb92309437f0542825e12404857b04a79355
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-10-25 17:47:16 +0000

    radvd, version bump to 1.9.5, mostly performance fixes
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6247 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -3,7 +3,7 @@
 # radvd
 #
 #############################################################
-RADVD_VERSION = 1.9.3
+RADVD_VERSION = 1.9.5
 RADVD_SOURCE = radvd-$(RADVD_VERSION).tar.gz
 RADVD_SITE = http://www.litech.org/radvd/dist/
 RADVD_DEPENDENCIES:=flex libdaemon host-flex host-pkg-config
