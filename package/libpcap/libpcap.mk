3bcfed3b2c092e7c6bd86218d90f9de14a57192d
Lonnie Abelbeck <lonnie@abelbeck.com>
2013-11-16 01:43:48 +0000

    libpcap / tcpdump version bumps to 1.4.0 / 4.4.0
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6281 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,24 +4,37 @@
 #
 #############################################################
 
-LIBPCAP_VERSION = 1.3.0
+LIBPCAP_VERSION = 1.4.0
 LIBPCAP_SITE = http://www.tcpdump.org/release
 LIBPCAP_SOURCE = libpcap-$(LIBPCAP_VERSION).tar.gz
 LIBPCAP_INSTALL_STAGING = YES
-
-# The configure.in file contains the necessary code to handle
-# --enable-shared/--disable-shared properly, but the configure script
-# bundled with the libpcap tarball is not up-to-date with the
-# configure.in file. In order to get proper handling of
-# --enable-shared/--disable-shared, which is needed when doing a
-# static library build, we autoreconfigure this package. This can be
-# removed when bumping libpcap, after checking that the bundled
-# configure script correctly handles --enable-shared/--disable-shared.
-LIBPCAP_AUTORECONF = YES
-
 LIBPCAP_DEPENDENCIES = zlib
+
+# We're patching configure.in
+LIBPCAP_AUTORECONF = YES
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
-		  ac_cv_header_linux_wireless_h=yes # configure misdetects this
+		ac_cv_header_linux_wireless_h=yes \
+		CFLAGS="$(LIBPCAP_CFLAGS)"
+LIBPCAP_CFLAGS = $(TARGET_CFLAGS)
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
+
+ifeq ($(BR2_PACKAGE_LIBUSB),y)
+LIBPCAP_CONF_OPT += --enable-canusb
+LIBPCAP_DEPENDENCIES += libusb
+else
+LIBPCAP_CONF_OPT += --disable-canusb
+endif
+
+ifeq ($(BR2_PACKAGE_LIBNL),y)
+LIBPCAP_DEPENDENCIES += libnl
+LIBPCAP_CFLAGS += "-I$(STAGING_DIR)/usr/include/libnl3"
+else
+LIBPCAP_CONF_OPT += --without-libnl
+endif
+
+define LIBPCAP_STAGING_FIXUP_PCAP_CONFIG
+        $(SED) "s,prefix=\"/usr\",prefix=\"$(STAGING_DIR)/usr\",g" $(STAGING_DIR)/usr/bin/pcap-config
+endef
+LIBPCAP_POST_INSTALL_STAGING_HOOKS += LIBPCAP_STAGING_FIXUP_PCAP_CONFIG
 
 $(eval $(call AUTOTARGETS,package,libpcap))
--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -6,7 +6,7 @@
 # Copyright (C) 2001-2003 by Erik Andersen <andersen@codepoet.org>
 # Copyright (C) 2002 by Tim Riker <Tim@Rikers.org>
 
-TCPDUMP_VERSION = 4.3.0
+TCPDUMP_VERSION = 4.4.0
 TCPDUMP_SITE = http://www.tcpdump.org/release
 TCPDUMP_SOURCE = tcpdump-$(TCPDUMP_VERSION).tar.gz
 
