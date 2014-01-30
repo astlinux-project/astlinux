8b903b2859eb996b1a220dea1f289f0edd9a73e8
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-01-30 20:22:09 +0000

    radvd, version bump to 1.9.8
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6388 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -3,7 +3,7 @@
 # radvd
 #
 #############################################################
-RADVD_VERSION = 1.9.7
+RADVD_VERSION = 1.9.8
 RADVD_SOURCE = radvd-$(RADVD_VERSION).tar.gz
 RADVD_SITE = http://www.litech.org/radvd/dist/
 RADVD_DEPENDENCIES:=flex libdaemon host-flex host-pkg-config
