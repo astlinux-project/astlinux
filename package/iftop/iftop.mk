11bcdd56e39c0582a3ba7eb68590061166f79143
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-02-04 19:22:09 +0000

    iftop, version bump to 1.0pre4, should be 1.0 soon, builds with autoconf now
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6403 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/iftop/iftop.mk
+++ package/iftop/iftop.mk
@@ -4,22 +4,9 @@
 #
 #############################################################
 
-IFTOP_VERSION = 1.0pre2
+IFTOP_VERSION = 1.0pre4
 IFTOP_SOURCE:=iftop-$(IFTOP_VERSION).tar.gz
-IFTOP_SITE = http://www.ex-parrot.com/~pdw/iftop/download
+IFTOP_SITE = http://www.ex-parrot.com/pdw/iftop/download
 IFTOP_DEPENDENCIES = libpcap ncurses
-
-IFTOP_MAKE_OPT = CC='$(TARGET_CC)' LD='$(TARGET_LD)' -C $(@D) iftop
-
-IFTOP_CONF_OPT += \
-	--prefix=/
-
-define IFTOP_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/iftop $(TARGET_DIR)/usr/bin/iftop
-endef
-
-define IFTOP_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/iftop
-endef
 
 $(eval $(call AUTOTARGETS,package,iftop))
