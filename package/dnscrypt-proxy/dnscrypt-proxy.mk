bbce8ac2209a5f393bcf49341f0a1f93947cda38
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-02-07 21:54:46 +0000

    New packages: libsodium and dnscrypt-proxy, both disabled by default.  Hooks added to dnsmasq to make it work
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6406 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/dnscrypt-proxy/dnscrypt-proxy.mk
@@ -0,0 +1,26 @@
+#############################################################
+#
+# dnscrypt-proxy
+#
+#############################################################
+DNSCRYPT_PROXY_VERSION = 1.3.3
+DNSCRYPT_PROXY_SOURCE = dnscrypt-proxy-$(DNSCRYPT_PROXY_VERSION).tar.gz
+DNSCRYPT_PROXY_SITE = http://download.dnscrypt.org/dnscrypt-proxy
+
+DNSCRYPT_PROXY_DEPENDENCIES += libsodium
+
+define DNSCRYPT_PROXY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/proxy/dnscrypt-proxy $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 0755 package/dnscrypt-proxy/dnscrypt-proxy.init $(TARGET_DIR)/etc/init.d/dnscrypt
+	ln -sf ../../init.d/dnscrypt $(TARGET_DIR)/etc/runlevels/default/S18dnscrypt
+	ln -sf ../../init.d/dnscrypt $(TARGET_DIR)/etc/runlevels/default/K15dnscrypt
+endef
+
+define DNSCRYPT_PROXY_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/dnscrypt-proxy
+	rm -f $(TARGET_DIR)/etc/init.d/dnscrypt
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S18dnscrypt
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K15dnscrypt
+endef
+
+$(eval $(call AUTOTARGETS,package,dnscrypt-proxy))
---
+++ package/libsodium/libsodium.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# libsodium
+#
+#############################################################
+LIBSODIUM_VERSION = 0.4.5
+LIBSODIUM_SOURCE = libsodium-$(LIBSODIUM_VERSION).tar.gz
+LIBSODIUM_SITE = http://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)
+
+LIBSODIUM_INSTALL_STAGING = YES
+
+define LIBSODIUM_INSTALL_TARGET_CMDS
+	cp -a $(STAGING_DIR)/usr/lib/libsodium.so* $(TARGET_DIR)/usr/lib/
+endef
+
+define LIBSODIUM_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/lib/libsodium.so*
+endef
+
+$(eval $(call AUTOTARGETS,package,libsodium))
