74125f0cdec80888cb0f3af53a792bec16ca6915
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-02-09 19:58:46 +0000

    libsodium, some wget's have trouble getting the filename correct using the github link
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6411 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -5,7 +5,8 @@
 #############################################################
 LIBSODIUM_VERSION = 0.4.5
 LIBSODIUM_SOURCE = libsodium-$(LIBSODIUM_VERSION).tar.gz
-LIBSODIUM_SITE = http://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)
+#LIBSODIUM_SITE = http://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)
+LIBSODIUM_SITE = http://files.astlinux.org
 
 LIBSODIUM_INSTALL_STAGING = YES
 
