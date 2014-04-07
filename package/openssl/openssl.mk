f7ea5d914c8035f42bf66fc46bf1a9fa0af21edf
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-04-07 22:57:17 +0000

    openssl, version bump to 1.0.1g, major security fix: CVE-2014-0160
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6491 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.1f
+OPENSSL_VERSION = 1.0.1g
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
