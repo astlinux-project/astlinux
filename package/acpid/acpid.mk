0ab25f377be0983af3a98c2044d0bcfa770c9194
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-04-29 16:41:14 +0000

    acpid, version bump to 2.0.22
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6509 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -3,21 +3,17 @@
 # acpid
 #
 #############################################################
-ACPID_VERSION = 2.0.17
-ACPID_SOURCE = acpid_$(ACPID_VERSION).orig.tar.gz
-ACPID_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/a/acpid
 
-define ACPID_INSTALL_TARGET_CMDS
-	install -D -m 755 $(@D)/acpid $(TARGET_DIR)/usr/sbin/acpid
-	install -D -m 755 $(@D)/acpi_listen $(TARGET_DIR)/usr/bin/acpi_listen
+ACPID_VERSION = 2.0.22
+ACPID_SOURCE = acpid-$(ACPID_VERSION).tar.xz
+ACPID_SITE = http://downloads.sourceforge.net/project/acpid2
+
+define ACPID_SET_EVENTS
 	mkdir -p $(TARGET_DIR)/etc/acpi/events
 	/bin/echo -e "event=button[ /]power\naction=/sbin/poweroff" > $(TARGET_DIR)/etc/acpi/events/powerbtn
-	install -D -m 755 package/acpid/acpid.init $(TARGET_DIR)/etc/init.d/acpid
+	$(INSTALL) -D -m 755 package/acpid/acpid.init $(TARGET_DIR)/etc/init.d/acpid
 endef
 
-define ACPID_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/sbin/acpid
-	rm -f $(TARGET_DIR)/usr/bin/acpi_listen
-endef
+ACPID_POST_INSTALL_TARGET_HOOKS += ACPID_SET_EVENTS
 
 $(eval $(call AUTOTARGETS,package,acpid))
