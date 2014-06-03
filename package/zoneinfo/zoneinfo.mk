bde638b1f573b3761aa94323e04d1090adcbd947
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-06-03 17:45:41 +0000

    zoneinfo, version bump to tzdata2014d and tzcode2014d
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6535 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/zoneinfo/zoneinfo.mk
+++ package/zoneinfo/zoneinfo.mk
@@ -4,8 +4,8 @@
 #
 ##############################################################
 ZONEINFO_VERSION :=
-ZONEINFO_DATA := tzdata2013h.tar.gz
-ZONEINFO_SOURCE := tzcode2013h.tar.gz
+ZONEINFO_DATA := tzdata2014d.tar.gz
+ZONEINFO_SOURCE := tzcode2014d.tar.gz
 ZONEINFO_SITE := ftp://ftp.iana.org/tz/releases
 ZONEINFO_DIR := $(BUILD_DIR)/zoneinfo
 ZONEINFO_BINARY := usr/share/zoneinfo/.tzcompiled
