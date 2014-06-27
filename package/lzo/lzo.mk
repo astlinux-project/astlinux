90f35a1fbfd5a91a85cadc4c41cba5cdc8fc3e7c
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-06-27 18:14:33 +0000

    lzo, version bump to 2.07, integer overflow security fix: CVE-2014-4608
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6569 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -3,9 +3,17 @@
 # lzo
 #
 #############################################################
-LZO_VERSION = 2.05
+LZO_VERSION = 2.07
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_INSTALL_STAGING = YES
+
+# Manually patch libtool
+LZO_LIBTOOL_PATCH = NO
+define LZO_LIBTOOL_PATCH_2.4.2
+	@echo "Patching libtool 2.4.2"
+	toolchain/patch-kernel.sh $(@D) package buildroot-libtool-v2.4.2.patch
+endef
+LZO_POST_PATCH_HOOKS += LZO_LIBTOOL_PATCH_2.4.2
 
 $(eval $(call AUTOTARGETS,package,lzo))
 $(eval $(call AUTOTARGETS,package,lzo,host))
