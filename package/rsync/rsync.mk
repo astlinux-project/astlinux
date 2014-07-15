c52c4984618c9d142ca643e5cc0a616de89ca36c
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-07-15 21:40:36 +0000

    rsync, version bump to 3.1.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6588 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,11 +4,19 @@
 #
 #############################################################
 
-RSYNC_VERSION = 3.0.9
+RSYNC_VERSION = 3.1.1
 RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
-RSYNC_INSTALL_STAGING = NO
-RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
-RSYNC_CONF_OPT = --with-included-popt
+RSYNC_DEPENDENCIES = zlib popt
+RSYNC_CONF_OPT = \
+	$(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug) \
+	--with-included-zlib=no \
+	--with-included-popt=no
+
+ifeq ($(BR2_PACKAGE_ACL),y)
+	RSYNC_DEPENDENCIES += acl
+else
+	RSYNC_CONF_OPT += --disable-acl-support
+endif
 
 $(eval $(call AUTOTARGETS,package,rsync))
