8f3183b13f0f894362f4b7172885e4984d09dfc0
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-07-16 23:44:56 +0000

    igb, new package, Intel Gigabit Ethernet Network Driver, including Intel i210 and i354 support
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6596 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/igb/igb.mk
@@ -0,0 +1,39 @@
+#############################################################
+#
+# igb
+#
+#############################################################
+
+IGB_VERSION = 5.2.5
+IGB_SOURCE:=igb-$(IGB_VERSION).tar.gz
+IGB_SITE = http://downloads.sourceforge.net/project/e1000/igb%20stable/$(IGB_VERSION)
+IGB_DEPENDENCIES = linux
+IGB_SUBDIR = src
+
+IGB_UNINSTALL_STAGING_OPT = --version
+
+IGB_MAKE_OPT += \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	BUILD_KERNEL=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR) \
+	RHEL_CODE=0 \
+	VERSION_FILE=$(LINUX_DIR)/include/generated/utsrelease.h \
+	CONFIG_FILE=$(LINUX_DIR)/include/generated/autoconf.h \
+	ARCH=$(KERNEL_ARCH)
+
+define IGB_CONFIGURE_CMDS
+	@echo "No configure"
+endef
+
+define IGB_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(@D)/src/igb.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/igb/igb.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+define IGB_UNINSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/igb/igb.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call AUTOTARGETS,package,igb))
