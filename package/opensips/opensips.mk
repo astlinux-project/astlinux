ff46428bb6c84ddac242fef7825dcd5619cf532a
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-08-24 00:16:10 +0000

    opensips, remove db_unixodbc module as it does not appear to work, as in seg-fault
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6653 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/opensips/opensips.mk
+++ package/opensips/opensips.mk
@@ -11,11 +11,6 @@ OPENSIPS_DEPENDENCIES = openssl
 
 OPENSIPS_INCLUDE_MODULES =
 
-ifeq ($(strip $(BR2_PACKAGE_UNIXODBC)),y)
-OPENSIPS_DEPENDENCIES += unixodbc
-OPENSIPS_INCLUDE_MODULES += db_unixodbc
-endif
-
 OPENSIPS_EXCLUDE_MODULES = siptrace sipcapture cachedb_couchbase cachedb_memcached cachedb_cassandra cachedb_redis \
 	cachedb_mongodb db_berkeley db_oracle db_perlvdb db_unixodbc event_rabbitmq identity jabber json ldap lua mi_xmlrpc osp \
 	perl perlvdb python h350 snmpstats sngtc aaa_radius carrierroute db_http db_mysql db_postgres mmgeoip httpd \
@@ -43,23 +38,23 @@ define OPENSIPS_BUILD_CMDS
 endef
 
 define OPENSIPS_INSTALL_TARGET_CMDS
-	$(INSTALL) -s -D -m 0755 $(@D)/opensips $(TARGET_DIR)/usr/sbin/opensips
+	$(INSTALL) -D -m 0755 $(@D)/opensips $(TARGET_DIR)/usr/sbin/opensips
 	# Install modules
 	(for i in `find $(@D)/modules -name \*.so`; \
-	do $(INSTALL) -s -D -m 0755 "$$i" $(TARGET_DIR)/usr/lib/opensips/modules/`basename "$$i"`; done)
+	do $(INSTALL) -D -m 0755 "$$i" $(TARGET_DIR)/usr/lib/opensips/modules/`basename "$$i"`; done)
 	# Install opensipsctl
-	$(INSTALL) -D -m 0640 $(@D)/scripts/opensipsctl.base $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.base
+	$(INSTALL) -D -m 0644 $(@D)/scripts/opensipsctl.base $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.base
+	$(INSTALL) -D -m 0644 $(@D)/scripts/opensipsctl.ctlbase $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.ctlbase
+	$(INSTALL) -D -m 0644 $(@D)/scripts/opensipsctl.fifo $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.fifo
 	$(SED) 's:/usr/local/etc/opensips:/etc/opensips:g' \
-		$(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.base
-	$(INSTALL) -D -m 0640 $(@D)/scripts/opensipsctl.ctlbase $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.ctlbase
-	$(INSTALL) -D -m 0640 $(@D)/scripts/opensipsctl.fifo $(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.fifo
+		$(TARGET_DIR)/usr/lib/opensips/opensipsctl/opensipsctl.*
 	$(INSTALL) -D -m 0755 $(@D)/scripts/opensipsctl $(TARGET_DIR)/usr/sbin/opensipsctl
 	$(SED) 's:/bin/sh:/bin/bash:' \
 	    -e 's:/usr/local/etc/opensips:/etc/opensips:g' \
 	    -e 's:/usr/local/lib/opensips:/usr/lib/opensips:g' \
 		$(TARGET_DIR)/usr/sbin/opensipsctl
-	$(INSTALL) -D -m 0640 $(@D)/scripts/opensipsctlrc $(TARGET_DIR)/stat/etc/opensips/opensipsctlrc
-	$(INSTALL) -D -m 0640 package/opensips/opensips.cfg $(TARGET_DIR)/stat/etc/opensips/opensips.cfg
+	$(INSTALL) -D -m 0644 $(@D)/scripts/opensipsctlrc $(TARGET_DIR)/stat/etc/opensips/opensipsctlrc
+	$(INSTALL) -D -m 0644 package/opensips/opensips.cfg $(TARGET_DIR)/stat/etc/opensips/opensips.cfg
 	$(INSTALL) -D -m 0755 package/opensips/opensips.init $(TARGET_DIR)/etc/init.d/opensips
 	ln -snf /tmp/etc/opensips $(TARGET_DIR)/etc/opensips
 	ln -sf ../../init.d/opensips $(TARGET_DIR)/etc/runlevels/default/S58opensips
@@ -69,7 +64,6 @@ endef
 define OPENSIPS_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/opensips
 	rm -rf $(TARGET_DIR)/usr/lib/opensips
-	rm -f $(TARGET_DIR)/usr/sbin/opensipsctl.base
 	rm -f $(TARGET_DIR)/usr/sbin/opensipsctl
 	rm -rf $(TARGET_DIR)/stat/etc/opensips
 	rm -f $(TARGET_DIR)/etc/init.d/opensips
