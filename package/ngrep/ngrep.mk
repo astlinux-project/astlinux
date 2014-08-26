ff7fb3ed4680821346b73658e8d9e576147fb9d7
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-08-26 15:03:22 +0000

    ngrep, build using the pcre library and install into /usr/bin/
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6667 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ngrep/ngrep.mk
+++ package/ngrep/ngrep.mk
@@ -7,22 +7,22 @@
 NGREP_VERSION = 1.45
 NGREP_SOURCE = ngrep-$(NGREP_VERSION).tar.bz2
 NGREP_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ngrep
-NGREP_DEPENDENCIES = libpcap
+NGREP_DEPENDENCIES = libpcap pcre
 
 NGREP_UNINSTALL_STAGING_OPT = --version
 
 NGREP_CONF_OPT += \
-	--with-pcap-includes=$(LIBPCAP_DIR) \
+	--with-pcap-includes=$(STAGING_DIR)/usr/include \
+	--with-pcre=$(STAGING_DIR)/usr \
+	--enable-pcre \
 	--enable-ipv6
 
-NGREP_MAKE_OPT = CC='$(TARGET_CC)' MAKEFLAGS="" -C $(@D)
-
 define NGREP_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/ngrep $(TARGET_DIR)/usr/sbin/ngrep
+	$(INSTALL) -m 0755 -D $(@D)/ngrep $(TARGET_DIR)/usr/bin/ngrep
 endef
 
 define NGREP_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/sbin/ngrep
+	rm -f $(TARGET_DIR)/usr/bin/ngrep
 endef
 
 $(eval $(call AUTOTARGETS,package,ngrep))
