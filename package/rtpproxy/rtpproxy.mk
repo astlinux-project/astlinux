fac3813c361db5039849983f9ddb6d7b6f8fd3af
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-08-26 13:48:43 +0000

    rtpproxy, add comment to makefile, shame github direct downloads are so messed-up
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6666 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/rtpproxy/rtpproxy.mk
+++ package/rtpproxy/rtpproxy.mk
@@ -6,6 +6,7 @@
 
 RTPPROXY_VERSION = 1.3-beta.1
 RTPPROXY_SITE = http://files.astlinux.org
+#RTPPROXY_SITE = https://github.com/sippy/rtpproxy/releases
 RTPPROXY_SOURCE = rtpproxy-$(RTPPROXY_VERSION).tar.gz
 
 $(eval $(call AUTOTARGETS,package,rtpproxy))
