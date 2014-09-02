8784406d50ca2c97a964ce62eed8f81d74bb3f34
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-09-02 22:32:11 +0000

    kamailio, change rtpproxy to Daniel-Constantin Mierla's version which suports the 'advertise' option which is set by the new variable RTPPROXY_ADVERTISE_IP Ref: https://github.com/miconda/rtpproxy
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6687 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/rtpproxy/rtpproxy.mk
+++ package/rtpproxy/rtpproxy.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-RTPPROXY_VERSION = 1.3-beta.1
+RTPPROXY_VERSION = 2013-07-02
 RTPPROXY_SITE = http://files.astlinux.org
-#RTPPROXY_SITE = https://github.com/sippy/rtpproxy/releases
+#RTPPROXY_SITE = https://github.com/miconda/rtpproxy
 RTPPROXY_SOURCE = rtpproxy-$(RTPPROXY_VERSION).tar.gz
 
 $(eval $(call AUTOTARGETS,package,rtpproxy))
