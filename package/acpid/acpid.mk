737c46edc8321b02136fb29efacf94b9761321e6
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-09-21 13:46:50 +0000

    acpid, version bump to 2.0.23
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6716 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ACPID_VERSION = 2.0.22
+ACPID_VERSION = 2.0.23
 ACPID_SOURCE = acpid-$(ACPID_VERSION).tar.xz
 ACPID_SITE = http://downloads.sourceforge.net/project/acpid2
 
