3ff073e5803a6b01d2b5b55b61ecfdaa4d74d51f
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-10-03 17:38:12 +0000

    bash, add build option BR2_PACKAGE_BASH_DEFAULT_SHELL=y whether to link /bin/sh to bash
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6745 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -18,22 +18,27 @@ ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 BASH_DEPENDENCIES += busybox
 endif
 
+ifeq ($(BR2_PACKAGE_BASH_DEFAULT_SHELL),y)
+define BASH_DEFAULT_SHELL
+	if [ -e $(TARGET_DIR)/bin/sh ]; then \
+		mv -f $(TARGET_DIR)/bin/sh $(TARGET_DIR)/bin/sh.prebash; \
+	fi
+	ln -sf bash $(TARGET_DIR)/bin/sh
+endef
+endif
+
 # Save the old sh file/link if there is one and symlink bash->sh
 define BASH_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
 		DESTDIR=$(TARGET_DIR) exec_prefix=/ install
 	rm -f $(TARGET_DIR)/bin/bashbug
-	if [ -e $(TARGET_DIR)/bin/sh ]; then \
-		mv -f $(TARGET_DIR)/bin/sh $(TARGET_DIR)/bin/sh.prebash; \
-	fi
-	ln -sf bash $(TARGET_DIR)/bin/sh
+	$(BASH_DEFAULT_SHELL)
 endef
 
 # Restore the old shell file/link if there was one
 define BASH_UNINSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) \
 		-C $(BASH_DIR) exec_prefix=/ uninstall
-	rm -f $(TARGET_DIR)/bin/sh
 	if [ -e $(TARGET_DIR)/bin/sh.prebash ]; then \
 		mv -f $(TARGET_DIR)/bin/sh.prebash $(TARGET_DIR)/bin/sh; \
 	fi
