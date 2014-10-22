d7bc8f564e7e24b4838c8f871b856e4cb6ba8e27
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-10-22 20:06:49 +0000

    fop2, version bump to 2.28, add 'service fop2 test' and automaticaly copy any fop2.lic file
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6775 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/asterisk-fop2/asterisk-fop2.mk
+++ package/asterisk-fop2/asterisk-fop2.mk
@@ -3,7 +3,7 @@
 # asterisk-fop2
 #
 #############################################################
-ASTERISK_FOP2_VERSION = 2.27
+ASTERISK_FOP2_VERSION = 2.28
 ASTERISK_FOP2_SOURCE = fop2-$(ASTERISK_FOP2_VERSION)-debian-i386.tgz
 ASTERISK_FOP2_SITE = http://download.fop2.com
 # Note: be sure to edit "project/astlinux/target_skeleton/usr/sbin/upgrade-package" on version change
@@ -13,6 +13,8 @@ define ASTERISK_FOP2_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/fop2.cfg $(TARGET_DIR)/stat/etc/fop2/fop2.cfg
 	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/buttons.cfg $(TARGET_DIR)/stat/etc/fop2/buttons.cfg
 	$(INSTALL) -D -m 0644 package/asterisk-fop2/config/users.cfg $(TARGET_DIR)/stat/etc/fop2/users.cfg
+	mkdir -p $(TARGET_DIR)/usr/local
+	ln -snf /stat/var/packages/fop2/server $(TARGET_DIR)/usr/local/fop2
 	ln -snf /tmp/etc/fop2 $(TARGET_DIR)/etc/fop2
 	ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/S98fop2
 	ln -sf ../../init.d/fop2 $(TARGET_DIR)/etc/runlevels/default/K00fop2
