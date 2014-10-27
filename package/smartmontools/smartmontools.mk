fcd9bd8ffb963a8bfe5135c91226a62e150c29c8
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-10-27 23:19:02 +0000

    smartmontools, now enabled but only install 'smartctl'
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6784 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,17 @@
 #
 #############################################################
 
-SMARTMONTOOLS_VERSION = 5.40
-SMARTMONTOOLS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/smartmontools
+SMARTMONTOOLS_VERSION = 6.3
+SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
+
+define SMARTMONTOOLS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/smartctl $(TARGET_DIR)/usr/sbin/smartctl
+	ln -sf /tmp/etc/smart_drivedb.h $(TARGET_DIR)/etc/smart_drivedb.h
+endef
+
+define SMARTMONTOOLS_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/smartctl
+	rm -f $(TARGET_DIR)/etc/smart_drivedb.h
+endef
 
 $(eval $(call AUTOTARGETS,package,smartmontools))
