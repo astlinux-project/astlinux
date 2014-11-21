94b9f543a1f2d54ac6375dd0d489e030b15af8c8
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-11-21 14:48:13 +0000

    asterisk, version bump to 1.8.32.1 and 11.14.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6830 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -4,9 +4,9 @@
 #
 ##############################################################
 ifeq ($(BR2_PACKAGE_ASTERISK_v1_8),y)
-ASTERISK_VERSION := 1.8.32.0
+ASTERISK_VERSION := 1.8.32.1
 else
-ASTERISK_VERSION := 11.14.0
+ASTERISK_VERSION := 11.14.1
 endif
 ASTERISK_SOURCE := asterisk-$(ASTERISK_VERSION).tar.gz
 ASTERISK_SITE := http://downloads.asterisk.org/pub/telephony/asterisk/releases
