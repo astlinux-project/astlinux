83235240dfc3a0c3c791cd815d457bbbe4999d61
Lonnie Abelbeck <lonnie@abelbeck.com>
2014-11-24 16:36:12 +0000

    libsodium, version bump to 1.0.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6832 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -3,7 +3,7 @@
 # libsodium
 #
 #############################################################
-LIBSODIUM_VERSION = 1.0.0
+LIBSODIUM_VERSION = 1.0.1
 LIBSODIUM_SOURCE = libsodium-$(LIBSODIUM_VERSION).tar.gz
 LIBSODIUM_SITE = http://download.dnscrypt.org/libsodium/releases
 
