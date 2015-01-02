203f0af98dfe0c1e2ba31a780baba7576e6c9c2f
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-02 18:31:43 +0000

    jansson, new package, required for Asterisk 13
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6895 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/jansson/jansson.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# jansson
+#
+################################################################################
+
+JANSSON_VERSION = 2.7
+JANSSON_SITE = http://www.digip.org/jansson/releases
+JANSSON_INSTALL_STAGING = YES
+
+JANSSON_CONF_ENV = LIBS="-lm"
+
+$(eval $(call AUTOTARGETS,package,jansson))
