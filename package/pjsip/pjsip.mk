169f887f834ea9a09b645249204eca9eed3b03f3
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-03 17:08:52 +0000

    pjsip, new package for Asterisk 13, version 2.3
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6898 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/pjsip/pjsip.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# pjsip
+#
+################################################################################
+
+PJSIP_VERSION = 2.3
+PJSIP_SOURCE = pjproject-$(PJSIP_VERSION).tar.bz2
+PJSIP_SITE = http://www.pjsip.org/release/$(PJSIP_VERSION)
+PJSIP_INSTALL_STAGING = YES
+
+PJSIP_DEPENDENCIES = libsrtp
+
+PJSIP_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) -O2 -DPJ_HAS_IPV6=1 -DNDEBUG" \
+	LDFLAGS="-L$(STAGING_DIR)/usr/lib" \
+	LD="$(TARGET_CC)"
+
+PJSIP_CONF_OPT = \
+	--with-external-srtp \
+	--disable-sound \
+	--disable-resample \
+	--disable-video \
+	--disable-opencore-amr
+
+$(eval $(call AUTOTARGETS,package,pjsip))
