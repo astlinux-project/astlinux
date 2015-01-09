9a67dc9dba470a99c203fdc6d34233d5f8d4c208
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-09 04:48:31 +0000

    dnscrypt-proxy, version bump to 1.4.3
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6913 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/dnscrypt-proxy/dnscrypt-proxy.mk
+++ package/dnscrypt-proxy/dnscrypt-proxy.mk
@@ -3,7 +3,7 @@
 # dnscrypt-proxy
 #
 #############################################################
-DNSCRYPT_PROXY_VERSION = 1.4.2
+DNSCRYPT_PROXY_VERSION = 1.4.3
 DNSCRYPT_PROXY_SOURCE = dnscrypt-proxy-$(DNSCRYPT_PROXY_VERSION).tar.gz
 DNSCRYPT_PROXY_SITE = http://download.dnscrypt.org/dnscrypt-proxy
 
