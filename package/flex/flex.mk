dff09e8e35ae9ffb62e0f3ffd2bea8edf6e04113
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-09 17:37:39 +0000

    host-flex, version bump to 2.5.37 and cleanup
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6917 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -3,42 +3,26 @@
 # flex
 #
 #############################################################
-FLEX_VERSION:=2.5.35
-FLEX_PATCH_VERSION:=10
-FLEX_SOURCE:=flex_$(FLEX_VERSION).orig.tar.gz
-FLEX_PATCH:=flex_$(FLEX_VERSION)-$(FLEX_PATCH_VERSION).diff.gz
-FLEX_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/f/flex
-FLEX_DIR:=$(BUILD_DIR)/flex-$(FLEX_VERSION)
+FLEX_VERSION = 2.5.37
+FLEX_SOURCE = flex-$(FLEX_VERSION).tar.gz
+FLEX_SITE = http://download.sourceforge.net/project/flex
 FLEX_INSTALL_STAGING=YES
-FLEX_DEPENDENCIES = \
-	$(if $(BR2_PACKAGE_GETTEXT),gettext) \
-	$(if $(BR2_PACKAGE_LIBINTL),libintl)
+FLEX_DEPENDENCIES = host-m4
+HOST_FLEX_DEPENDENCIES = host-m4
 
-# lex -> flex
-define FLEX_INSTALL_LEX
-cd $(TARGET_DIR)/usr/bin && ln -snf flex lex
+FLEX_CONF_ENV = ac_cv_path_M4=$(HOST_DIR)/usr/bin/m4
+HOST_FLEX_CONF_ENV = ac_cv_path_M4=$(HOST_DIR)/usr/bin/m4
+
+define FLEX_DISABLE_PROGRAM
+	$(SED) 's/^bin_PROGRAMS.*//' $(@D)/Makefile.in
 endef
+FLEX_POST_PATCH_HOOKS += FLEX_DISABLE_PROGRAM
 
-define FLEX_UNINSTALL_LEX
--rm $(TARGET_DIR)/usr/bin/lex
+# flex++ symlink is broken when flex binary is not installed
+define FLEX_REMOVE_BROKEN_SYMLINK
+	rm -f $(TARGET_DIR)/usr/bin/flex++
 endef
-
-FLEX_POST_INSTALL_HOOKS += FLEX_INSTALL_LEX
-FLEX_POST_CLEAN_HOOKS += FLEX_UNINSTALL_LEX
-
-# libfl installation
-ifeq ($(BR2_PACKAGE_FLEX_LIBFL),y)
-define FLEX_INSTALL_LIBFL
-install -D $(FLEX_DIR)/libfl.a $(STAGING_DIR)/usr/lib/libfl.a
-endef
-
-define FLEX_UNINSTALL_LIBFL
--rm $(STAGING_DIR)/lib/libfl.a
-endef
-
-FLEX_POST_INSTALL_HOOKS += FLEX_INSTALL_LIBFL
-FLEX_POST_CLEAN_HOOKS += FLEX_UNINSTALL_LIBFL
-endif
+FLEX_POST_INSTALL_TARGET_HOOKS += FLEX_REMOVE_BROKEN_SYMLINK
 
 $(eval $(call AUTOTARGETS,package,flex))
 $(eval $(call AUTOTARGETS,package,flex,host))
