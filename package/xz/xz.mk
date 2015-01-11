4b2ad96ec1b31345d01bb394dead4f84a12f1e52
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-11 14:31:03 +0000

    xz, add threads support
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6922 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -9,5 +9,11 @@ XZ_SITE = http://tukaani.org/xz
 XZ_INSTALL_STAGING = YES
 XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
+XZ_CONF_OPT = --enable-threads
+else
+XZ_CONF_OPT = --disable-threads
+endif
+
 $(eval $(call AUTOTARGETS,package,xz))
 $(eval $(call AUTOTARGETS,package,xz,host))
