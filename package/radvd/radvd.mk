8808078f8aaacdc6e39e7dd701466a0f6036a0fb
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-13 19:23:42 +0000

    retire radvd, use dnsmasq for IPv6 router advertisements
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6931 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -13,8 +13,17 @@ RADVD_AUTORECONF = YES
 define RADVD_INSTALL_INITSCRIPT
 	$(INSTALL) -D -m 0755 package/radvd/radvd.init $(TARGET_DIR)/etc/init.d/radvd
 	ln -sf /tmp/etc/radvd.conf $(TARGET_DIR)/etc/radvd.conf
+	ln -sf ../../init.d/radvd $(TARGET_DIR)/etc/runlevels/default/S92radvd
+	ln -sf ../../init.d/radvd $(TARGET_DIR)/etc/runlevels/default/K05radvd
 endef
 
 RADVD_POST_INSTALL_TARGET_HOOKS += RADVD_INSTALL_INITSCRIPT
+
+define RADVD_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/etc/init.d/radvd
+	rm -f $(TARGET_DIR)/etc/radvd.conf
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S92radvd
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K05radvd
+endef
 
 $(eval $(call AUTOTARGETS,package,radvd))
