b47ef40e19725f8520a8c073e4e9678d8efb3877
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-23 22:25:06 +0000

    host-bison, version bump to 3.0.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6936 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,8 +4,17 @@
 #
 #############################################################
 
-BISON_VERSION = 2.4.3
+BISON_VERSION = 3.0.2
+BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
+BISON_AUTORECONF = YES
+HOST_BISON_AUTORECONF = YES
+
+BISON_DEPENDENCIES = m4
+BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
+
+HOST_BISON_DEPENDENCIES = host-m4
+HOST_BISON_CONF_ENV = ac_cv_path_M4=$(HOST_DIR)/usr/bin/m4
 
 define BISON_DISABLE_EXAMPLES
 	echo 'all install:' > $(@D)/examples/Makefile
