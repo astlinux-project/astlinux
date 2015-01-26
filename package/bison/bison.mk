17eae85895029fba732844519d57722b6de8caa5
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-01-26 02:43:37 +0000

    host-bison, version bump to 3.0.3
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6942 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,23 +4,11 @@
 #
 #############################################################
 
-BISON_VERSION = 3.0.2
+BISON_VERSION = 3.0.3
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
-BISON_AUTORECONF = YES
-HOST_BISON_AUTORECONF = YES
-
-BISON_DEPENDENCIES = m4
-BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
 
 HOST_BISON_DEPENDENCIES = host-m4
 HOST_BISON_CONF_ENV = ac_cv_path_M4=$(HOST_DIR)/usr/bin/m4
 
-define BISON_DISABLE_EXAMPLES
-	echo 'all install:' > $(@D)/examples/Makefile
-endef
-
-BISON_POST_CONFIGURE_HOOKS += BISON_DISABLE_EXAMPLES
-
-$(eval $(call AUTOTARGETS,package,bison))
 $(eval $(call AUTOTARGETS,package,bison,host))
