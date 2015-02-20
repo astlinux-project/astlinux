07f50434c762ed9bd876ad217897519403fccf96
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-02-20 18:32:02 +0000

    sudo, version bump to 1.8.12, security fix: CVE-2014-9680
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6981 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,22 +4,26 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.8.6p7
-SUDO_SITE    = http://www.sudo.ws/sudo/dist
+SUDO_VERSION = 1.8.12
+SUDO_SITE = http://www.sudo.ws/sudo/dist
+
+# This is to avoid sudo's make install from chown()ing files which fails
+SUDO_INSTALL_TARGET_OPT = INSTALL_OWNER="" DESTDIR="$(TARGET_DIR)" install
 
 SUDO_CONF_OPT = \
-		--without-lecture \
-		--without-sendmail \
-		--without-umask \
-		--with-logging=syslog \
-		--without-interfaces \
-		--without-pam
+	--without-lecture \
+	--without-sendmail \
+	--without-umask \
+	--with-logging=syslog \
+	--without-interfaces \
+	--without-pam \
+	--with-env-editor
 
 # mksigname/mksiglist needs to run on build host to generate source files
 define SUDO_BUILD_MKSIGNAME_MKSIGLIST_HOST
 	$(MAKE) $(HOST_CONFIGURE_OPTS) \
-		CPPFLAGS="$(HOST_CPPFLAGS) -I../include -I.." \
-		-C $(@D)/compat mksigname mksiglist
+		CPPFLAGS="$(HOST_CPPFLAGS) -I../../include -I../.." \
+		-C $(@D)/lib/util mksigname mksiglist
 endef
 
 SUDO_POST_CONFIGURE_HOOKS += SUDO_BUILD_MKSIGNAME_MKSIGLIST_HOST
