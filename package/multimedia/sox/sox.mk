84cad2c2d30ac33d28fb6c0b50488f91cca41f64
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-03-08 16:40:05 +0000

    sox, version bump to 14.4.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@6999 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/multimedia/sox/sox.mk
+++ package/multimedia/sox/sox.mk
@@ -4,24 +4,87 @@
 #
 #############################################################
 
-SOX_VERSION = 14.4.1
-SOX_SOURCE:=sox-$(SOX_VERSION).tar.gz
-SOX_SITE = http://downloads.sourceforge.net/sourceforge/sox
+SOX_VERSION = 14.4.2
+SOX_SITE = http://downloads.sourceforge.net/project/sox/sox/$(SOX_VERSION)
+SOX_SOURCE = sox-$(SOX_VERSION).tar.bz2
 SOX_INSTALL_STAGING = YES
 
+SOX_DEPENDENCIES = host-pkg-config
+
 SOX_CONF_OPT = \
+	--with-distro="Buildroot" \
+	--disable-openmp \
 	--without-libltdl \
-	--without-flac \
-	--without-ffmpeg \
-	--without-ladspa \
-	--without-lame \
-	--without-id3tag
+	--without-ladspa
+
+SOX_CONF_OPT += --without-alsa
+
+ifeq ($(BR2_PACKAGE_FILE),y)
+SOX_DEPENDENCIES += file
+else
+SOX_CONF_OPT += --without-magic
+endif
+
+ifeq ($(BR2_PACKAGE_FLAC),y)
+SOX_DEPENDENCIES += flac
+else
+SOX_CONF_OPT += --without-flac
+endif
+
+ifeq ($(BR2_PACKAGE_LAME),y)
+SOX_DEPENDENCIES += lame
+else
+SOX_CONF_OPT += --without-lame
+endif
+
+ifeq ($(BR2_PACKAGE_LIBAO),y)
+SOX_DEPENDENCIES += libao
+else
+SOX_CONF_OPT += --without-ao
+endif
+
+ifeq ($(BR2_PACKAGE_LIBID3TAG),y)
+SOX_DEPENDENCIES += libid3tag
+else
+SOX_CONF_OPT += --without-id3tag
+endif
 
 ifeq ($(BR2_PACKAGE_LIBMAD),y)
-	SOX_DEPENDENCIES += libmad
-	SOX_CONF_OPT += --with-mad
+SOX_DEPENDENCIES += libmad
 else
-	SOX_CONF_OPT += --without-mad
+SOX_CONF_OPT += --without-mad
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPNG),y)
+SOX_DEPENDENCIES += libpng
+else
+SOX_CONF_OPT += --without-png
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
+SOX_DEPENDENCIES += libsndfile
+else
+SOX_CONF_OPT += --without-sndfile
+endif
+
+ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
+SOX_DEPENDENCIES += libvorbis
+else
+SOX_CONF_OPT += --without-oggvorbis
+endif
+
+SOX_CONF_OPT += --without-amrwb --without-amrnb
+
+SOX_CONF_OPT += --without-opus
+
+SOX_CONF_OPT += --without-pulseaudio
+
+SOX_CONF_OPT += --without-twolame
+
+ifeq ($(BR2_PACKAGE_WAVPACK),y)
+SOX_DEPENDENCIES += wavpack
+else
+SOX_CONF_OPT += --without-wavpack
 endif
 
 define SOX_INSTALL_TARGET_CMDS
