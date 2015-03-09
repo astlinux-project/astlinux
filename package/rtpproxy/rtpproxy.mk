39f03a721ae3cb9c1807c0337103355b626c9a11
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-03-09 23:29:41 +0000

    rtpproxy, version bump to 2.0.0, only used with kamailio
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7005 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/rtpproxy/rtpproxy.mk
+++ package/rtpproxy/rtpproxy.mk
@@ -4,9 +4,16 @@
 #
 #############################################################
 
-RTPPROXY_VERSION = 2013-07-02
+RTPPROXY_VERSION = 2.0.0
 RTPPROXY_SITE = http://files.astlinux.org
-#RTPPROXY_SITE = https://github.com/miconda/rtpproxy
+#RTPPROXY_SITE = https://github.com/sippy/rtpproxy
 RTPPROXY_SOURCE = rtpproxy-$(RTPPROXY_VERSION).tar.gz
+
+RTPPROXY_AUTORECONF = YES
+
+define RTPPROXY_TARGET_CLEANUP
+	rm -f $(TARGET_DIR)/usr/bin/rtpproxy_debug
+endef
+RTPPROXY_POST_INSTALL_TARGET_HOOKS += RTPPROXY_TARGET_CLEANUP
 
 $(eval $(call AUTOTARGETS,package,rtpproxy))
