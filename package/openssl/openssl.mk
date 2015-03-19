0fb14d6ddfd32ee435ce3d40c91ee53ac40d3263
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-03-19 17:18:25 +0000

    openssl, version bump to 1.0.1m, Moderate and Low severity security fixes: CVE-2015-0286, CVE-2015-0287, CVE-2015-0289, CVE-2015-0292, CVE-2015-0293, CVE-2015-0209, CVE-2015-0288
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7010 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.1k
+OPENSSL_VERSION = 1.0.1m
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
