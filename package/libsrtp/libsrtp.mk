a4272b87fd5951245eb77bbfa799278ea9f4aa64
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-04-27 15:22:32 +0000

    libsrtp, version bump to 1.5.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7054 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libsrtp/libsrtp.mk
+++ package/libsrtp/libsrtp.mk
@@ -3,12 +3,12 @@
 # libsrtp
 #
 #############################################################
-LIBSRTP_VERSION = 1.5.0
+LIBSRTP_VERSION = 1.5.2
 LIBSRTP_SITE = http://files.astlinux.org
 #LIBSRTP_SITE = https://github.com/cisco/libsrtp/releases
 LIBSRTP_SOURCE = libsrtp-$(LIBSRTP_VERSION).tar.gz
 LIBSRTP_INSTALL_STAGING = YES
-LIBSRTP_DEPENDENCIES = openssl
+LIBSRTP_DEPENDENCIES = host-pkg-config openssl
 
 LIBSRTP_CONF_OPT = \
 	--prefix=/usr \
@@ -17,14 +17,12 @@ LIBSRTP_CONF_OPT = \
 	--disable-stdout \
 	--disable-debug
 
-LIBSRTP_MAKE_OPT = \
-	CC='$(TARGET_CC)' \
-	LD='$(TARGET_LD)' \
-	CFLAGS='$(TARGET_CFLAGS) -fPIC' \
-	-C $(@D) libsrtp.so
+LIBSRTP_MAKE_OPT = shared_library
 
 define LIBSRTP_INSTALL_TARGET_CMDS
-	cp -a $(STAGING_DIR)/usr/lib/libsrtp.so* $(TARGET_DIR)/usr/lib/
+	# Must install from build directory since Makefile does not properly copy symlinks
+	# cp -a $(STAGING_DIR)/usr/lib/libsrtp.so* $(TARGET_DIR)/usr/lib/
+	cp -a $(@D)/libsrtp.so* $(TARGET_DIR)/usr/lib/
 endef
 
 define LIBSRTP_UNINSTALL_TARGET_CMDS
