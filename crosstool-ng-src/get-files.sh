03c8eeb1ea7b4ca415258646ef9afa57fdd74eb1
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-05-08 01:20:02 +0000

    crosstool-ng, tweak toolchain instructions to workaround broken svn server at eglibc.org
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7064 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ crosstool-ng-src/get-files.sh
@@ -0,0 +1,24 @@
+# shell script to add additional files
+
+FILES_URL="http://files.astlinux.org"
+
+TARBALLS=".build/tarballs"
+
+LINUX_KERNEL="linux-3.2.66.tar.gz"
+
+EGLIBC="eglibc-2_18.tar.bz2"
+
+if [ ! -f "$LINUX_KERNEL" ]; then
+  echo "Downloading $FILES_URL/$LINUX_KERNEL ..."
+  wget "$FILES_URL/$LINUX_KERNEL"
+fi
+
+if [ ! -f "$TARBALLS/$EGLIBC" ]; then
+  echo "Downloading $FILES_URL/$EGLIBC ..."
+  wget "$FILES_URL/$EGLIBC"
+
+  mkdir -p "$TARBALLS"
+  mv "$EGLIBC" "$TARBALLS/$EGLIBC"
+fi
+
+exit 0
