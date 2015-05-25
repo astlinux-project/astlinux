9af3b418adc37a00589942f0a7cf4606ad41c738
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-05-25 22:38:50 +0000

    libpcap / tcpdump version bumps to 1.7.3 / 4.7.4
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7078 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBPCAP_VERSION = 1.7.2
+LIBPCAP_VERSION = 1.7.3
 LIBPCAP_SITE = http://www.tcpdump.org/release
 LIBPCAP_SOURCE = libpcap-$(LIBPCAP_VERSION).tar.gz
 LIBPCAP_INSTALL_STAGING = YES
--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-TCPDUMP_VERSION = 4.7.3
+TCPDUMP_VERSION = 4.7.4
 TCPDUMP_SITE = http://www.tcpdump.org/release
 TCPDUMP_SOURCE = tcpdump-$(TCPDUMP_VERSION).tar.gz
 
