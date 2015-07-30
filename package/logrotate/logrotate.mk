6227b7eaa30150a8a933f0c53118f0af2c7fe712
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-07-30 16:58:17 +0000

    logrotate, version bump to 3.9.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7167 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -3,31 +3,35 @@
 # logrotate
 #
 #############################################################
-LOGROTATE_VERSION = 3.8.7
+LOGROTATE_VERSION = 3.9.1
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.gz
 LOGROTATE_SITE = https://www.fedorahosted.org/releases/l/o/logrotate
 
-LOGROTATE_DEPENDENCIES = popt
+LOGROTATE_DEPENDENCIES = popt host-pkg-config
 
-define LOGROTATE_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC) $(TARGET_CFLAGS)" LDFLAGS="$(LDFLAGS)" -C $(@D)
-endef
+# tarball does not have a generated configure script
+LOGROTATE_AUTORECONF = YES
 
-define LOGROTATE_INSTALL_TARGET_CMDS
-	$(MAKE) PREFIX=$(TARGET_DIR) -C $(@D) install
+LOGROTATE_CONF_OPT = --without-selinux
+
+ifeq ($(BR2_PACKAGE_ACL),y)
+LOGROTATE_DEPENDENCIES += acl
+LOGROTATE_CONF_OPT += --with-acl
+else
+LOGROTATE_CONF_OPT += --without-acl
+endif
+
+define LOGROTATE_INSTALL_TARGET_CONF
 	$(INSTALL) -m 0644 package/logrotate/logrotate.conf $(TARGET_DIR)/etc/logrotate.conf
 	if [ ! -d $(TARGET_DIR)/etc/logrotate.d ]; then \
 		$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/logrotate.d; \
 	fi
 endef
+LOGROTATE_POST_INSTALL_TARGET_HOOKS += LOGROTATE_INSTALL_TARGET_CONF
 
 define LOGROTATE_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/logrotate
 	rm -f $(TARGET_DIR)/etc/logrotate.conf
 endef
 
-define LOGROTATE_CLEAN_CMDS
-	-$(MAKE) -C $(@D) clean
-endef
-
-$(eval $(call GENTARGETS,package,logrotate))
+$(eval $(call AUTOTARGETS,package,logrotate))
