9dc64708af096808ac2097a93905913e3735184a
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-08-15 21:55:36 +0000

    host-tcl, version bump to 8.6.3
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7194 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/tcl/tcl.mk
+++ package/tcl/tcl.mk
@@ -1,27 +1,93 @@
-#############################################################
+################################################################################
 #
-# TCL8.4
+# tcl
 #
-#############################################################
-TCL_VERSION:=8.4.19
-TCL_SOURCE:=tcl$(TCL_VERSION)-src.tar.gz
-TCL_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/tcl
+################################################################################
+
+TCL_VERSION_MAJOR = 8.6
+TCL_VERSION = $(TCL_VERSION_MAJOR).3
+TCL_SOURCE = tcl$(TCL_VERSION)-src.tar.gz
+TCL_SITE = http://downloads.sourceforge.net/project/tcl/Tcl/$(TCL_VERSION)
 TCL_SUBDIR = unix
+TCL_INSTALL_STAGING = YES
+TCL_AUTORECONF = YES
+
+# Note that --with-system-sqlite will only make a difference
+# in the sqlite package (which gets removed if sqlite not
+# configured).  Don't need to worry about conditionally including
+# it in the configure options
 TCL_CONF_OPT = \
-		--disable-symbols \
-		--disable-langinfo \
-		--disable-framework
+	--disable-symbols \
+	--disable-langinfo \
+	--disable-framework \
+	--with-system-sqlite
 
-define TCL_POST_INSTALL_CLEANUP
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libtcl8.4.so
-	-if [ "$(BR2_PACKAGE_TCL_DEL_ENCODINGS)" = "y" ]; then \
-	rm -Rf $(TARGET_DIR)/usr/lib/tcl8.4/encoding/*; \
-	fi
-	-if [ "$(BR2_PACKAGE_TCL_SHLIB_ONLY)" = "y" ]; then \
-	rm -f $(TARGET_DIR)/usr/bin/tclsh8.4; \
-	fi
+HOST_TCL_SUBDIR = unix
+
+HOST_TCL_CONF_OPT = \
+	--disable-symbols \
+	--disable-langinfo \
+	--disable-framework
+
+# I haven't found a good way to force pkgs to not build
+# or configure without just removing the entire pkg directory.
+define HOST_TCL_REMOVE_PACKAGES
+	rm -fr $(@D)/pkgs/sqlite[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbc[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcmysql[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcodbc[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcpostgres[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcsqlite3-[0-9].[0-9].[0-9]
 endef
+HOST_TCL_PRE_CONFIGURE_HOOKS += HOST_TCL_REMOVE_PACKAGES
+define TCL_REMOVE_PACKAGES
+	rm -fr $(if $(BR2_PACKAGE_SQLITE),,$(@D)/pkgs/sqlite[0-9].[0-9].[0-9]) \
+		$(if $(BR2_PACKAGE_SQLITE),,$(@D)/pkgs/tdbc[0-9].[0-9].[0-9]) \
+		$(@D)/pkgs/tdbcmysql[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcodbc[0-9].[0-9].[0-9] \
+		$(@D)/pkgs/tdbcpostgres[0-9].[0-9].[0-9] \
+		$(if $(BR2_PACKAGE_SQLITE),,$(@D)/pkgs/tdbcsqlite3-[0-9].[0-9].[0-9])
+endef
+TCL_PRE_CONFIGURE_HOOKS += TCL_REMOVE_PACKAGES
 
-TCL_POST_INSTALL_TARGET_HOOKS += TCL_POST_INSTALL_CLEANUP
+ifeq ($(BR2_PACKAGE_TCL_DEL_ENCODINGS),y)
+define TCL_REMOVE_ENCODINGS
+	rm -rf $(TARGET_DIR)/usr/lib/tcl$(TCL_VERSION_MAJOR)/encoding/*
+endef
+TCL_POST_INSTALL_TARGET_HOOKS += TCL_REMOVE_ENCODINGS
+endif
+
+ifeq ($(BR2_PACKAGE_TCL_SHLIB_ONLY),y)
+define TCL_REMOVE_TCLSH
+	rm -f $(TARGET_DIR)/usr/bin/tclsh$(TCL_VERSION_MAJOR)
+endef
+TCL_POST_INSTALL_TARGET_HOOKS += TCL_REMOVE_TCLSH
+else
+define TCL_SYMLINK_TCLSH
+	ln -sf tclsh$(TCL_VERSION_MAJOR) $(TARGET_DIR)/usr/bin/tclsh
+endef
+TCL_POST_INSTALL_TARGET_HOOKS += TCL_SYMLINK_TCLSH
+endif
+
+define HOST_TCL_SYMLINK_TCLSH
+	ln -sf tclsh$(TCL_VERSION_MAJOR) $(HOST_DIR)/usr/bin/tclsh
+endef
+HOST_TCL_POST_INSTALL_HOOKS += HOST_TCL_SYMLINK_TCLSH
+
+# Until someone needs it, we don't handle locale installation.  tcl has
+# a complicated method of translating LANG-style locale names into its internal
+# .msg name which makes it difficult to save the correct locales per the
+# configured whitelist.
+define TCL_REMOVE_EXTRA
+	rm -fr $(TARGET_DIR)/usr/lib/tclConfig.sh \
+		$(TARGET_DIR)/usr/lib/tclooConfig.sh \
+		$(TARGET_DIR)/usr/lib/tcl$(TCL_VERSION_MAJOR)/tclAppInit.c \
+		$(TARGET_DIR)/usr/lib/tcl$(TCL_VERSION_MAJOR)/msgs
+endef
+TCL_POST_INSTALL_TARGET_HOOKS += TCL_REMOVE_EXTRA
+
+TCL_DEPENDENCIES = $(if $(BR2_PACKAGE_SQLITE),sqlite)
+HOST_TCL_DEPENDENCIES =
 
 $(eval $(call AUTOTARGETS,package,tcl))
+$(eval $(call AUTOTARGETS,package,tcl,host))
