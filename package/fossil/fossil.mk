f2f3a7a8dee200565eba31c71bf5a86e32dcfee8
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-08-18 23:58:06 +0000

    fossil, init.d tweaks and some useful scripts
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7202 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/fossil/fossil.mk
+++ package/fossil/fossil.mk
@@ -29,6 +29,9 @@ FOSSIL_UNINSTALL_STAGING_OPT = --version
 
 define FOSSIL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/fossil $(TARGET_DIR)/usr/bin/fossil
+	$(INSTALL) -m 0755 -D package/fossil/scripts/fossil-open $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 -D package/fossil/scripts/fossil-close $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 -D package/fossil/scripts/fossil-update $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 -D package/fossil/fossil.init $(TARGET_DIR)/etc/init.d/fossil
 	ln -sf ../../init.d/fossil $(TARGET_DIR)/etc/runlevels/default/S75fossil
 	ln -sf ../../init.d/fossil $(TARGET_DIR)/etc/runlevels/default/K15fossil
@@ -36,6 +39,9 @@ endef
 
 define FOSSIL_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/fossil
+	rm -f $(TARGET_DIR)/usr/bin/fossil-open
+	rm -f $(TARGET_DIR)/usr/bin/fossil-close
+	rm -f $(TARGET_DIR)/usr/bin/fossil-update
 	rm -f $(TARGET_DIR)/etc/init.d/fossil
 	rm -f $(TARGET_DIR)/etc/runlevels/default/S75fossil
 	rm -f $(TARGET_DIR)/etc/runlevels/default/K15fossil
