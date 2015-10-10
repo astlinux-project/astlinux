95ca29e727df387e8ead97fd89c597904d6d6972
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-10-10 16:20:59 +0000

    stunnel, version bump to 5.24
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7265 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.23
+STUNNEL_VERSION = 5.24
 STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/5.x
 STUNNEL_DEPENDENCIES = openssl
 
