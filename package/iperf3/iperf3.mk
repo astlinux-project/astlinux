65f436960dae4dc476673accf76e96138a755b79
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-10-26 21:58:09 +0000

    iperf3, new package, enabled where iperf is enabled. iperf3 is becomming more common and since it is small (100KB) let it sit along side the old iperf since they are not compatible
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7287 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/iperf3/iperf3.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# iperf3
+#
+################################################################################
+
+IPERF3_VERSION = 3.0.11
+IPERF3_SOURCE = iperf-$(IPERF3_VERSION)-source.tar.gz
+IPERF3_SITE = --no-check-certificate https://iperf.fr/download/iperf_3.0
+
+$(eval $(call AUTOTARGETS,package,iperf3))
