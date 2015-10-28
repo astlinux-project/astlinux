8d0aa0a5ccc91e02c743f6adeae0dda8baf19a2b
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-10-28 17:10:33 +0000

    monit, version bump to 5.15
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7293 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONIT_VERSION = 5.14
+MONIT_VERSION = 5.15
 MONIT_SITE = http://mmonit.com/monit/dist
 MONIT_DEPENDENCIES = host-bison host-flex
 #
