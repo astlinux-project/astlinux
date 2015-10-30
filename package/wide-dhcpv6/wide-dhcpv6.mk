ca605f9822576e7bbd9d5c25950b4eb5acf8c988
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-10-30 12:43:29 +0000

    wide-dhcpv6, don't build/install dhcp6ctl since we don't really need it
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7295 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/wide-dhcpv6/wide-dhcpv6.mk
+++ package/wide-dhcpv6/wide-dhcpv6.mk
@@ -13,12 +13,11 @@ WIDE_DHCPV6_CONF_OPT = \
 	--sysconfdir=/etc/wide-dhcpv6 \
 	ac_cv_func_setpgrp_void=yes
 
-## Only build client and control
-WIDE_DHCPV6_MAKE_OPT = dhcp6c dhcp6ctl
+## Only build client
+WIDE_DHCPV6_MAKE_OPT = dhcp6c
 
 define WIDE_DHCPV6_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/dhcp6c $(TARGET_DIR)/usr/sbin/
-	$(INSTALL) -D -m 0755 $(@D)/dhcp6ctl $(TARGET_DIR)/usr/sbin/
 	ln -sf /tmp/etc/wide-dhcpv6 $(TARGET_DIR)/etc/wide-dhcpv6
 endef
 
@@ -26,7 +25,6 @@ WIDE_DHCPV6_UNINSTALL_STAGING_OPT = --version
 
 define WIDE_DHCPV6_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/dhcp6c
-	rm -f $(TARGET_DIR)/usr/sbin/dhcp6ctl
 	rm -f $(TARGET_DIR)/etc/wide-dhcpv6
 endef
 
