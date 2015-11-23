26fff2bf0db1a5ec8a2f4b77cf50bba9f0409ca9
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-11-23 15:59:22 +0000

    strace, version bump to 4.10
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7344 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,13 +4,13 @@
 #
 #############################################################
 
-STRACE_VERSION = 4.5.20
-STRACE_SOURCE = strace-$(STRACE_VERSION).tar.bz2
-STRACE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/strace
+STRACE_VERSION = 4.10
+STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
+STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
 
-STRACE_CONF_ENV = ac_cv_header_linux_if_packet_h=yes \
-		  ac_cv_header_linux_netlink_h=yes \
-	          $(if $(BR2_LARGEFILE),ac_cv_type_stat64=yes,ac_cv_type_stat64=no)
+STRACE_CONF_ENV = \
+	ac_cv_header_linux_if_packet_h=yes \
+	ac_cv_header_linux_netlink_h=yes
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
