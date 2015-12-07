3b9ef3e93bebb167252dc953bfd7c2b828cacfea
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-12-07 01:36:17 +0000

    host-fakeroot, version bump to 1.20.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7378 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -3,12 +3,13 @@
 # fakeroot
 #
 #############################################################
-FAKEROOT_VERSION = 1.18.2
+FAKEROOT_VERSION = 1.20.2
 FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
-FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20111201T093630Z/pool/main/f/fakeroot
+FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141005T221953Z/pool/main/f/fakeroot
 
-# The package for the target cannot be selected (build problems when
-# largefile is enabled), but is needed for the host package to work
-# due to deficiencies in the package infrastructure.
-$(eval $(call AUTOTARGETS,package,fakeroot))
+# Force capabilities detection off
+# For now these are process capabilities (faked) rather than file
+# so they're of no real use
+HOST_FAKEROOT_CONF_ENV = ac_cv_func_capset=no
+
 $(eval $(call AUTOTARGETS,package,fakeroot,host))
