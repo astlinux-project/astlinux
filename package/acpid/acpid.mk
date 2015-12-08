43beaf2f3c3986572d5a7e35fd640999c540004e
Lonnie Abelbeck <lonnie@abelbeck.com>
2015-12-08 15:07:11 +0000

    acpid, version bump to 2.0.25, add K01acpid runlevel
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7385 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -4,16 +4,26 @@
 #
 #############################################################
 
-ACPID_VERSION = 2.0.23
+ACPID_VERSION = 2.0.25
 ACPID_SOURCE = acpid-$(ACPID_VERSION).tar.xz
 ACPID_SITE = http://downloads.sourceforge.net/project/acpid2
 
-define ACPID_SET_EVENTS
+define ACPID_SET_EVENTS_FILES
 	mkdir -p $(TARGET_DIR)/etc/acpi/events
 	/bin/echo -e "event=button[ /]power\naction=/sbin/poweroff" > $(TARGET_DIR)/etc/acpi/events/powerbtn
 	$(INSTALL) -D -m 755 package/acpid/acpid.init $(TARGET_DIR)/etc/init.d/acpid
+	ln -sf ../../init.d/acpid $(TARGET_DIR)/etc/runlevels/default/S24acpid
+	ln -sf ../../init.d/acpid $(TARGET_DIR)/etc/runlevels/default/K01acpid
 endef
 
-ACPID_POST_INSTALL_TARGET_HOOKS += ACPID_SET_EVENTS
+ACPID_POST_INSTALL_TARGET_HOOKS += ACPID_SET_EVENTS_FILES
+
+define ACPID_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/acpid
+	rm -f $(TARGET_DIR)/usr/bin/acpi_listen
+	rm -f $(TARGET_DIR)/etc/init.d/acpid
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S24acpid
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K01acpid
+endef
 
 $(eval $(call AUTOTARGETS,package,acpid))
