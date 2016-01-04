413ad04fc6528620205b41d0739dee5811cfd6c8
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-01-04 22:11:16 +0000

    fakeroot: fix build when libcap-dev is installed Ref: https://git.busybox.net/buildroot/commit/package?id=6fc5de4ef466b649b37a86e4f4994a667149dd80
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7442 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -10,6 +10,8 @@ FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20141005T221953Z/pool/
 # Force capabilities detection off
 # For now these are process capabilities (faked) rather than file
 # so they're of no real use
-HOST_FAKEROOT_CONF_ENV = ac_cv_func_capset=no
+HOST_FAKEROOT_CONF_ENV = \
+	ac_cv_header_sys_capability_h=no \
+	ac_cv_func_capset=no
 
 $(eval $(call AUTOTARGETS,package,fakeroot,host))
