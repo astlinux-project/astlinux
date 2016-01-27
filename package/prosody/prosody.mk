78a1d58adf318d45df2cb6a5dbac5475d89c9752
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-01-27 20:39:10 +0000

    prosody, version bump to 0.9.10, security fix: CVE-2016-0756
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7484 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/prosody/prosody.mk
+++ package/prosody/prosody.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PROSODY_VERSION = 0.9.9
+PROSODY_VERSION = 0.9.10
 PROSODY_SOURCE = prosody-$(PROSODY_VERSION).tar.gz
 PROSODY_SITE = http://prosody.im/downloads/source
 PROSODY_DEPENDENCIES = lua libidn openssl luafilesystem luaexpat luasocket luasec
