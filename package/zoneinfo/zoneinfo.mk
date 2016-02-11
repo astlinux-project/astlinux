12787967c28763066d1d70d91f6dc0172cf51557
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-02-11 22:03:31 +0000

    zoneinfo, version bump to 2016a and PHP_TIMEZONEDB 2016.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7522 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/zoneinfo/zoneinfo.mk
+++ package/zoneinfo/zoneinfo.mk
@@ -3,7 +3,7 @@
 # zoneinfo
 #
 ##############################################################
-ZONEINFO_VERSION := 2015g
+ZONEINFO_VERSION := 2016a
 ZONEINFO_DATA := tzdata$(ZONEINFO_VERSION).tar.gz
 ZONEINFO_SOURCE := tzcode$(ZONEINFO_VERSION).tar.gz
 ZONEINFO_SITE := http://www.iana.org/time-zones/repository/releases
