630b1710ef8d93e13415dc4dcc5ce47b22dcb333
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-02-18 23:26:07 +0000

    nano, version bump to 2.5.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7541 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/nano/nano.mk
+++ package/nano/nano.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-NANO_VERSION = 2.5.1
+NANO_VERSION = 2.5.2
 NANO_SITE = http://www.nano-editor.org/dist/v2.5
 NANO_MAKE_ENV = CURSES_LIB="-lncurses"
 NANO_CONF_ENV = ac_cv_prog_NCURSESW_CONFIG=false
