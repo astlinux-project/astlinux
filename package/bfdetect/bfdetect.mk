81d77df4464c1009c0d10bbbac5e3277ead953af
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-02-18 23:12:38 +0000

    bfdetect, version bump to 'Version: 3.0'
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7540 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/bfdetect/bfdetect.mk
+++ package/bfdetect/bfdetect.mk
@@ -3,30 +3,32 @@
 # bfdetect
 #
 ##############################################################
-BFDETECT_SOURCE:=bfdetect.tar.gz
-BFDETECT_SITE:=http://www.beronet.com/download/berofix/tools/
+BFDETECT_SOURCE:=bfdetect_src.tar.gz
+BFDETECT_SITE:=http://www.beronet.com/downloads/berofix/tools
 BFDETECT_DIR := $(BUILD_DIR)/bfdetect
 BFDETECT_CAT:=zcat
 BFDETECT_BINARY := bfdetect
 BFDETECT_TARGET_BINARY = usr/bin/$(BFDETECT_BINARY)
 
 $(DL_DIR)/$(BFDETECT_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(BFDETECT_SITE)/$(BFDETECT_SOURCE)
+	$(WGET) -P $(DL_DIR) $(BFDETECT_SITE)/$(BFDETECT_SOURCE)
 
 $(BFDETECT_DIR)/.unpacked: $(DL_DIR)/$(BFDETECT_SOURCE)
-	$(BFDETECT_CAT) $(DL_DIR)/$(BFDETECT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	mkdir -p $(BFDETECT_DIR)
+	$(BFDETECT_CAT) $(DL_DIR)/$(BFDETECT_SOURCE) | tar -C $(BFDETECT_DIR) $(TAR_OPTIONS) -
 	touch $(BFDETECT_DIR)/.unpacked
-	
+
 $(BFDETECT_DIR)/.configured: $(BFDETECT_DIR)/.unpacked
 	touch $(BFDETECT_DIR)/.configured
 
 $(BFDETECT_DIR)/$(BFDETECT_BINARY): $(BFDETECT_DIR)/.configured
+	$(SED) 's/^[[:space:]]*strip /#strip /' $(BFDETECT_DIR)/Makefile
 	$(MAKE) CC=$(TARGET_CC) CFLAGS='$(TARGET_CFLAGS)' -C $(BFDETECT_DIR) 
-	
+
 $(TARGET_DIR)/$(BFDETECT_TARGET_BINARY): $(BFDETECT_DIR)/$(BFDETECT_BINARY)
 	$(INSTALL) -D -m 0755 $(BFDETECT_DIR)/$(BFDETECT_BINARY) $(TARGET_DIR)/$(BFDETECT_TARGET_BINARY)
  
-	
+
 bfdetect: $(TARGET_DIR)/$(BFDETECT_TARGET_BINARY)
 
 bfdetect-source: $(BFDETECT_DIR)/bfdetect.c
