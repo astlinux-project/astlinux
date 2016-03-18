ee1e4d8285e7ff7d63f7bbf0d45305a38df3208c
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-03-18 23:28:36 +0000

    mac2vendor, build system, re-code to use GENTARGETS
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7599 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/mac2vendor/mac2vendor.mk
+++ package/mac2vendor/mac2vendor.mk
@@ -1,48 +1,37 @@
-#############################################################
+################################################################################
 #
 # mac2vendor
 #
-##############################################################
-MAC2VENDOR_VERSION := 2016-01-17
-MAC2VENDOR_DATA := oui-$(MAC2VENDOR_VERSION).txt
-MAC2VENDOR_DATA_ORIG := oui.txt
-MAC2VENDOR_SITE := http://standards-oui.ieee.org
-MAC2VENDOR_DIR := $(BUILD_DIR)/mac2vendor
-MAC2VENDOR_BINARY := usr/sbin/mac2vendor
+################################################################################
 
-$(DL_DIR)/$(MAC2VENDOR_DATA):
-	rm -f $(DL_DIR)/$(MAC2VENDOR_DATA_ORIG)
-	$(WGET) -P $(DL_DIR) $(MAC2VENDOR_SITE)/$(MAC2VENDOR_DATA_ORIG)
-	cp -a $(DL_DIR)/$(MAC2VENDOR_DATA_ORIG) $(DL_DIR)/$(MAC2VENDOR_DATA)
+MAC2VENDOR_VERSION = 2016-01-17
+MAC2VENDOR_SOURCE = oui-$(MAC2VENDOR_VERSION).txt
+MAC2VENDOR_SITE = http://files.astlinux.org
 
-$(MAC2VENDOR_DIR)/.data: $(DL_DIR)/$(MAC2VENDOR_DATA)
-	mkdir -p $(MAC2VENDOR_DIR)/oui-db
+##
+## curl -o dl/oui-2016-01-17.txt http://standards-oui.ieee.org/oui.txt
+## ./scripts/upload-dl-pair dl/oui-2016-01-17.txt
+##
+
+define MAC2VENDOR_EXTRACT_CMDS
+	mkdir -p $(@D)/oui-db
 	for i in 0 1 2 3 4 5 6 7 8 9 A B C D E F; do \
-	  sed 's/^ *//' $(DL_DIR)/$(MAC2VENDOR_DATA) | \
+	  sed 's/^ *//' $(DL_DIR)/$(MAC2VENDOR_SOURCE) | \
 	  grep "^[0-9A-F]\{5\}$$i " | \
-	  sed 's/ [^(]*.base 16.[^0-9a-zA-Z]*/~/' > $(MAC2VENDOR_DIR)/oui-db/xxxxx$$i ; \
-	  chmod a-w $(MAC2VENDOR_DIR)/oui-db/xxxxx$$i ; \
+	  sed 's/ [^(]*.base 16.[^0-9a-zA-Z]*/~/' > $(@D)/oui-db/xxxxx$$i ; \
 	done
-	touch $@
+endef
 
-$(TARGET_DIR)/$(MAC2VENDOR_BINARY): $(MAC2VENDOR_DIR)/.data
-	$(INSTALL) -D -m 0755 package/mac2vendor/mac2vendor $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
-	cp -a $(MAC2VENDOR_DIR)/oui-db $(TARGET_DIR)/usr/share/oui-db
+define MAC2VENDOR_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 package/mac2vendor/mac2vendor $(TARGET_DIR)/usr/sbin/mac2vendor
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/oui-db
+	cp $(@D)/oui-db/* $(TARGET_DIR)/usr/share/oui-db/
+	chmod a-w $(TARGET_DIR)/usr/share/oui-db/*
+endef
 
-mac2vendor: $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
-
-mac2vendor-clean:
-	rm -f $(TARGET_DIR)/$(MAC2VENDOR_BINARY)
+define MAC2VENDOR_UNINSTALL_TARGET_CMDS
+	rm -f  $(TARGET_DIR)/usr/sbin/mac2vendor
 	rm -rf $(TARGET_DIR)/usr/share/oui-db
+endef
 
-mac2vendor-dirclean:
-	rm -rf $(MAC2VENDOR_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(strip $(BR2_PACKAGE_MAC2VENDOR)),y)
-TARGETS+=mac2vendor
-endif
+$(eval $(call GENTARGETS,package,mac2vendor))
