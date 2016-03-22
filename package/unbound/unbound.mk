93188a5197d989861c693045f0dfb1fec5e206a8
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-03-22 18:06:53 +0000

    unbound, new package, enabled by default, only install the lib and the 'unbound-host' command.  Adds 780 KB un-compressed, 350 KB compressed to the image.  May be used by Asterisk per Matthew Jordan. Ref: https://wiki.asterisk.org/wiki/display/~jcolp/DNS+Support+in+Asterisk#DNSSupportinAsterisk-libunbound
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7605 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/unbound/unbound.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# unbound
+#
+#############################################################
+
+UNBOUND_VERSION = 1.5.8
+UNBOUND_SITE = https://www.unbound.net/downloads
+UNBOUND_SOURCE = unbound-$(UNBOUND_VERSION).tar.gz
+UNBOUND_INSTALL_STAGING = YES
+
+UNBOUND_DEPENDENCIES = openssl expat
+
+UNBOUND_CONF_OPT = \
+	--with-libexpat="$(STAGING_DIR)/usr" \
+	--with-ssl="$(STAGING_DIR)/usr"
+
+# GOST cipher support requires openssl extra engines
+ifeq ($(BR2_PACKAGE_OPENSSL_ENGINES),y)
+UNBOUND_CONF_OPT += --enable-gost
+else
+UNBOUND_CONF_OPT += --disable-gost
+endif
+
+define UNBOUND_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(STAGING_DIR)/usr/sbin/unbound-host $(TARGET_DIR)/usr/sbin/unbound-host
+	ln -sf ../sbin/unbound-host $(TARGET_DIR)/usr/bin/host
+	cp -a $(STAGING_DIR)/usr/lib/libunbound.so* $(TARGET_DIR)/usr/lib/
+endef
+
+define UNBOUND_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/unbound-host
+	rm -f $(TARGET_DIR)/usr/bin/host
+	rm -f $(TARGET_DIR)/usr/lib/libunbound.so*
+endef
+
+$(eval $(call AUTOTARGETS,package,unbound))
