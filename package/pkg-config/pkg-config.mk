eecef10b81d857fedd2a8515245b093c1389ec66
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-04-29 17:29:56 +0000

    build system, use pkgconf for pkg-config HOST-only package.  This is what Buildroot now uses.
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7660 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/pkg-config/pkg-config.mk
+++ package/pkg-config/pkg-config.mk
@@ -1,24 +1,37 @@
 #############################################################
 #
-# pkgconfig
+# pkg-config
 #
 #############################################################
-PKG_CONFIG_VERSION = 0.25
-PKG_CONFIG_SOURCE = pkg-config-$(PKG_CONFIG_VERSION).tar.gz
-PKG_CONFIG_SITE = http://pkgconfig.freedesktop.org/releases/
 
-PKG_CONFIG_DEPENDENCIES = libglib2
+PKG_CONFIG_VERSION = 0.9.12
+PKG_CONFIG_SOURCE = pkgconf-$(PKG_CONFIG_VERSION).tar.bz2
+PKG_CONFIG_SITE = https://github.com/pkgconf/pkgconf/releases/download/pkgconf-$(PKG_CONFIG_VERSION)
 
-PKG_CONFIG_CONF_OPT = --with-installed-glib
+define HOST_PKG_CONFIG_INSTALL_WRAPPER
+	$(INSTALL) -m 0755 -D package/pkg-config/pkg-config.in \
+		$(HOST_DIR)/usr/bin/pkg-config
+	$(SED) 's,@PKG_CONFIG_LIBDIR@,$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig,' \
+		-e 's,@STAGING_DIR@,$(STAGING_DIR),' \
+		$(HOST_DIR)/usr/bin/pkg-config
+endef
 
-HOST_PKG_CONFIG_CONF_OPT = \
-		--with-pc-path="$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig" \
-		--with-sysroot="$(STAGING_DIR)" \
-		--disable-static
+define HOST_PKG_CONFIG_STATIC
+	$(SED) 's,@STATIC@,--static,' $(HOST_DIR)/usr/bin/pkg-config
+endef
 
-HOST_PKG_CONFIG_AUTORECONF = YES
+define HOST_PKG_CONFIG_SHARED
+	$(SED) 's,@STATIC@,,' $(HOST_DIR)/usr/bin/pkg-config
+endef
 
-$(eval $(call AUTOTARGETS,package,pkg-config))
+HOST_PKG_CONFIG_POST_INSTALL_HOOKS += HOST_PKG_CONFIG_INSTALL_WRAPPER
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+HOST_PKG_CONFIG_POST_INSTALL_HOOKS += HOST_PKG_CONFIG_STATIC
+else
+HOST_PKG_CONFIG_POST_INSTALL_HOOKS += HOST_PKG_CONFIG_SHARED
+endif
+
 $(eval $(call AUTOTARGETS,package,pkg-config,host))
 
-PKG_CONFIG_HOST_BINARY:=$(HOST_DIR)/usr/bin/pkg-config
+PKG_CONFIG_HOST_BINARY = $(HOST_DIR)/usr/bin/pkg-config
