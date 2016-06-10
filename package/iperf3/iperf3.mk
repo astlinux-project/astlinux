fe7fca779da1dea45a6d7cfdbac84d11d8ac82b9
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-06-10 03:17:07 +0000

    iperf3, version bump to 3.0.12, security fix: CVE-2016-4303 (ESNET-SECADV-2016-0001)
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7713 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.0.11
+IPERF3_VERSION = 3.0.12
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION)-source.tar.gz
 IPERF3_SITE = https://iperf.fr/download/source
 
