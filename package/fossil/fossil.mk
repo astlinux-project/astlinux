decdb50dbe6b7988ec32d52be0fbf87453479f6c
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-06-15 17:33:47 +0000

    fossil, version bump to 1.35
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7721 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/fossil/fossil.mk
+++ package/fossil/fossil.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FOSSIL_VERSION = 1.34
+FOSSIL_VERSION = 1.35
 FOSSIL_SOURCE = fossil-src-$(FOSSIL_VERSION).tar.gz
 FOSSIL_SITE = http://www.fossil-scm.org/download
 FOSSIL_DEPENDENCIES = zlib openssl
