3cfd38ed0215e16b0c96472365bcc398f498c531
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-06-17 15:50:31 +0000

    tweak upload_script.sh script
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7725 57edc7d3-a90e-0410-b364-91459a37fc5a

--- scripts/upload_script.sh
+++ scripts/upload_script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-for i in $(ls dl | grep -v .sha1); do
-  if [ ! -f dl/"$i".sha1 -a -f dl/"$i" ]; then
-    sha1sum dl/"$i" > dl/"$i".sha1
+action="$1"
+
+if [ -z "$action" ]; then
+  echo "Usage: upload_script.sh all|dry-run"
+  exit 1
+fi
+
+for i in $(ls -1 dl | grep -v .sha1); do
+  if [ -f "dl/${i}" ] && [ ! -f "dl/${i}.sha1" ]; then
+    echo "Adding SHA1: dl/${i}.sha1"
+    sha1sum "dl/${i}" > "dl/${i}.sha1"
   fi
 done
 
-if [ -z "$1" ]; then
+if [ "$action" = "all" ]; then
   s3cmd sync --acl-public --exclude '*/*' -v dl/ s3://astlinuxfiles/
+elif [ "$action" = "dry-run" ]; then
+  s3cmd sync --dry-run --exclude '*/*' -v dl/ s3://astlinuxfiles/
 fi
