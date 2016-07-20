78c3903b2130d10b89f4e7312eb1c4fdae592185
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-07-20 14:41:24 +0000

    screen, version bump to 4.4.0, synced to upstream Buildroot
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7758 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/screen/screen.mk
+++ package/screen/screen.mk
@@ -4,11 +4,17 @@
 #
 #############################################################
 
-SCREEN_VERSION = 4.0.3
+SCREEN_VERSION = 4.4.0
 SCREEN_SITE = $(BR2_GNU_MIRROR)/screen
 SCREEN_DEPENDENCIES = ncurses
-SCREEN_CONF_ENV = CFLAGS=-DSYSV=1
-SCREEN_MAKE_OPT = -j1
+SCREEN_AUTORECONF = YES
+SCREEN_CONF_ENV = CFLAGS="$(TARGET_CFLAGS)"
 SCREEN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) SCREEN=screen install_bin
+
+define SCREEN_INSTALL_SCREENRC
+	$(INSTALL) -m 0755 -D $(@D)/etc/screenrc $(TARGET_DIR)/etc/screenrc
+endef
+
+SCREEN_POST_INSTALL_TARGET_HOOKS += SCREEN_INSTALL_SCREENRC
 
 $(eval $(call AUTOTARGETS,package,screen))
