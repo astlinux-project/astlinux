18bb13d4f1e8c3b182296033596315e7d331a599
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-07-21 21:45:19 +0000

    asterisk-codec-silk, version bump to 1.0.2 for the binary blobs
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7770 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/asterisk-codec-silk/asterisk-codec-silk.mk
+++ package/asterisk-codec-silk/asterisk-codec-silk.mk
@@ -5,19 +5,19 @@
 ##############################################################
 ifeq ($(BR2_PACKAGE_ASTERISK_v11),y)
   ifeq ($(ARCH),x86_64)
-ASTERISK_CODEC_SILK_VERSION := 11.0_1.0.0-generic_64
+ASTERISK_CODEC_SILK_VERSION := 11.0_1.0.2-generic_64
 ASTERISK_CODEC_SILK_SITE := http://downloads.digium.com/pub/telephony/codec_silk/asterisk-11.0/x86-64
   else
-ASTERISK_CODEC_SILK_VERSION := 11.0_1.0.0-i686_32
+ASTERISK_CODEC_SILK_VERSION := 11.0_1.0.2-i686_32
 ASTERISK_CODEC_SILK_SITE := http://downloads.digium.com/pub/telephony/codec_silk/asterisk-11.0/x86-32
   endif
 endif
 ifeq ($(BR2_PACKAGE_ASTERISK_v13),y)
   ifeq ($(ARCH),x86_64)
-ASTERISK_CODEC_SILK_VERSION := 13.0_1.0.0-generic_64
+ASTERISK_CODEC_SILK_VERSION := 13.0_1.0.2-generic_64
 ASTERISK_CODEC_SILK_SITE := http://downloads.digium.com/pub/telephony/codec_silk/asterisk-13.0/x86-64
   else
-ASTERISK_CODEC_SILK_VERSION := 13.0_1.0.0-i686_32
+ASTERISK_CODEC_SILK_VERSION := 13.0_1.0.2-i686_32
 ASTERISK_CODEC_SILK_SITE := http://downloads.digium.com/pub/telephony/codec_silk/asterisk-13.0/x86-32
   endif
 endif
