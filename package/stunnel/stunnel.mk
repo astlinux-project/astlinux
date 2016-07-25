85495b76546ba18c952b9b756629e8c54ae32a5b
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-07-25 20:28:20 +0000

    stunnel, version bump to 5.35 Fixes: TLS session caching memory leak Malfunctioning 'verify = 4'
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7775 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.31
+STUNNEL_VERSION = 5.35
 STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/5.x
 STUNNEL_DEPENDENCIES = openssl
 
