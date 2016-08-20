157beb500e3e8e5cadc5d9362d5ffe99156f70e8
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-08-20 16:09:38 +0000

    strace, version bump to 4.13
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7800 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,15 +4,9 @@
 #
 #############################################################
 
-STRACE_VERSION = 4.11
+STRACE_VERSION = 4.13
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
 STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
-
-STRACE_CONF_ENV = \
-	st_cv_m32_mpers=no \
-	st_cv_mx32_mpers=no \
-	ac_cv_header_linux_if_packet_h=yes \
-	ac_cv_header_linux_netlink_h=yes
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
