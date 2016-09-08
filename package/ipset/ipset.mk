516753fe847be220b0f0997468ff9ee814e78b3e
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-09-08 22:06:37 +0000

    ipset, version bump to 6.29, not enabled yet, needs added kernel modules
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7824 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,10 +4,13 @@
 #
 #############################################################
 
-IPSET_VERSION = 6.8
+IPSET_VERSION = 6.29
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
-IPSET_AUTORECONF = YES
 IPSET_DEPENDENCIES = libmnl host-pkg-config
+
+IPSET_CONF_OPT = \
+	--disable-static \
+	--with-kmod=no
 
 $(eval $(call AUTOTARGETS,package,ipset))
