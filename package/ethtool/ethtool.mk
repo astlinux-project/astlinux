012b0701ff2a79eba8dab8d6c1fa038265d45a3d
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-10-06 16:26:14 +0000

    ethtool, version bump to 4.8
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7879 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 4.6
+ETHTOOL_VERSION = 4.8
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 
 $(eval $(call AUTOTARGETS,package,ethtool))
