035096ec1d688a86a1f5cc1f1c29b14020239655
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-10-09 16:51:54 +0000

    iperf, public release file iperf-2.0.9.tar.gz has changed, continue with our origional copy until this is resolved
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7882 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -5,7 +5,8 @@
 #############################################################
 IPERF_VERSION = 2.0.9
 IPERF_SOURCE = iperf-$(IPERF_VERSION).tar.gz
-IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
+IPERF_SITE = http://files.astlinux-project.org
+#IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
 
 IPERF_CONF_ENV = \
 	ac_cv_func_malloc_0_nonnull=yes
