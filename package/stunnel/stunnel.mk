4c7e1114a61fdb8723f4e0ffdf08e3fe6fa66623
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-11-07 16:42:21 +0000

    stunnel, version bump to 5.37
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7936 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.36
+STUNNEL_VERSION = 5.37
 STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/5.x
 STUNNEL_DEPENDENCIES = openssl
 
