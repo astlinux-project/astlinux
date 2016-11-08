d75dda521df6c89a637cf4d3d1ae2477540853f6
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-11-08 02:19:38 +0000

    runnix, missed a mirror.astlinux.org to mirror.astlinux-project.org edit
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7940 57edc7d3-a90e-0410-b364-91459a37fc5a

--- boot/runnix/runnix.mk
+++ boot/runnix/runnix.mk
@@ -5,7 +5,7 @@
 #############################################################
 RUNNIX_VER:=0.4-7671
 RUNNIX_SOURCE:=runnix-$(RUNNIX_VER).tar.gz
-RUNNIX_SITE:=http://mirror.astlinux.org/runnix4
+RUNNIX_SITE:=http://mirror.astlinux-project.org/runnix4
 RUNNIX_DIR:=$(BUILD_DIR)/runnix-$(RUNNIX_VER)
 RUNNIX_CAT:=zcat
 RUNFS_DIR:=$(BUILD_DIR)/runfs
