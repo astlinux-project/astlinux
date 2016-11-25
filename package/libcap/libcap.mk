cc2991ff8c30bfa69608281957f142f6e56ed5be
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-11-25 15:03:04 +0000

    libcap, version bump to 2.25
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@7982 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/libcap/libcap.mk
+++ package/libcap/libcap.mk
@@ -4,34 +4,47 @@
 #
 #############################################################
 
-LIBCAP_VERSION = 2.20
-LIBCAP_SITE = $(BR2_KERNEL_MIRROR)/linux/libs/security/linux-privs/libcap2
+LIBCAP_VERSION = 2.25
+LIBCAP_SITE = https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2
+LIBCAP_SOURCE = libcap-$(LIBCAP_VERSION).tar.xz
 LIBCAP_DEPENDENCIES = host-libcap
+
+LIBCAP_DEPENDENCIES = host-libcap host-gperf
 LIBCAP_INSTALL_STAGING = YES
 
+HOST_LIBCAP_DEPENDENCIES = host-gperf
+
+LIBCAP_MAKE_TARGET = all
+LIBCAP_MAKE_INSTALL_TARGET = install
+
+LIBCAP_MAKE_FLAGS = \
+	BUILD_CC="$(HOSTCC)" \
+	BUILD_CFLAGS="$(HOST_CFLAGS)"
+
+
 define LIBCAP_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		LIBATTR=no BUILD_CC="$(HOSTCC)" BUILD_CFLAGS="$(HOST_CFLAGS)"
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/libcap \
+		$(LIBCAP_MAKE_FLAGS) $(LIBCAP_MAKE_TARGET)
 endef
 
 define LIBCAP_INSTALL_STAGING_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(STAGING_DIR) \
-		prefix=/usr lib=lib install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libcap $(LIBCAP_MAKE_FLAGS) \
+		DESTDIR=$(STAGING_DIR) prefix=/usr lib=lib $(LIBCAP_MAKE_INSTALL_TARGET)
 endef
 
 define LIBCAP_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(TARGET_DIR) \
-		prefix=/usr lib=lib install
-	rm -f $(addprefix $(TARGET_DIR)/usr/sbin/,capsh getpcaps)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/libcap $(LIBCAP_MAKE_FLAGS) \
+		DESTDIR=$(TARGET_DIR) prefix=/usr lib=lib $(LIBCAP_MAKE_INSTALL_TARGET)
 endef
 
 define HOST_LIBCAP_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D) LIBATTR=no
+	$(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D)\
+		RAISE_SETFCAP=no
 endef
 
 define HOST_LIBCAP_INSTALL_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(HOST_DIR) \
-		prefix=/usr lib=lib install
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(HOST_DIR) \
+		RAISE_SETFCAP=no prefix=/usr lib=lib install
 endef
 
 $(eval $(call GENTARGETS,package,libcap))
