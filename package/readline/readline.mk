68f35802c8a169e1c044bfc9d3952bf6fa369757
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-12-02 19:09:43 +0000

    readline, version bump to 7.0
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8008 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/readline/readline.mk
+++ package/readline/readline.mk
@@ -4,24 +4,26 @@
 #
 #############################################################
 
-READLINE_VERSION = 6.2
+READLINE_VERSION = 7.0
 READLINE_SOURCE = readline-$(READLINE_VERSION).tar.gz
 READLINE_SITE = $(BR2_GNU_MIRROR)/readline
 READLINE_INSTALL_STAGING = YES
-READLINE_INSTALL_TARGET = YES
 
 READLINE_DEPENDENCIES = ncurses
 
-READLINE_CONF_ENV = bash_cv_func_sigsetjmp=yes
+READLINE_CONF_ENV = \
+	bash_cv_func_sigsetjmp=yes \
+	bash_cv_wcwidth_broken=no
 
 define READLINE_INSTALL_TARGET_CMDS
-	$(MAKE1) DESTDIR=$(TARGET_DIR) -C $(@D) uninstall
-	$(MAKE1) DESTDIR=$(TARGET_DIR) -C $(@D) install-shared uninstall-doc
-	chmod 775 $(TARGET_DIR)/usr/lib/libreadline.so.$(READLINE_VERSION) \
-		$(TARGET_DIR)/usr/lib/libhistory.so.$(READLINE_VERSION)
-	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) \
-		$(TARGET_DIR)/usr/lib/libreadline.so.$(READLINE_VERSION) \
-		$(TARGET_DIR)/usr/lib/libhistory.so.$(READLINE_VERSION)
+	cp -a $(STAGING_DIR)/usr/lib/libhistory.so* $(TARGET_DIR)/usr/lib/
+	cp -a $(STAGING_DIR)/usr/lib/libreadline.so* $(TARGET_DIR)/usr/lib/
+	chmod +w $(addprefix $(TARGET_DIR)/usr/lib/,libhistory.so.* libreadline.so.*)
+endef
+
+define READLINE_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/lib/libhistory.so*
+	rm -f $(TARGET_DIR)/usr/lib/libreadline.so*
 endef
 
 $(eval $(call AUTOTARGETS,package,readline))
