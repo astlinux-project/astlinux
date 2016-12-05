dfb45eb524f60f5b97fe07c7bf19a82cbb8c131c
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-12-05 16:10:15 +0000

    bc, enable libedit support
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8016 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/bc/bc.mk
+++ package/bc/bc.mk
@@ -9,6 +9,11 @@ BC_SOURCE = bc-$(BC_VERSION).tar.bz2
 BC_SITE = http://alpha.gnu.org/gnu/bc
 BC_DEPENDENCIES = host-bison host-flex
 
+ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+BC_DEPENDENCIES += libedit
+BC_CONF_OPT += --with-libedit
+endif
+
 define BC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/bc/bc $(TARGET_DIR)/usr/bin/bc
 endef
