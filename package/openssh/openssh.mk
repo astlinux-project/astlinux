6e6bdd8205c7859dbf102cabee9bc2183a5ab4a2
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-12-05 14:16:36 +0000

    openssh, enable libedit support which sftp uses for command history and tab completion
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8014 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -7,11 +7,23 @@
 OPENSSH_VERSION = 6.6p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
-OPENSSH_CONF_OPT = --libexecdir=/usr/libexec --disable-lastlog --disable-utmp \
-		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip \
-		--sysconfdir=/etc/ssh
 
 OPENSSH_DEPENDENCIES = zlib openssl
+
+OPENSSH_CONF_OPT = \
+	--libexecdir=/usr/libexec \
+	--disable-lastlog \
+	--disable-utmp \
+	--disable-utmpx \
+	--disable-wtmp \
+	--disable-wtmpx \
+	--disable-strip \
+	--sysconfdir=/etc/ssh
+
+ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+OPENSSH_DEPENDENCIES += libedit
+OPENSSH_CONF_OPT += --with-libedit="$(STAGING_DIR)/usr"
+endif
 
 OPENSSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) -C $(@D) install-nosysconf
 
