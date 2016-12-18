2ba30c9c5cebf99dda3bae90b48f1a2425a3a83c
Lonnie Abelbeck <lonnie@abelbeck.com>
2016-12-18 17:46:40 +0000

    miniupnpd, version bump to 2.0.20161216
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8044 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/miniupnpd/miniupnpd.mk
+++ package/miniupnpd/miniupnpd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MINIUPNPD_VERSION = 2.0
+MINIUPNPD_VERSION = 2.0.20161216
 MINIUPNPD_SOURCE = miniupnpd-$(MINIUPNPD_VERSION).tar.gz
 MINIUPNPD_SITE = http://miniupnp.free.fr/files
 MINIUPNPD_DEPENDENCIES = host-pkg-config linux iptables openssl util-linux
