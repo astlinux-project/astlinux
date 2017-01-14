90aebf4bb694c9af3bd3c0d85421e87d59007348
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-01-14 22:29:31 +0000

    acme, build system, add /etc/acme symlink and keep dns_nsupdate.sh if BIND is enabled
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8095 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/acme/acme.mk
+++ package/acme/acme.mk
@@ -8,6 +8,8 @@ ACME_VERSION = 2.6.5-2017-01-13
 ACME_SOURCE = acme.sh-$(ACME_VERSION).tar.gz
 ACME_SITE = http://files.astlinux-project.org
 
+ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh $(if $(BR2_PACKAGE_BIND),,dns_nsupdate.sh) README.md
+
 ##
 ## curl -L -o dl/acme.sh-2.6.5-2017-01-13.tar.gz https://github.com/Neilpang/acme.sh/archive/master.tar.gz
 ## ./scripts/upload-dl-pair dl/acme.sh-2.6.5-2017-01-13.tar.gz
@@ -18,14 +20,16 @@ define ACME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 package/acme/astlinux.sh $(TARGET_DIR)/stat/etc/acme/deploy/astlinux.sh
 	$(INSTALL) -D -m 0755 $(@D)/acme.sh $(TARGET_DIR)/stat/etc/acme/acme.sh
 	cp -a $(@D)/dnsapi $(TARGET_DIR)/stat/etc/acme/
+	ln -s /mnt/kd/acme $(TARGET_DIR)/etc/acme
+	# Remove non-required dnsapi files
+	rm -f $(addprefix $(TARGET_DIR)/stat/etc/acme/dnsapi/, $(ACME_REMOVE_TARGET_DNSAPI))
+	# Make sure the dnsapi scripts are executable
 	find $(TARGET_DIR)/stat/etc/acme/dnsapi/ -name '*.sh' -print0 | xargs -0 chmod 755
-	# We don't enable BIND, so no nsupdate, and remove sample
-	rm -f $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_nsupdate.sh
-	rm -f $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_myapi.sh
 endef
 
 define ACME_UNINSTALL_TARGET_CMDS
 	rm -f  $(TARGET_DIR)/usr/sbin/acme-client
+	rm -f  $(TARGET_DIR)/etc/acme
 	rm -rf $(TARGET_DIR)/stat/etc/acme
 endef
 
