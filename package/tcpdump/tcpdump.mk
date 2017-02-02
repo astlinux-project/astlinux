ebddbbc8a68594c532b069abb27a7f21f6cf6fcf
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-02-02 14:53:44 +0000

    tcpdump, version bump to 4.9.0, fixes many CVE vulnerabilities that might of resulted in denial of service or the execution of arbitrary code
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8144 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-TCPDUMP_VERSION = 4.8.1
+TCPDUMP_VERSION = 4.9.0
 TCPDUMP_SITE = http://www.tcpdump.org/release
 TCPDUMP_SOURCE = tcpdump-$(TCPDUMP_VERSION).tar.gz
 
