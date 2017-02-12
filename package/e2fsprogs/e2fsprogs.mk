377aa0d86cb4189d62539effffd5eb3767963489
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-02-12 00:39:11 +0000

    e2fsprogs, version bump to 1.43.4 Build system, add FOO_PRE_INSTALL_STAGING_HOOKS and FOO_PRE_INSTALL_TARGET_HOOKS
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8157 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/e2fsprogs/e2fsprogs.mk
+++ package/e2fsprogs/e2fsprogs.mk
@@ -4,17 +4,27 @@
 #
 #############################################################
 
-E2FSPROGS_VERSION = 1.42.13
+E2FSPROGS_VERSION = 1.43.4
 E2FSPROGS_SITE = http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v$(E2FSPROGS_VERSION)
 E2FSPROGS_INSTALL_STAGING = YES
 E2FSPROGS_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) LDCONFIG=true install-libs
 E2FSPROGS_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) LDCONFIG=true install
+
+E2FSPROGS_DEPENDENCIES = host-bison host-pkg-config util-linux
+# we don't have a host-util-linux
+HOST_E2FSPROGS_DEPENDENCIES = host-bison host-pkg-config
+
+# e4defrag doesn't build on older systems like RHEL5.x, and we don't
+# need it on the host anyway.
+# Disable fuse2fs as well to avoid carrying over deps, and it's unused
+HOST_E2FSPROGS_CONF_OPT += --disable-defrag --disable-fuse2fs
 
 E2FSPROGS_CONF_OPT = \
 	$(if $(BR2_PREFER_STATIC_LIB),,--enable-elf-shlibs) \
 	$(if $(BR2_PACKAGE_E2FSPROGS_DEBUGFS),,--disable-debugfs) \
 	$(if $(BR2_PACKAGE_E2FSPROGS_E2IMAGE),,--disable-imager) \
 	--disable-defrag \
+	--disable-fuse2fs \
 	$(if $(BR2_PACKAGE_E2FSPROGS_RESIZE2FS),,--disable-resizer) \
 	--disable-uuidd \
 	--disable-libblkid \
@@ -24,7 +34,17 @@ E2FSPROGS_CONF_OPT = \
 	--disable-testio-debug \
 	--disable-rpath
 
-E2FSPROGS_DEPENDENCIES = host-bison host-pkg-config util-linux
+# Some programs are built for the host, but use definitions guessed by
+# the configure script (i.e with the cross-compiler). Help them by
+# saying that <sys/stat.h> is available on the host, which is needed
+# for util/subst.c to build properly.
+E2FSPROGS_CONF_ENV += BUILD_CFLAGS="-DHAVE_SYS_STAT_H"
+
+# Disable use of the host magic.h, as on older hosts (e.g. RHEL 5)
+# it doesn't provide definitions expected by e2fsprogs support lib.
+HOST_E2FSPROGS_CONF_ENV += \
+	ac_cv_header_magic_h=no \
+	ac_cv_lib_magic_magic_file=no
 
 E2FSPROGS_MAKE_OPT = \
 	LDCONFIG=true
@@ -32,8 +52,6 @@ E2FSPROGS_MAKE_OPT = \
 define HOST_E2FSPROGS_INSTALL_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) install install-libs
 endef
-# we don't have a host-util-linux
-HOST_E2FSPROGS_DEPENDENCIES = host-bison host-pkg-config
 
 # binaries to keep or remove
 E2FSPROGS_BINTARGETS_$(BR2_PACKAGE_E2FSPROGS_BADBLOCKS) += usr/sbin/badblocks
@@ -49,6 +67,7 @@ E2FSPROGS_BINTARGETS_$(BR2_PACKAGE_E2FSPROGS_LOGSAVE) += usr/sbin/logsave
 E2FSPROGS_BINTARGETS_$(BR2_PACKAGE_E2FSPROGS_LSATTR) += usr/bin/lsattr
 E2FSPROGS_BINTARGETS_$(BR2_PACKAGE_E2FSPROGS_MKE2FS) += usr/sbin/mke2fs
 E2FSPROGS_BINTARGETS_$(BR2_PACKAGE_E2FSPROGS_MKLOSTFOUND) += usr/sbin/mklost+found
+E2FSPROGS_BINTARGETS_ += usr/sbin/e4crypt
 
 # files to remove
 E2FSPROGS_TXTTARGETS_ = \
@@ -85,6 +104,29 @@ endef
 
 ifeq ($(BR2_PACKAGE_E2FSPROGS_E2FSCK),y)
 E2FSPROGS_POST_INSTALL_TARGET_HOOKS += E2FSPROGS_TARGET_E2FSCK_SYMLINKS
+endif
+
+# If BusyBox is included, its configuration may supply its own variant
+# of ext2-related tools. Since Buildroot desires having full blown
+# variants take precedence (in this case, e2fsprogs), we want to remove
+# BusyBox's variant of e2fsprogs provided binaries. e2fsprogs targets
+# /usr/{bin,sbin} where BusyBox targets /{bin,sbin}. We will attempt to
+# remove BusyBox-generated ext2-related tools from /{bin,sbin}. We need
+# to do this in the pre-install stage to ensure we do not accidentally
+# remove e2fsprogs's binaries in usr-merged environments (ie. if they
+# are removed, they would be re-installed in this package's install
+# stage).
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+E2FSPROGS_DEPENDENCIES += busybox
+
+define E2FSPROGS_REMOVE_BUSYBOX_APPLETS
+	rm -f $(TARGET_DIR)/bin/chattr
+	rm -f $(TARGET_DIR)/bin/lsattr
+	rm -f $(TARGET_DIR)/sbin/fsck
+	rm -f $(TARGET_DIR)/sbin/tune2fs
+	rm -f $(TARGET_DIR)/sbin/e2label
+endef
+E2FSPROGS_PRE_INSTALL_TARGET_HOOKS += E2FSPROGS_REMOVE_BUSYBOX_APPLETS
 endif
 
 define E2FSPROGS_TARGET_TUNE2FS_SYMLINK
