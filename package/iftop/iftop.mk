f61c46032b321d1129602885602272f6ff434578
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-02-15 16:57:35 +0000

    iftop, set setuid bit so rbash users can use it, better would be if iftop added libcap 'cap_net_raw+ep' capabilities
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8162 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/iftop/iftop.mk
+++ package/iftop/iftop.mk
@@ -9,4 +9,12 @@ IFTOP_SOURCE:=iftop-$(IFTOP_VERSION).tar.gz
 IFTOP_SITE = http://www.ex-parrot.com/pdw/iftop/download
 IFTOP_DEPENDENCIES = libpcap ncurses
 
+define IFTOP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 4711 $(@D)/iftop $(TARGET_DIR)/usr/sbin/
+endef
+
+define IFTOP_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/iftop
+endef
+
 $(eval $(call AUTOTARGETS,package,iftop))
