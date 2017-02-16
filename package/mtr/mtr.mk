ea193f0d5abcd55a570035ee64e06a38cfab2668
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-02-16 16:14:31 +0000

    mtr, set setuid bit and allow rbash users to use it, move installation from /usr/bin/ to /usr/sbin/
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8166 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/mtr/mtr.mk
+++ package/mtr/mtr.mk
@@ -13,11 +13,11 @@ MTR_CONF_OPT = \
 	--without-gtk
 
 define MTR_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/mtr $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 4711 $(@D)/mtr $(TARGET_DIR)/usr/sbin/
 endef
 
 define MTR_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/mtr
+	rm -f $(TARGET_DIR)/usr/sbin/mtr
 endef
 
 $(eval $(call AUTOTARGETS,package,mtr))
