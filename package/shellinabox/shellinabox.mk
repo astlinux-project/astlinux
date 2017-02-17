8c7256431cda0fa325e68b27beea7704d65330fc
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-02-17 14:35:43 +0000

    shellinabox, add user selectable styles; Black, White or Green on White or Black background, right-click for menu
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8170 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/shellinabox/shellinabox.mk
+++ package/shellinabox/shellinabox.mk
@@ -16,14 +16,26 @@ SHELLINABOX_CONF_OPT = \
 	--disable-pam \
 	--disable-runtime-loading
 
+define SHELLINABOX_GREEN_ON_BLACK
+	cp $(@D)/shellinabox/white-on-black.css $(@D)/shellinabox/green-on-black.css
+	$(SED) 's/white/green/g' \
+	    -e 's/#ffffff/#00ff00/g' \
+		$(@D)/shellinabox/green-on-black.css
+endef
+SHELLINABOX_POST_BUILD_HOOKS = SHELLINABOX_GREEN_ON_BLACK
+
 define SHELLINABOX_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/shellinaboxd $(TARGET_DIR)/usr/bin/shellinaboxd
 	$(INSTALL) -D -m 0755 package/shellinabox/shellinaboxd.init $(TARGET_DIR)/etc/init.d/shellinaboxd
+	$(INSTALL) -D -m 0644 $(@D)/shellinabox/black-on-white.css $(TARGET_DIR)/usr/share/shellinabox/00xBlack_on_White.css
+	$(INSTALL) -D -m 0644 $(@D)/shellinabox/white-on-black.css $(TARGET_DIR)/usr/share/shellinabox/01-White_on_Black.css
+	$(INSTALL) -D -m 0644 $(@D)/shellinabox/green-on-black.css $(TARGET_DIR)/usr/share/shellinabox/02-Green_on_Black.css
 endef
 
 define SHELLINABOX_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/shellinaboxd
 	rm -f $(TARGET_DIR)/etc/init.d/shellinaboxd
+	rm -rf $(TARGET_DIR)/usr/share/shellinabox
 endef
 
 $(eval $(call AUTOTARGETS,package,shellinabox))
