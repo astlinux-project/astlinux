672a4bf59b7b61b8e14466e36f10f27529e6caeb
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-03-23 20:49:22 +0000

    openvpn, version bump to 2.4.1, add rc.conf variable OVPNC_REMOTE_CERT_TLS to replace deprecated OVPNC_NSCERTTYPE variable
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8225 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENVPN_VERSION = 2.4.0
+OPENVPN_VERSION = 2.4.1
 OPENVPN_SITE = http://swupdate.openvpn.net/community/releases
 OPENVPN_DEPENDENCIES = host-pkg-config openssl
 
