58473aa5dda89e316a70bc7a0ecc16abbeba0947
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-03-26 01:06:12 +0000

    netcalc, new package, enabled for default builds
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8233 57edc7d3-a90e-0410-b364-91459a37fc5a

---
+++ package/netcalc/netcalc.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# netcalc
+#
+################################################################################
+
+NETCALC_VERSION = 2.0.1-pre
+NETCALC_SOURCE = netcalc-$(NETCALC_VERSION).tar.gz
+NETCALC_SITE = http://files.astlinux-project.org
+#NETCALC_SITE = https://github.com/troglobit/netcalc/releases/download/v$(NETCALC_VERSION)
+NETCALC_AUTORECONF = YES
+
+## Default install adds ipcalc symlink, confused with Busybox's
+##
+define NETCALC_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/netcalc $(TARGET_DIR)/usr/bin/
+endef
+
+define NETCALC_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/netcalc
+endef
+
+$(eval $(call AUTOTARGETS,package,netcalc))
