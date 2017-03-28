a817a44a939fcdaabf2547e3162167272fec4a7b
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-03-28 21:59:53 +0000

    netcalc, version bump to 2.1.1
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8241 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/netcalc/netcalc.mk
+++ package/netcalc/netcalc.mk
@@ -4,20 +4,11 @@
 #
 ################################################################################
 
-NETCALC_VERSION = 2.1-694614e
+NETCALC_VERSION = 2.1.1
 NETCALC_SOURCE = netcalc-$(NETCALC_VERSION).tar.gz
-NETCALC_SITE = http://files.astlinux-project.org
-#NETCALC_SITE = https://github.com/troglobit/netcalc/releases/download/v$(NETCALC_VERSION)
-NETCALC_AUTORECONF = YES
+NETCALC_SITE = https://github.com/troglobit/netcalc/releases/download/v$(NETCALC_VERSION)
 
-## Default install adds ipcalc symlink, confused with Busybox's
-##
-define NETCALC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/netcalc $(TARGET_DIR)/usr/bin/
-endef
-
-define NETCALC_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/netcalc
-endef
+NETCALC_CONF_OPT = \
+	--disable-ipcalc-symlink
 
 $(eval $(call AUTOTARGETS,package,netcalc))
