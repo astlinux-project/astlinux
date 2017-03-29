42324b2f674a15b071ffc62b39a7e91a1d59cb7c
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-03-29 14:12:29 +0000

    ddclient, install the get-ipv6-from-ipv4 script from the ddclient-curl repository, allowing to retrieve a neighboring IPv6 address to be published as an AAAA record
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8243 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/ddclient/ddclient.mk
+++ package/ddclient/ddclient.mk
@@ -10,6 +10,7 @@ DDCLIENT_SITE = https://github.com/astlinux-project/ddclient-curl/releases/downl
 define DDCLIENT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D package/ddclient/dynamicdns.init $(TARGET_DIR)/etc/init.d/dynamicdns
 	$(INSTALL) -m 0755 -D $(@D)/ddclient $(TARGET_DIR)/usr/sbin/ddclient
+	$(INSTALL) -m 0755 -D $(@D)/contrib/get-ipv6-from-ipv4/get-ipv6-from-ipv4.pl $(TARGET_DIR)/usr/sbin/get-ipv6-from-ipv4
 	$(INSTALL) -m 0644 -D package/ddclient/ddclient.conf $(TARGET_DIR)/stat/etc/ddclient.conf
 	ln -sf /tmp/etc/ddclient.conf $(TARGET_DIR)/etc/ddclient.conf
 endef
@@ -17,6 +18,7 @@ endef
 define DDCLIENT_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/etc/init.d/dynamicdns
 	rm -f $(TARGET_DIR)/usr/sbin/ddclient
+	rm -f $(TARGET_DIR)/usr/sbin/get-ipv6-from-ipv4
 	rm -f $(TARGET_DIR)/stat/etc/ddclient.conf
 endef
 
