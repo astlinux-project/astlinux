6607344a0f3d7b83880ac59789577c8dfe710adf
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-04-29 15:56:32 +0000

    logrotate, version bump to 3.12.1, plus upstream build fixes
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8282 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -3,7 +3,7 @@
 # logrotate
 #
 #############################################################
-LOGROTATE_VERSION = 3.11.0
+LOGROTATE_VERSION = 3.12.1
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.gz
 LOGROTATE_SITE = https://github.com/logrotate/logrotate/releases/download/$(LOGROTATE_VERSION)
 
