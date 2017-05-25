6d2757ec34522898e2bdd291f6ff7dcc2f2e78b2
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-05-25 22:42:13 +0000

    sqlite, version bump to 3.19.2
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8342 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3190100
+SQLITE_VERSION = 3190200
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2017
 SQLITE_INSTALL_STAGING = YES
