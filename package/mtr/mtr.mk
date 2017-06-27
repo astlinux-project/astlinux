1b84a6dda5c6e517aaa560f44f86cb893e40cd86
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-06-27 17:35:27 +0000

    mtr, version bump to 0.92, now requires a mtr-packet helper and uses libcap, fix displayed version number
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8416 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/mtr/mtr.mk
+++ package/mtr/mtr.mk
@@ -3,21 +3,25 @@
 # mtr
 #
 #############################################################
-MTR_VERSION = 0.87
+MTR_VERSION = 0.92
 MTR_SITE = ftp://ftp.bitwizard.nl/mtr
 MTR_SOURCE = mtr-$(MTR_VERSION).tar.gz
 
-MTR_DEPENDENCIES = host-pkg-config ncurses
+MTR_DEPENDENCIES = host-pkg-config ncurses libcap
 
 MTR_CONF_OPT = \
+	PACKAGE_VERSION="$(MTR_VERSION)" \
+	PACKAGE_STRING="mtr $(MTR_VERSION)" \
 	--without-gtk
 
 define MTR_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 4711 $(@D)/mtr $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 0755 $(@D)/mtr $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 4711 $(@D)/mtr-packet $(TARGET_DIR)/usr/sbin/
 endef
 
 define MTR_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/mtr
+	rm -f $(TARGET_DIR)/usr/sbin/mtr-packet
 endef
 
 $(eval $(call AUTOTARGETS,package,mtr))
