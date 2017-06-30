d806ea74e981a13f87c5a4d75040445d798c0898
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-06-30 17:40:12 +0000

    acme, version bump to 2.7.2, add David Kerr's 'ssh.sh' deploy script
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8419 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/acme/acme.mk
+++ package/acme/acme.mk
@@ -4,27 +4,28 @@
 #
 ################################################################################
 
-ACME_VERSION = 2.6.5
+ACME_VERSION = 2.7.2
 ACME_SOURCE = acme.sh-$(ACME_VERSION).tar.gz
 ACME_SITE = http://files.astlinux-project.org
 
-ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh $(if $(BR2_PACKAGE_BIND),,dns_nsupdate.sh) README.md
+ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh dns_lexicon.sh $(if $(BR2_PACKAGE_BIND),,dns_nsupdate.sh) README.md
 
 ##
-## curl -L -o dl/acme.sh-2.6.5-2017-01-13.tar.gz https://github.com/Neilpang/acme.sh/archive/master.tar.gz
-## ./scripts/upload-dl-pair dl/acme.sh-2.6.5-2017-01-13.tar.gz
+## curl -L -o dl/acme.sh-2.7.2.tar.gz https://github.com/Neilpang/acme.sh/archive/2.7.2.tar.gz
+## ./scripts/upload-dl-pair dl/acme.sh-2.7.2.tar.gz
 ##
 
 define ACME_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 package/acme/deploy/astlinux.sh $(TARGET_DIR)/stat/etc/acme/deploy/astlinux.sh
+	$(INSTALL) -D -m 0644 package/acme/deploy/ssh.sh $(TARGET_DIR)/stat/etc/acme/deploy/ssh.sh
 	$(INSTALL) -D -m 0755 package/acme/acme-client.sh $(TARGET_DIR)/usr/sbin/acme-client
-	$(INSTALL) -D -m 0755 package/acme/astlinux.sh $(TARGET_DIR)/stat/etc/acme/deploy/astlinux.sh
 	$(INSTALL) -D -m 0755 $(@D)/acme.sh $(TARGET_DIR)/stat/etc/acme/acme.sh
 	cp -a $(@D)/dnsapi $(TARGET_DIR)/stat/etc/acme/
 	ln -s /mnt/kd/acme $(TARGET_DIR)/etc/acme
 	# Remove non-required dnsapi files
 	rm -f $(addprefix $(TARGET_DIR)/stat/etc/acme/dnsapi/, $(ACME_REMOVE_TARGET_DNSAPI))
-	# Make sure the dnsapi scripts are executable
-	find $(TARGET_DIR)/stat/etc/acme/dnsapi/ -name '*.sh' -print0 | xargs -0 chmod 755
+	# Make the dnsapi scripts non-executable, they are sourced by acme.sh
+	find $(TARGET_DIR)/stat/etc/acme/dnsapi/ -name '*.sh' -print0 | xargs -0 chmod 644
 endef
 
 define ACME_UNINSTALL_TARGET_CMDS
