00318e7f5ef75eb9ecfde12c3281eb1acae4405a
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-07-07 18:09:16 +0000

    php, version bump to 5.6.31, security fixes
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8430 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PHP_VERSION = 5.6.30
+PHP_VERSION = 5.6.31
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
