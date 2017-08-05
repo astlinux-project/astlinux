8efe2cd2675314940772cafdce61a7204f128a2c
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-08-05 13:17:48 +0000

    expat, version bump to 2.2.3, no longer requires the XML_POOR_ENTROPY nonsense
    git-svn-id: https://svn.code.sf.net/p/astlinux/code/branches/1.0@8466 57edc7d3-a90e-0410-b364-91459a37fc5a

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-EXPAT_VERSION = 2.2.2
+EXPAT_VERSION = 2.2.3
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_INSTALL_STAGING = YES
@@ -13,11 +13,7 @@ EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
 
 EXPAT_DEPENDENCIES = host-pkg-config
 
-EXPAT_CONF_ENV = CFLAGS='$(TARGET_CFLAGS) -DXML_POOR_ENTROPY'
-
 HOST_EXPAT_DEPENDENCIES = host-pkg-config
-
-HOST_EXPAT_CONF_ENV = CFLAGS='$(HOST_CFLAGS) -DXML_POOR_ENTROPY'
 
 $(eval $(call AUTOTARGETS,package,expat))
 $(eval $(call AUTOTARGETS,package,expat,host))
