db4bfaec46a142e97c8c873a0f595c6d94a16843
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-08-15 11:56:12 -0500

    build system, label versions with 6 character hashes

--- scripts/astlinux_release_version.sh
+++ scripts/astlinux_release_version.sh
@@ -7,13 +7,13 @@ astlinux_release_version()
   if svn info >/dev/null 2>&1; then
     ASTREV="$(LANG=C svn info | awk -F': ' '/^Last Changed Rev:/ { print $2 }')"
     if [ -n "$ASTREV" ]; then
-      ASTGIT="$(svn propget git-commit --revprop -r "$ASTREV" 2>/dev/null | cut -c 1-7)"
+      ASTGIT="$(svn propget git-commit --revprop -r "$ASTREV" 2>/dev/null | cut -c 1-6)"
       if [ -n "$ASTGIT" ]; then
         ASTREV="$ASTREV-$ASTGIT"
       fi
     fi
   else
-    ASTREV="$(git rev-parse --verify --short HEAD 2>/dev/null)"
+    ASTREV="$(git rev-parse --verify HEAD 2>/dev/null | cut -c 1-6)"
   fi
   if [ -z "$ASTREV" ]; then
     ASTREV="unknown"
