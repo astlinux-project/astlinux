0001d0b6ee930cce051a935f8028bfd30947a111
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-08-15 11:34:37 -0500

    build system, add git-commit hash after svn revision number if available

--- scripts/astlinux_release_version.sh
+++ scripts/astlinux_release_version.sh
@@ -6,6 +6,12 @@ astlinux_release_version()
 {
   if svn info >/dev/null 2>&1; then
     ASTREV="$(LANG=C svn info | awk -F': ' '/^Last Changed Rev:/ { print $2 }')"
+    if [ -n "$ASTREV" ]; then
+      ASTGIT="$(svn propget git-commit --revprop -r "$ASTREV" 2>/dev/null | cut -c 1-7)"
+      if [ -n "$ASTGIT" ]; then
+        ASTREV="$ASTREV-$ASTGIT"
+      fi
+    fi
   else
     ASTREV="$(git rev-parse --verify --short HEAD 2>/dev/null)"
   fi
