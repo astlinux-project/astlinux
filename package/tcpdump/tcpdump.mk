5419224420bc81d029802a17de022a2e595046a3
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-09-09 09:17:37 -0500

    tcpdump, version bump to 4.9.2, security fixes: 92 CVE's

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-TCPDUMP_VERSION = 4.9.1
+TCPDUMP_VERSION = 4.9.2
 TCPDUMP_SITE = http://www.tcpdump.org/release
 TCPDUMP_SOURCE = tcpdump-$(TCPDUMP_VERSION).tar.gz
 
