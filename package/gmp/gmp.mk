b10372bbfc9d31caa95e1b2a6500f24eecf97ca4
David Kerr <david@kerr.net>
2017-09-10 18:35:15 -0400

    Version bumb gmp package to 6.1.2.  Required by gnutls version bump

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -1,13 +1,18 @@
-#############################################################
+
+################################################################################
 #
 # gmp
 #
-#############################################################
+################################################################################
 
-GMP_VERSION = 5.0.1
+GMP_VERSION = 6.1.2
 GMP_SITE = $(BR2_GNU_MIRROR)/gmp
-GMP_SOURCE = gmp-$(GMP_VERSION).tar.bz2
+GMP_SOURCE = gmp-$(GMP_VERSION).tar.xz
 GMP_INSTALL_STAGING = YES
+GMP_LICENSE = LGPL-3.0+ or GPL-2.0+
+GMP_LICENSE_FILES = COPYING.LESSERv3 COPYINGv2
+GMP_DEPENDENCIES = host-m4
+HOST_GMP_DEPENDENCIES = host-m4
 
 $(eval $(call AUTOTARGETS,package,gmp))
 $(eval $(call AUTOTARGETS,package,gmp,host))
