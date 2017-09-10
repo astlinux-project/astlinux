f4f9d970febace8c11056cf6073fbf15680852e6
David Kerr <david@kerr.net>
2017-09-10 18:39:32 -0400

    Version bump libgpg-error to 1.8.  Required as I am bumping gnutls to current

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -3,9 +3,9 @@
 # libgpg-error
 #
 #############################################################
-LIBGPG_ERROR_VERSION:=1.8
+LIBGPG_ERROR_VERSION:=1.27
 LIBGPG_ERROR_SOURCE:=libgpg-error-$(LIBGPG_ERROR_VERSION).tar.bz2
-LIBGPG_ERROR_SITE:=ftp://gd.tuwien.ac.at/privacy/gnupg/libgpg-error
+LIBGPG_ERROR_SITE:=ftp://ftp.gnupg.org/gcrypt/libgpg-error
 
 LIBGPG_ERROR_INSTALL_STAGING = YES
 
