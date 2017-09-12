aa703bfc48c0b939348205318387a512bb1fe1fe
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-09-12 16:38:05 -0500

    openvpn, tls-verify script, if /mnt/kd/rc.conf.d/gui.openvpn.conf is newer than /etc/rc.conf source gui.openvpn.conf allowing the latest OVPN_VALIDCLIENTS from the web interface to be used without restarting OpenVPN server

--- package/openvpn/tls-verify.sh
+++ package/openvpn/tls-verify.sh
@@ -5,7 +5,13 @@
 # Return 0 to accept the client certificate
 #
 
-. /etc/rc.conf
+WEB_INTERFACE_GUI_OPENVPN="/mnt/kd/rc.conf.d/gui.openvpn.conf"
+
+if [ "$WEB_INTERFACE_GUI_OPENVPN" -nt /etc/rc.conf ]; then
+  . $WEB_INTERFACE_GUI_OPENVPN
+else
+  . /etc/rc.conf
+fi
 
 if [ "$1" -eq 0 ]; then
   # Extract the CommonName from the X509 subject string
