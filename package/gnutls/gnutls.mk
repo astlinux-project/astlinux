b23d735133e2d5d6f5452e00938d6e55fc589b17
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-09-13 08:11:11 -0500

    build system, followup to 2933802, cleanup build configs we don't have

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -20,7 +20,7 @@ GNUTLS_CONF_OPT = \
 	--with-libunistring-prefix=$(STAGING_DIR)/usr \
 	--with-librt-prefix=$(STAGING_DIR) \
 	--without-tpm \
-  --without-p11-kit \
+	--without-p11-kit \
 	$(if $(BR2_PACKAGE_GNUTLS_TOOLS),--enable-tools,--disable-tools)
 GNUTLS_CONF_ENV = gl_cv_socket_ipv6=yes \
 	ac_cv_header_wchar_h=$(if $(BR2_USE_WCHAR),yes,no) \
