a7c4b2fcf743840037c84efa843fdc03e44a2f70
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-09-14 12:45:10 -0500

    iperf, version bump to 2.0.10

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -3,13 +3,9 @@
 # iperf
 #
 #############################################################
-IPERF_VERSION = 2.0.9
+IPERF_VERSION = 2.0.10
 IPERF_SOURCE = iperf-$(IPERF_VERSION).tar.gz
-IPERF_SITE = https://s3.amazonaws.com/files.astlinux-project
-#IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
-
-IPERF_CONF_ENV = \
-	ac_cv_func_malloc_0_nonnull=yes
+IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
 
 IPERF_CONF_OPT = \
 	--disable-dependency-tracking \
