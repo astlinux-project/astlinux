2378218aae50235e4bb837e88b31696f0ab9f587
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-10-29 10:13:16 -0500

    empty, new package, allows scripting the un-scriptable Similar functionality to 'expect' very small 18 kB binary enabled in Asterisk builds by default

---
+++ package/empty/empty.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# empty
+#
+################################################################################
+
+EMPTY_VERSION = 0.6.20b
+EMPTY_SOURCE = empty-$(EMPTY_VERSION).tgz
+EMPTY_SITE = http://downloads.sourceforge.net/project/empty/empty/empty-$(EMPTY_VERSION)
+
+define EMPTY_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define EMPTY_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/empty $(TARGET_DIR)/usr/bin/empty
+endef
+
+define EMPTY_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/empty
+endef
+
+$(eval $(call GENTARGETS,package,empty))
