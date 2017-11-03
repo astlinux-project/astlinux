2f5a251c83827947c5a9d947f7b08ed7586aea0c
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-11-03 09:29:49 -0500

    wireguard, new package, disabled by default Fast, secure, and modern VPN tunnel

---
+++ package/wireguard/wireguard.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# wireguard
+#
+################################################################################
+
+WIREGUARD_VERSION = 0.0.20171101
+WIREGUARD_SITE = https://git.zx2c4.com/WireGuard/snapshot
+WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VERSION).tar.xz
+WIREGUARD_DEPENDENCIES = host-pkg-config linux libmnl
+
+WIREGUARD_MAKE_OPTS = \
+	WITH_BASHCOMPLETION=no \
+	WITH_WGQUICK=no \
+	WITH_SYSTEMDUNITS=no \
+	KERNELDIR=$(LINUX_DIR)
+
+define WIREGUARD_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(WIREGUARD_MAKE_OPTS) \
+		-C $(@D)/src tools module
+endef
+
+define WIREGUARD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/src/tools/wg $(TARGET_DIR)/usr/bin/wg
+	$(INSTALL) -m 0644 -D $(@D)/src/wireguard.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/net/wireguard/wireguard.ko
+	ln -s /mnt/kd/wireguard $(TARGET_DIR)/etc/wireguard
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+define WIREGUARD_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/wg
+	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/net/wireguard/wireguard.ko
+	rm -f $(TARGET_DIR)/etc/wireguard
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call GENTARGETS,package,wireguard))
