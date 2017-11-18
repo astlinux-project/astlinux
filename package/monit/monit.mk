6d110552325095d0ec72805aaa70a8a72b01348b
Lonnie Abelbeck <lonnie@abelbeck.com>
2017-11-18 09:06:27 -0600

    monit, version bump to 5.25.1, fixes 5.25.0 regression

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONIT_VERSION = 5.25.0
+MONIT_VERSION = 5.25.1
 MONIT_SITE = https://mmonit.com/monit/dist
 MONIT_DEPENDENCIES = host-bison host-flex
 #
