b255b728db19f78320d1b3104085e0c7d39a0045
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-01-27 15:58:18 -0600

    unbound, version bump to 1.6.8, security fix: CVE-2017-15105

--- package/unbound/unbound.mk
+++ package/unbound/unbound.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-UNBOUND_VERSION = 1.6.7
+UNBOUND_VERSION = 1.6.8
 UNBOUND_SITE = https://www.unbound.net/downloads
 UNBOUND_SOURCE = unbound-$(UNBOUND_VERSION).tar.gz
 UNBOUND_INSTALL_STAGING = YES
