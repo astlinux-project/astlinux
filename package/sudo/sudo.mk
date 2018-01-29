5aeaa1cfd65bb0c76168e2e6f6383cc311f00ed5
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-01-29 10:18:57 -0600

    sudo, version bump to 1.8.22

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.8.21p2
+SUDO_VERSION = 1.8.22
 SUDO_SITE = http://www.sudo.ws/sudo/dist
 
 # This is to avoid sudo's make install from chown()ing files which fails
