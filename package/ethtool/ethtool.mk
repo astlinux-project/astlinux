8f8a7b4c5bfc51647195188eb2aa5b61e2f61bfd
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-02-04 13:13:19 -0600

    ethtool, version bump to 4.15

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 4.13
+ETHTOOL_VERSION = 4.15
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 
 ETHTOOL_CONF_OPT = \
