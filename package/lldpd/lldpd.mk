77aebdb592cb04e77a3031b58300e36a51a4b7fa
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-03-03 10:44:02 -0600

    lldpd, new package, custom build only The file /mnt/kd/lldpd.conf must exist for lldpd to start The rc.conf variable LLDP_OPTIONS can be use to specify lldpd options at start

---
+++ package/lldpd/lldpd.mk
@@ -0,0 +1,53 @@
+################################################################################
+#
+# lldpd
+#
+################################################################################
+
+LLDPD_VERSION = 0.9.9
+LLDPD_SITE = https://media.luffy.cx/files/lldpd
+LLDPD_DEPENDENCIES = host-pkg-config
+
+LLDPD_CONF_OPT = \
+	--localstatedir=/var \
+	--with-embedded-libevent \
+	--with-privsep-user=nobody \
+	--with-privsep-group=nobody \
+	--with-privsep-chroot=/var/lib/lldpd \
+	--without-snmp \
+	--without-xml \
+	--without-seccomp \
+	--disable-doxygen-doc
+
+ifeq ($(BR2_PACKAGE_READLINE),y)
+LLDPD_DEPENDENCIES += readline
+LLDPD_CONF_OPT += --with-readline
+else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+LLDPD_DEPENDENCIES += libedit
+LLDPD_CONF_OPT += --with-readline
+else
+LLDPD_CONF_OPT += --without-readline
+endif
+
+define LLDPD_POST_INSTALL
+	$(INSTALL) -m 0755 -D package/lldpd/lldpd.init $(TARGET_DIR)/etc/init.d/lldpd
+	ln -sf /tmp/etc/lldpd.conf $(TARGET_DIR)/etc/lldpd.conf
+	ln -sf ../../init.d/lldpd $(TARGET_DIR)/etc/runlevels/default/S12lldpd
+	ln -sf ../../init.d/lldpd $(TARGET_DIR)/etc/runlevels/default/K28lldpd
+	# Remove extra installed data files
+	rm -rf $(TARGET_DIR)/etc/lldpd.d
+	rm -rf $(TARGET_DIR)/usr/share/bash-completion
+	rm -rf $(TARGET_DIR)/usr/share/zsh
+endef
+LLDPD_POST_INSTALL_TARGET_HOOKS += LLDPD_POST_INSTALL
+
+define LLDPD_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/lldp*
+	rm -f $(TARGET_DIR)/usr/lib/liblldpctl*
+	rm -f $(TARGET_DIR)/etc/init.d/lldpd
+	rm -f $(TARGET_DIR)/etc/lldpd.conf
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S12lldpd
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K28lldpd
+endef
+
+$(eval $(call AUTOTARGETS,package,lldpd))
