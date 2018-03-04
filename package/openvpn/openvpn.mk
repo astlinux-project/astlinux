3c4a49b0248ba1cf7cece55bc2fbf29446c1914b
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-03-04 14:45:47 -0600

    openvpn, version bump to 2.4.5

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENVPN_VERSION = 2.4.4
+OPENVPN_VERSION = 2.4.5
 OPENVPN_SITE = http://swupdate.openvpn.net/community/releases
 OPENVPN_DEPENDENCIES = host-pkg-config openssl
 
