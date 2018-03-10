9d605ca87b7d0ada1835b60d96c3d8b8d6463d4f
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-03-10 16:20:53 -0600

    logrotate, version bump to 3.14.0

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -3,7 +3,7 @@
 # logrotate
 #
 #############################################################
-LOGROTATE_VERSION = 3.13.0
+LOGROTATE_VERSION = 3.14.0
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.gz
 LOGROTATE_SITE = https://github.com/logrotate/logrotate/releases/download/$(LOGROTATE_VERSION)
 
