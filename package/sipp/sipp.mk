0028c4aee7819ad0cb6a7f34e8e01852708775d5
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-03-22 14:56:49 -0500

    sipp, new package SIPp - a SIP protocol testing tool disabled for standard builds

---
+++ package/sipp/sipp.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# sipp
+#
+################################################################################
+
+SIPP_VERSION = 3.5.1
+SIPP_SOURCE = sipp-$(SIPP_VERSION).tar.gz
+SIPP_SITE = https://github.com/SIPp/sipp/releases/download/v$(SIPP_VERSION)
+SIPP_DEPENDENCIES = libpcap ncurses openssl
+
+SIPP_CONF_OPT = \
+	--with-pcap \
+	--with-openssl
+
+$(eval $(call AUTOTARGETS,package,sipp))
