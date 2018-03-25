71ee22820044c0bd6897431f902a065fce54545f
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-03-25 10:29:30 -0500

    util-linux, major version bump to 2.28.2 Note this package has far-reaching effects, not only the AstLinux image also the initrd image, the runnix image and is used to create bootable images via the build system. Commands fdisk and losetup are two such commands that must be installed.
    For those performing custom builds, 'make oldconfig' and accept the defaults should properly
    update your config.  Be sure by diff'ing with the standard build AstLinux configs as a sanity check.

--- package/fdisk/fdisk.mk
+++ package/fdisk/fdisk.mk
@@ -5,44 +5,93 @@
 # Used by: ./scripts/astlinux-makeimage
 #
 #############################################################
-FDISK_VERSION = $(FDISK_VERSION_MAJOR).1
-FDISK_VERSION_MAJOR = 2.20
-FDISK_SOURCE = util-linux-$(FDISK_VERSION).tar.bz2
-FDISK_SITE = http://www.kernel.org/pub/linux/utils/util-linux/v$(FDISK_VERSION_MAJOR)
+
+FDISK_VERSION_MAJOR = 2.28
+FDISK_VERSION = $(FDISK_VERSION_MAJOR).2
+FDISK_SOURCE = util-linux-$(FDISK_VERSION).tar.xz
+FDISK_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/util-linux/v$(FDISK_VERSION_MAJOR)
 
 HOST_FDISK_DEPENDENCIES = host-pkg-config
 HOST_FDISK_CONF_ENV = scanf_cv_type_modifier=no
 
-HOST_FDISK_CONF_OPT += --disable-rpath --disable-makeinstall-chown --without-ncurses
+HOST_FDISK_CONF_OPT += \
+	--enable-static \
+	--disable-shared \
+	--disable-rpath \
+	--disable-makeinstall-chown \
+	--disable-nls \
+	--disable-bash-completion \
+	--without-bashcompletiondir \
+	--without-tinfo \
+	--without-readline \
+	--without-utempter \
+	--without-cap-ng \
+	--without-user \
+	--without-audit \
+	--without-udev \
+	--without-python \
+	--without-btrfs \
+	--without-systemd \
+	--without-systemdsystemunitdir \
+	--without-ncurses
 
 HOST_FDISK_CONF_OPT += \
-	--disable-agetty \
-	--disable-arch \
-	--disable-cramfs \
-	--disable-ddate \
-	--disable-fallocate \
-	--disable-fsck \
-	--disable-kill \
-	--disable-libblkid \
+	--enable-all-programs \
+	--enable-libblkid \
+	--enable-libfdisk \
+	--enable-libsmartcols \
+	--enable-libuuid \
 	--disable-libmount \
-	--disable-libuuid \
+	--disable-agetty \
+	--disable-bfs \
+	--disable-cal \
+	--disable-chfn-chsh \
+	--disable-cramfs \
+	--disable-eject \
+	--disable-fallocate \
+	--disable-fdformat \
+	--disable-fsck \
+	--disable-hwclock \
+	--disable-ipcrm \
+	--disable-ipcs \
+	--disable-kill \
 	--disable-last \
+	--disable-line \
+	--disable-logger \
+	--disable-last --disable-login --disable-runuser --disable-su --disable-sulogin \
+	--disable-losetup \
+	--disable-lslogins \
 	--disable-mesg \
+	--disable-minix \
+	--disable-more \
 	--disable-mount \
+	--disable-mountpoint \
+	--disable-newgrp \
+	--disable-nologin \
+	--disable-nsenter \
 	--disable-partx \
+	--disable-pg \
 	--disable-pivot_root \
 	--disable-raw \
 	--disable-rename \
 	--disable-reset \
 	--disable-schedutils \
+	--disable-setpriv \
+	--disable-setterm \
 	--disable-switch_root \
+	--disable-tunelp \
+	--disable-ul \
 	--disable-unshare \
+	--disable-utmpdump \
 	--disable-uuidd \
+	--disable-vipw \
 	--disable-wall \
-	--disable-write
+	--disable-wdctl \
+	--disable-write \
+	--disable-zramctl
 
 define HOST_FDISK_INSTALL_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/fdisk/fdisk $(HOST_DIR)/usr/sbin/fdisk
+	$(INSTALL) -m 0755 -D $(@D)/fdisk $(HOST_DIR)/usr/sbin/fdisk
 endef
 
 $(eval $(call AUTOTARGETS,package,fdisk,host))
--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -3,16 +3,35 @@
 # util-linux
 #
 #############################################################
+
+UTIL_LINUX_VERSION_MAJOR = 2.28
 UTIL_LINUX_VERSION = $(UTIL_LINUX_VERSION_MAJOR).2
-UTIL_LINUX_VERSION_MAJOR = 2.22
-UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VERSION).tar.bz2
-UTIL_LINUX_SITE = http://www.kernel.org/pub/linux/utils/util-linux/v$(UTIL_LINUX_VERSION_MAJOR)
-UTIL_LINUX_AUTORECONF = YES
+UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VERSION).tar.xz
+UTIL_LINUX_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/util-linux/v$(UTIL_LINUX_VERSION_MAJOR)
 UTIL_LINUX_INSTALL_STAGING = YES
 UTIL_LINUX_DEPENDENCIES = host-pkg-config
 UTIL_LINUX_CONF_ENV = scanf_cv_type_modifier=no
 
-UTIL_LINUX_CONF_OPT += --disable-rpath --disable-makeinstall-chown
+UTIL_LINUX_CONF_OPT += \
+	--localstatedir=/var/run \
+	--libdir=/usr/lib \
+	--disable-static \
+	--disable-rpath \
+	--disable-makeinstall-chown \
+	--disable-nls \
+	--disable-bash-completion \
+	--without-bashcompletiondir \
+	--without-tinfo \
+	--without-readline \
+	--without-utempter \
+	--without-cap-ng \
+	--without-user \
+	--without-audit \
+	--without-udev \
+	--without-python \
+	--without-btrfs \
+	--without-systemd \
+	--without-systemdsystemunitdir
 
 # If both util-linux and busybox are selected, make certain util-linux
 # wins the fight over who gets to have their utils actually installed
@@ -26,39 +45,81 @@ else
 UTIL_LINUX_CONF_OPT += --without-ncurses
 endif
 
-ifeq ($(BR2_PACKAGE_LIBINTL),y)
-UTIL_LINUX_DEPENDENCIES += libintl
-UTIL_LINUX_MAKE_OPT += LIBS=-lintl
-endif
-
 # Disable/Enable utilities
 UTIL_LINUX_CONF_OPT += \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_BINARIES),--enable-all-programs,--disable-all-programs) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_AGETTY),--enable-agetty,--disable-agetty) \
-	$(if $(BR2_PACKAGE_UTIL_LINUX_ARCH),--enable-arch,--disable-arch) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_BFS),--enable-bfs,--disable-bfs) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_CAL),--enable-cal,--disable-cal) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_CHFN_CHSH),--enable-chfn-chsh,--disable-chfn-chsh) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_CRAMFS),--enable-cramfs,--disable-cramfs) \
-	--disable-ddate \
-	--disable-eject \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_EJECT),--enable-eject,--disable-eject) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_FALLOCATE),--enable-fallocate,--disable-fallocate) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_FDFORMAT),--enable-fdformat,--disable-fdformat) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_FSCK),--enable-fsck,--disable-fsck) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_HWCLOCK),--enable-hwclock,--disable-hwclock) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_IPCRM),--enable-ipcrm,--disable-ipcrm) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_IPCS),--enable-ipcs,--disable-ipcs) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_KILL),--enable-kill,--disable-kill) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LAST),--enable-last,--disable-last) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBBLKID),--enable-libblkid,--disable-libblkid) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBFDISK),--enable-libfdisk,--disable-libfdisk) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBMOUNT),--enable-libmount,--disable-libmount) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBSMARTCOLS),--enable-libsmartcols,--disable-libsmartcols) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_LIBUUID),--enable-libuuid,--disable-libuuid) \
-	$(if $(BR2_PACKAGE_UTIL_LINUX_LOGIN_UTILS),--enable-last --enable-login --enable-su --enable-sulogin,--disable-last --disable-login --disable-su --disable-sulogin) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LINE),--enable-line,--disable-line) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LOGGER),--enable-logger,--disable-logger) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LOGIN_UTILS),--enable-last --enable-login --enable-runuser --enable-su --enable-sulogin,--disable-last --disable-login --disable-runuser --disable-su --disable-sulogin) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LOSETUP),--enable-losetup,--disable-losetup) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_LSLOGINS),--enable-lslogins,--disable-lslogins) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_MESG),--enable-mesg,--disable-mesg) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_MINIX),--enable-minix,--disable-minix) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_MORE),--enable-more,--disable-more) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_MOUNT),--enable-mount,--disable-mount) \
-	$(if $(BR2_PACKAGE_UTIL_LINUX_PARTX),,--disable-partx) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_MOUNTPOINT),--enable-mountpoint,--disable-mountpoint) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_NEWGRP),--enable-newgrp,--disable-newgrp) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_NOLOGIN),--enable-nologin,--disable-nologin) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_NSENTER),--enable-nsenter,--disable-nsenter) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_PARTX),--enable-partx,--disable-partx) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_PG),--enable-pg,--disable-pg) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_PIVOT_ROOT),--enable-pivot_root,--disable-pivot_root) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_RAW),--enable-raw,--disable-raw) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_RENAME),--enable-rename,--disable-rename) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_RESET),--enable-reset,--disable-reset) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_SCHEDUTILS),--enable-schedutils,--disable-schedutils) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_SETPRIV),--enable-setpriv,--disable-setpriv) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_SETTERM),--enable-setterm,--disable-setterm) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_SWITCH_ROOT),--enable-switch_root,--disable-switch_root) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_TUNELP),--enable-tunelp,--disable-tunelp) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_UL),--enable-ul,--disable-ul) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_UNSHARE),--enable-unshare,--disable-unshare) \
-	--disable-utmpdump \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_UTMPDUMP),--enable-utmpdump,--disable-utmpdump) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_UUIDD),--enable-uuidd,--disable-uuidd) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_VIPW),--enable-vipw,--disable-vipw) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_WALL),--enable-wall,--disable-wall) \
-	$(if $(BR2_PACKAGE_UTIL_LINUX_WRITE),--enable-write,--disable-write)
+	$(if $(BR2_PACKAGE_UTIL_LINUX_WDCTL),--enable-wdctl,--disable-wdctl) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_WRITE),--enable-write,--disable-write) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_ZRAMCTL),--enable-zramctl,--disable-zramctl)
+
+
+define UTIL_LINUX_REMOVE_BINARIES
+	## Removing selected /sbin binaries
+	for i in ctrlaltdel fsfreeze blkdiscard chcpu wipefs mkfs mkswap swaplabel sfdisk cfdisk; do \
+	  rm -f $(TARGET_DIR)/sbin/$$i ; \
+	done
+	## Removing selected /usr/bin binaries
+	for i in colcrt colrm column rev tailf script scriptreplay flock ipcmk ipcrm lsns look mcookie namei; do \
+	  rm -f $(TARGET_DIR)/usr/bin/$$i ; \
+	done
+	## Removing selected /usr/sbin binaries
+	for i in readprofile ldattach rtcwake; do \
+	  rm -f $(TARGET_DIR)/usr/sbin/$$i ; \
+	done
+endef
+
+ifeq ($(BR2_PACKAGE_UTIL_LINUX_MINIMAL_BINARIES),y)
+UTIL_LINUX_POST_INSTALL_TARGET_HOOKS = UTIL_LINUX_REMOVE_BINARIES
+endif
 
 $(eval $(call AUTOTARGETS,package,util-linux))
 
