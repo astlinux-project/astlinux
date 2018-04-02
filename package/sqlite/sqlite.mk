71ce897e8683524cfd935979b6ab7880364e523c
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-04-02 08:56:15 -0500

    sqlite, version bump to 3.23.0

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3220000
+SQLITE_VERSION = 3230000
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2018
 SQLITE_INSTALL_STAGING = YES
