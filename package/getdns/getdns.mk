a512b4f151faf43524ecda2c98049bd66bf4347b
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-05-05 10:25:52 -0500

    getdns/stubby, version bump to 1.4.2-rc1/0.2.3-rc1

--- package/getdns/getdns.mk
+++ package/getdns/getdns.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-GETDNS_VERSION = 1.4.1
+GETDNS_VERSION = 1.4.2-rc1
 GETDNS_SITE = https://getdnsapi.net/dist
 GETDNS_SOURCE = getdns-$(GETDNS_VERSION).tar.gz
 GETDNS_INSTALL_STAGING = YES
