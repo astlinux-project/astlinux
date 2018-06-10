8f6b2b54570bc29e3d82b42a01bb3b9c324c3c10
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-06-10 14:30:24 -0500

    stunnel, version bump to 5.46

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.44
-STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/5.x
+STUNNEL_VERSION = 5.46
+STUNNEL_SITE = https://www.usenix.org.uk/mirrors/stunnel/archive/5.x
 STUNNEL_DEPENDENCIES = openssl
 
 STUNNEL_CONF_OPT = \
