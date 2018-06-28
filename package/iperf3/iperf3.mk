b14c69a9c051d3826cd38e38c653be759951d09e
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-06-28 09:45:05 -0500

    iperf3, version bump to 3.6

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.5
+IPERF3_VERSION = 3.6
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION).tar.gz
-IPERF3_SITE = http://downloads.es.net/pub/iperf
+IPERF3_SITE = https://downloads.es.net/pub/iperf
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 IPERF3_CONF_OPT += --with-openssl=$(STAGING_DIR)/usr
