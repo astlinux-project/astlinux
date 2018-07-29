0b00c264aff2115d5468a2d449e8bb4c1f621196
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-07-29 13:50:47 -0500

    keepalived, adjust runlevel order start keepalived after iptables

--- package/keepalived/keepalived.mk
+++ package/keepalived/keepalived.mk
@@ -20,16 +20,16 @@ define KEEPALIVED_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/keepalived/keepalived $(TARGET_DIR)/usr/sbin/keepalived
 	$(INSTALL) -m 0755 -D package/keepalived/keepalived.init $(TARGET_DIR)/etc/init.d/keepalived
 	ln -sf /tmp/etc/keepalived $(TARGET_DIR)/etc/keepalived
-	ln -sf ../../init.d/keepalived $(TARGET_DIR)/etc/runlevels/default/S04keepalived
-	ln -sf ../../init.d/keepalived $(TARGET_DIR)/etc/runlevels/default/K89keepalived
+	ln -sf ../../init.d/keepalived $(TARGET_DIR)/etc/runlevels/default/S12keepalived
+	ln -sf ../../init.d/keepalived $(TARGET_DIR)/etc/runlevels/default/K28keepalived
 endef
 
 define KEEPALIVED_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/keepalived
 	rm -f $(TARGET_DIR)/etc/init.d/keepalived
 	rm -f $(TARGET_DIR)/etc/keepalived
-	rm -f $(TARGET_DIR)/etc/runlevels/default/S04keepalived
-	rm -f $(TARGET_DIR)/etc/runlevels/default/K89keepalived
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S12keepalived
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K28keepalived
 endef
 
 $(eval $(call AUTOTARGETS,package,keepalived))
