fd2f58f13a013b08257bb496a1035d0ba40a3a5a
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-08-03 08:24:03 -0500

    wireguard, version bump to 0.0.20180802

--- package/wireguard/wireguard.mk
+++ package/wireguard/wireguard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_VERSION = 0.0.20180731
+WIREGUARD_VERSION = 0.0.20180802
 WIREGUARD_SITE = https://git.zx2c4.com/WireGuard/snapshot
 WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VERSION).tar.xz
 WIREGUARD_DEPENDENCIES = host-pkg-config linux libmnl
