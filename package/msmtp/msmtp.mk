ee9e641274d5ec696f77f4949f3111ca37f1d9d3
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-08-16 14:33:42 -0500

    mail, separate msmtp and msmtpd Allow msmtpd to start much earlier on startup, setup sendmail spool directory and listen on 127.0.0.1:25. Later msmtp will send any spooled email generated by msmtpd

--- package/msmtp/msmtp.mk
+++ package/msmtp/msmtp.mk
@@ -22,6 +22,7 @@ define MSMTP_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/src/msmtp $(TARGET_DIR)/usr/sbin/msmtp
 	$(INSTALL) -m 0755 -D $(@D)/src/msmtpd $(TARGET_DIR)/usr/sbin/msmtpd
 	$(INSTALL) -m 0755 -D package/msmtp/msmtp.init $(TARGET_DIR)/etc/init.d/msmtp
+	$(INSTALL) -m 0755 -D package/msmtp/msmtpd.init $(TARGET_DIR)/etc/init.d/msmtpd
 	$(INSTALL) -m 0755 -D package/msmtp/msmtpqueue.sh $(TARGET_DIR)/usr/sbin/msmtpqueue
 	$(INSTALL) -m 0755 -D package/msmtp/sendmail.sh $(TARGET_DIR)/usr/sbin/sendmail
 	$(INSTALL) -m 0755 -D package/msmtp/testmail.sh $(TARGET_DIR)/usr/sbin/testmail
@@ -29,12 +30,17 @@ define MSMTP_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D package/msmtp/mail.sh $(TARGET_DIR)/bin/mail
 	ln -sf ../../bin/mail $(TARGET_DIR)/usr/bin/mail
 	ln -sf /tmp/etc/msmtprc $(TARGET_DIR)/etc/msmtprc
+	ln -sf ../../init.d/msmtp $(TARGET_DIR)/etc/runlevels/default/S34msmtp
+	ln -sf ../../init.d/msmtp $(TARGET_DIR)/etc/runlevels/default/K24msmtp
+	ln -sf ../../init.d/msmtpd $(TARGET_DIR)/etc/runlevels/default/S08msmtpd
+	ln -sf ../../init.d/msmtpd $(TARGET_DIR)/etc/runlevels/default/K30msmtpd
 endef
 
 define MSMTP_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/msmtp
 	rm -f $(TARGET_DIR)/usr/sbin/msmtpd
 	rm -f $(TARGET_DIR)/etc/init.d/msmtp
+	rm -f $(TARGET_DIR)/etc/init.d/msmtpd
 	rm -f $(TARGET_DIR)/usr/sbin/msmtpqueue
 	rm -f $(TARGET_DIR)/usr/sbin/sendmail
 	rm -f $(TARGET_DIR)/usr/sbin/testmail
@@ -42,6 +48,10 @@ define MSMTP_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/bin/mail
 	rm -f $(TARGET_DIR)/usr/bin/mail
 	rm -f $(TARGET_DIR)/etc/msmtprc
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S34msmtp
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K24msmtp
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S08msmtpd
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K30msmtpd
 endef
 
 $(eval $(call AUTOTARGETS,package,msmtp))
