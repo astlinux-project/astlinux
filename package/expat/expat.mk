0297bf4ed745da0280e4b21e6888c40beaa023d1
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-08-17 09:25:51 -0500

    expat, version bump to 2.2.6

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-EXPAT_VERSION = 2.2.5
-EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
+EXPAT_VERSION = 2.2.6
+EXPAT_SITE = https://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_INSTALL_STAGING = YES
 
