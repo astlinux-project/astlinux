1dc3c273cb68596cf95725bfbe91181bb0358872
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-08-20 12:13:15 -0500

    acme, version bump to 2.7.9 Remove added dnsapi's that require external commands: dns_cyon.sh dns_inwx.sh Remove local deploy and dnsapi scripts that are now upstream Thanks to David Kerr

--- package/acme/acme.mk
+++ package/acme/acme.mk
@@ -4,27 +4,24 @@
 #
 ################################################################################
 
-ACME_VERSION = 2.7.2
+ACME_VERSION = 2.7.9
 ACME_SOURCE = acme.sh-$(ACME_VERSION).tar.gz
 ACME_SITE = https://s3.amazonaws.com/files.astlinux-project
 
-ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh dns_lexicon.sh $(if $(BR2_PACKAGE_BIND),,dns_nsupdate.sh) README.md
+ACME_REMOVE_TARGET_DNSAPI = dns_myapi.sh dns_lexicon.sh dns_cyon.sh dns_inwx.sh $(if $(BR2_PACKAGE_BIND),,dns_nsupdate.sh) README.md
 
 ##
-## curl -L -o dl/acme.sh-2.7.2.tar.gz https://github.com/Neilpang/acme.sh/archive/2.7.2.tar.gz
-## ./scripts/upload-dl-pair dl/acme.sh-2.7.2.tar.gz
+## curl -L -o dl/acme.sh-2.7.9.tar.gz https://github.com/Neilpang/acme.sh/archive/2.7.9.tar.gz
+## ./scripts/upload-dl-pair dl/acme.sh-2.7.9.tar.gz
 ##
 
 define ACME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 package/acme/deploy/astlinux.sh $(TARGET_DIR)/stat/etc/acme/deploy/astlinux.sh
 	$(INSTALL) -D -m 0644 package/acme/deploy/custom.sh $(TARGET_DIR)/stat/etc/acme/deploy/custom.sh
-	$(INSTALL) -D -m 0644 package/acme/deploy/ssh.sh $(TARGET_DIR)/stat/etc/acme/deploy/ssh.sh
-	$(INSTALL) -D -m 0644 package/acme/dnsapi/dns_dyn.sh $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_dyn.sh
-	$(INSTALL) -D -m 0644 package/acme/dnsapi/dns_duckdns.sh $(TARGET_DIR)/stat/etc/acme/dnsapi/dns_duckdns.sh
 	$(INSTALL) -D -m 0755 package/acme/acme-client.sh $(TARGET_DIR)/usr/sbin/acme-client
 	$(INSTALL) -D -m 0755 $(@D)/acme.sh $(TARGET_DIR)/stat/etc/acme/acme.sh
 	cp -a $(@D)/dnsapi $(TARGET_DIR)/stat/etc/acme/
-	ln -s /mnt/kd/acme $(TARGET_DIR)/etc/acme
+	ln -sf /mnt/kd/acme $(TARGET_DIR)/etc/acme
 	# Remove non-required dnsapi files
 	rm -f $(addprefix $(TARGET_DIR)/stat/etc/acme/dnsapi/, $(ACME_REMOVE_TARGET_DNSAPI))
 	# Make the dnsapi scripts non-executable, they are sourced by acme.sh
