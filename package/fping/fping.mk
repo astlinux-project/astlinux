7af3b9e3d5c7fcb3fe99911dcbad5c56171f06a5
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-10-20 16:17:18 -0500

    fping, major version bump to 4.1 Now handles both IPv4 and IPv6 in a single binary, fping6 is a sym-link for compatibility

--- package/fping/fping.mk
+++ package/fping/fping.mk
@@ -3,17 +3,17 @@
 # fping
 #
 #############################################################
-FPING_VERSION = 3.16
+
+FPING_VERSION = 4.1
 FPING_SITE = https://fping.org/dist
 FPING_SOURCE = fping-$(FPING_VERSION).tar.gz
-
-FPING_CONF_OPT = \
-	--enable-ipv6
+# Fix configure warning
+FPING_AUTORECONF = YES
 
 define FPING_INSTALL_TARGET_CMDS
 	# Must set SUID for zabbix user
-	$(INSTALL) -D -m 4711 $(@D)/src/fping $(TARGET_DIR)/usr/sbin/
-	$(INSTALL) -D -m 4711 $(@D)/src/fping6 $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -D -m 4711 $(@D)/src/fping $(TARGET_DIR)/usr/sbin/fping
+	ln -sf fping $(TARGET_DIR)/usr/sbin/fping6
 endef
 
 define FPING_UNINSTALL_TARGET_CMDS
