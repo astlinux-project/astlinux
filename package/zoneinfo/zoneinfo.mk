bf47a9ab3b8bf73e558ea60174c4b9e3214de3fa
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-10-21 16:24:33 -0500

    zoneinfo, version bump to 2018f and PHP_TIMEZONEDB 2018.6

--- package/zoneinfo/zoneinfo.mk
+++ package/zoneinfo/zoneinfo.mk
@@ -3,7 +3,7 @@
 # zoneinfo
 #
 ##############################################################
-ZONEINFO_VERSION := 2018e
+ZONEINFO_VERSION := 2018f
 ZONEINFO_DATA := tzdata$(ZONEINFO_VERSION).tar.gz
 ZONEINFO_SOURCE := tzcode$(ZONEINFO_VERSION).tar.gz
 ZONEINFO_SITE := http://www.iana.org/time-zones/repository/releases
