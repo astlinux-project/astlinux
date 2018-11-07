baca1168a3159a8e20bc658a0ecff78fb1c06b2f
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-11-07 08:13:00 -0600

    libpng, major version bump to 1.6.35 currently not enabled

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -1,35 +1,35 @@
 #############################################################
 #
-# libpng (Portable Network Graphic library)
+# libpng
 #
 #############################################################
-LIBPNG_VERSION = 1.4.8
-LIBPNG_SERIES = 14
-LIBPNG_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libpng
-LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.bz2
+
+LIBPNG_VERSION = 1.6.35
+LIBPNG_SERIES = 16
+LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
+LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng$(LIBPNG_SERIES)/$(LIBPNG_VERSION)
 LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkg-config zlib
 
 HOST_LIBPNG_DEPENDENCIES = host-pkg-config host-zlib
 
-define LIBPNG_STAGING_LIBPNG12_CONFIG_FIXUP
+LIBPNG_CONF_OPT = \
+	--disable-hardware-optimizations
+
+define LIBPNG_STAGING_LIBPNG_SERIES_CONFIG_FIXUP
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" \
 		-e "s,^exec_prefix=.*,exec_prefix=\'$(STAGING_DIR)/usr\',g" \
 		-e "s,^includedir=.*,includedir=\'$(STAGING_DIR)/usr/include/libpng$(LIBPNG_SERIES)\',g" \
 		-e "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" \
 		$(STAGING_DIR)/usr/bin/libpng$(LIBPNG_SERIES)-config
 endef
-
-LIBPNG_POST_INSTALL_STAGING_HOOKS += LIBPNG_STAGING_LIBPNG12_CONFIG_FIXUP
+LIBPNG_POST_INSTALL_STAGING_HOOKS += LIBPNG_STAGING_LIBPNG_SERIES_CONFIG_FIXUP
 
 define LIBPNG_REMOVE_CONFIG_SCRIPTS
 	$(RM) -f $(TARGET_DIR)/usr/bin/libpng$(LIBPNG_SERIES)-config \
 		 $(TARGET_DIR)/usr/bin/libpng-config
 endef
-
-ifneq ($(BR2_HAVE_DEVFILES),y)
 LIBPNG_POST_INSTALL_TARGET_HOOKS += LIBPNG_REMOVE_CONFIG_SCRIPTS
-endif
 
 $(eval $(call AUTOTARGETS,package,libpng))
 $(eval $(call AUTOTARGETS,package,libpng,host))
