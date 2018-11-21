a29b3c6c139e430f51751226f98e7cae577c23b8
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-11-21 17:40:42 -0600

    Linux version bump from linux-3.16.57 to linux-3.16.61

--- crosstool-ng-src/get-files.sh
+++ crosstool-ng-src/get-files.sh
@@ -4,7 +4,7 @@ FILES_URL="http://files.astlinux-project.org"
 
 TARBALLS=".build/tarballs"
 
-LINUX_KERNEL="linux-3.16.57.tar.gz"
+LINUX_KERNEL="linux-3.16.61.tar.gz"
 
 EGLIBC="eglibc-2_18.tar.bz2"
 
