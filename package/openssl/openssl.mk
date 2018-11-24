6aa024402137ce47fc80a150dd46b2d4e7f4def0
Lonnie Abelbeck <lonnie@abelbeck.com>
2018-11-24 10:10:29 -0600

    openssl, version bump to 1.0.2q Security fixes: CVE-2018-0734, CVE-2018-5407

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.2p
+OPENSSL_VERSION = 1.0.2q
 OPENSSL_SITE = https://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
