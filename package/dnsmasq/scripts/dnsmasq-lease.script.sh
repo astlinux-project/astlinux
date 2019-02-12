e1245eb10a4d5512521abf586a5585310e4ee313
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-02-12 16:56:00 -0600

    dnsmasq, dnsmasq-lease.script, use 'Start Time' rather then 'Expire Time' since dnsmasq does not call the 'dhcp-script' for lease expire updates Add web interface support as well

--- package/dnsmasq/scripts/dnsmasq-lease.script.sh
+++ package/dnsmasq/scripts/dnsmasq-lease.script.sh
@@ -6,6 +6,7 @@ address="$3"
 dns_name="$4"
 
 old_ipv6_mac=""
+old_time_secs=""
 old_vendor=""
 old_hostname=""
 
@@ -40,6 +41,7 @@ get_old_del_db()
     if [ $ip6 -eq 1 ]; then
       old_ipv6_mac="$(echo "$entry" | cut -d'~' -f3)"
     fi
+    old_time_secs="$(echo "$entry" | cut -d'~' -f5)"
     old_vendor="$(echo "$entry" | cut -d'~' -f7)"
     old_hostname="$(echo "$entry" | cut -d'~' -f8-)"
 
@@ -60,7 +62,7 @@ del_db()
 
 add_db()
 {
-  local mac mac_vendor vendor hostname
+  local mac mac_vendor time_secs vendor hostname
 
   if [ $ip6 -eq 1 ]; then
     mac="${DNSMASQ_MAC:-$old_ipv6_mac}"
@@ -70,10 +72,11 @@ add_db()
     mac_vendor="$mac_duid"
   fi
 
+  time_secs="${old_time_secs:-$(date +%s)}"
   vendor="${old_vendor:-$(mac2vendor $mac_vendor)}"
   hostname="${DNSMASQ_SUPPLIED_HOSTNAME:-$old_hostname}"
 
-  echo "${mac_duid}~${address}~${mac}~${DNSMASQ_INTERFACE}~${DNSMASQ_LEASE_EXPIRES}~${dns_name}~${vendor}~${hostname}" >> "$DB"
+  echo "${mac_duid}~${address}~${mac}~${DNSMASQ_INTERFACE}~${time_secs}~${dns_name}~${vendor}~${hostname}" >> "$DB"
 }
 
 ## main
