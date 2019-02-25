cdafccde46784ed0bc6eecf1bd575f13a089ee17
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-02-25 15:11:58 -0600

    keepalived, version bump to 2.0.13 Disable old linkbeat code, because now we can

--- package/keepalived/keepalived.mk
+++ package/keepalived/keepalived.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEEPALIVED_VERSION = 2.0.12
+KEEPALIVED_VERSION = 2.0.13
 KEEPALIVED_SOURCE = keepalived-$(KEEPALIVED_VERSION).tar.gz
 KEEPALIVED_SITE = http://www.keepalived.org/software
 KEEPALIVED_DEPENDENCIES = host-pkg-config linux openssl
@@ -16,6 +16,7 @@ KEEPALIVED_CONF_OPT = \
 	--disable-libipset \
 	--disable-libiptc \
 	--disable-track-process \
+	--disable-linkbeat \
 	--with-init=SYSV
 
 define KEEPALIVED_INSTALL_TARGET_CMDS
