cd839fb3623d9ce6a0d763cb28e435e47109f7d2
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-02-26 14:40:50 -0600

    openssl, version bump to 1.0.2r security fix: CVE-2019-1559

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.2q
+OPENSSL_VERSION = 1.0.2r
 OPENSSL_SITE = https://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
