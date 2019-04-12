c056fce6fb2793862eb890eaa39128673ae22b36
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-04-12 09:59:49 -0500

    keepalived, version bump to 2.0.15

--- package/keepalived/keepalived.mk
+++ package/keepalived/keepalived.mk
@@ -4,13 +4,10 @@
 #
 ################################################################################
 
-KEEPALIVED_VERSION = 2.0.14
+KEEPALIVED_VERSION = 2.0.15
 KEEPALIVED_SOURCE = keepalived-$(KEEPALIVED_VERSION).tar.gz
 KEEPALIVED_SITE = http://www.keepalived.org/software
 KEEPALIVED_DEPENDENCIES = host-pkg-config linux openssl
-
-# upstream patch
-KEEPALIVED_AUTORECONF = YES
 
 KEEPALIVED_CONF_OPT = \
 	--disable-warnings \
