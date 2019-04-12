3c02ae87006709e6ce99267a19f7396d8698ee17
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-04-12 09:49:06 -0500

    whois, version bump to 5.4.2

--- package/whois/whois.mk
+++ package/whois/whois.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WHOIS_VERSION = 5.4.1
+WHOIS_VERSION = 5.4.2
 WHOIS_SITE = http://ftp.debian.org/debian/pool/main/w/whois
 WHOIS_SOURCE = whois_$(WHOIS_VERSION).tar.xz
 
