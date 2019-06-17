bda27bb6695e2ee779e17dd9e0d462faf4f49cad
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-06-17 14:56:45 -0500

    htop, version bump to 2.2.0 Major bump from 1.0.3

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -4,18 +4,15 @@
 #
 #############################################################
 
-HTOP_VERSION = 1.0.3
+HTOP_VERSION = 2.2.0
 HTOP_SOURCE = htop-$(HTOP_VERSION).tar.gz
-HTOP_SITE = http://hisham.hm/htop/releases/$(HTOP_VERSION)
+HTOP_SITE = https://hisham.hm/htop/releases/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
-HTOP_AUTORECONF = YES
+# Prevent htop build system from searching the host paths
+HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/$(NCURSES_CONFIG_SCRIPTS)
 
 HTOP_CONF_OPT = \
 	--disable-unicode
-
-HTOP_CONF_ENV = \
-	ac_cv_file__proc_stat=yes \
-	ac_cv_file__proc_meminfo=yes
 
 define HTOP_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/htop $(TARGET_DIR)/usr/bin/htop
