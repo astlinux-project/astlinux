1b8dd41aac07ef7b225a3bc1815d1206a5d391cb
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-07-18 10:17:07 -0500

    fop2, version bump to 2.31.24

--- package/asterisk-fop2/asterisk-fop2.mk
+++ package/asterisk-fop2/asterisk-fop2.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ASTERISK_FOP2_VERSION = 2.31.23
+ASTERISK_FOP2_VERSION = 2.31.24
 ASTERISK_FOP2_SOURCE = fop2-$(ASTERISK_FOP2_VERSION)-debian-i386.tgz
 ASTERISK_FOP2_SITE = http://download2.fop2.com
 # Note: be sure to edit "project/astlinux/target_skeleton/usr/sbin/upgrade-package" on version change
