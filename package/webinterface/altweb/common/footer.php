1d6df497044adf369f8c4d074359e2c0f7ffc182
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-09-04 09:34:51 -0500

    webinterface, All tabs, add a 'Goto Top' button on the upper-right when scrolled down such that tabs are not shown. Thanks to David Kerr for his insights

--- package/webinterface/altweb/common/footer.php
+++ package/webinterface/altweb/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copyright (C) 2008 Lonnie Abelbeck
+// Copyright (C) 2008-2019 Lonnie Abelbeck
 // This is free software, licensed under the GNU General Public License
 // version 3 as published by the Free Software Foundation; you can
 // redistribute it and/or modify it under the terms of the GNU
@@ -8,7 +8,32 @@
 
 // footer.php for AstLinux
 // 03-25-2008
+// 09-03-2019, Add Goto-Top button
 
 ?>
+<button onclick="gototop_action()" id="gttBtn" title="Goto Top">&uarr;</button>
+<script language="JavaScript" type="text/javascript">
+//<![CDATA[
+// When the user clicks on the button, scroll to the top of the document
+function gototop_action() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function set_gototop_scroll() {
+  if (document.body.scrollTop > 48 || document.documentElement.scrollTop > 48) {
+    document.getElementById("gttBtn").style.display = "block";
+  } else {
+    document.getElementById("gttBtn").style.display = "none";
+  }
+}
+
+function setGTThandler() {
+  set_gototop_scroll();
+  window.onscroll = set_gototop_scroll;
+}
+setGTThandler();
+//]]>
+</script>
 </body>
 </html>
