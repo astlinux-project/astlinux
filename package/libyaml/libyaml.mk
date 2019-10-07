2fbbe171f072841fbdcad5b0f79b9d23b13e7098
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-10-07 17:26:46 -0500

    libyaml, version bump to 0.2.2

--- package/libyaml/libyaml.mk
+++ package/libyaml/libyaml.mk
@@ -3,10 +3,10 @@
 # libyaml
 #
 #############################################################
-LIBYAML_VERSION = 0.1.7
+
+LIBYAML_VERSION = 0.2.2
 LIBYAML_SOURCE = yaml-$(LIBYAML_VERSION).tar.gz
-LIBYAML_SITE = http://pyyaml.org/download/libyaml
+LIBYAML_SITE = https://pyyaml.org/download/libyaml
 LIBYAML_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS,package,libyaml))
-
