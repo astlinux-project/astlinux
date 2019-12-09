cda0cd946d03a081a017f51ee825b36b76a8d0b8
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-12-09 09:31:19 -0600

    ca-certificates, version bump to 2019-11-27

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2019-10-16
+CA_CERTIFICATES_VERSION = 2019-11-27
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
