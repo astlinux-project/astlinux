f83ea70a637fb38a447b8a1f61da71ca2c7ccb01
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-12-26 16:07:20 -0600

    wireguard, only the kernel module is installed via the wireguard package, The new wireguard-tools package builds 'wg', wireguard.init and supporting scripts

---
+++ package/wireguard-tools/wireguard-tools.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# wireguard-tools
+#
+################################################################################
+
+WIREGUARD_TOOLS_VERSION = 1.0.20191226
+WIREGUARD_TOOLS_SITE = https://git.zx2c4.com/wireguard-tools/snapshot
+WIREGUARD_TOOLS_SOURCE = wireguard-tools-$(WIREGUARD_TOOLS_VERSION).tar.xz
+WIREGUARD_TOOLS_DEPENDENCIES = host-pkg-config libmnl
+
+WIREGUARD_TOOLS_MAKE_OPTS = \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	WITH_BASHCOMPLETION=no \
+	WITH_WGQUICK=no \
+	WITH_SYSTEMDUNITS=no
+
+define WIREGUARD_TOOLS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(WIREGUARD_TOOLS_MAKE_OPTS) \
+		-C $(@D)/src wg
+endef
+
+define WIREGUARD_TOOLS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/src/wg $(TARGET_DIR)/usr/bin/wg
+	$(INSTALL) -m 0755 -D package/wireguard-tools/wireguard-monitor $(TARGET_DIR)/usr/sbin/wireguard-monitor
+	$(INSTALL) -m 0755 -D package/wireguard-tools/wireguard-mobile-client $(TARGET_DIR)/usr/sbin/wireguard-mobile-client
+	$(INSTALL) -m 0755 -D package/wireguard-tools/wireguard.init $(TARGET_DIR)/etc/init.d/wireguard
+	ln -sf /tmp/etc/wireguard $(TARGET_DIR)/etc/wireguard
+	ln -sf ../../init.d/wireguard $(TARGET_DIR)/etc/runlevels/default/S31wireguard
+	ln -sf ../../init.d/wireguard $(TARGET_DIR)/etc/runlevels/default/K20wireguard
+endef
+
+define WIREGUARD_TOOLS_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/wg
+	rm -f $(TARGET_DIR)/usr/sbin/wireguard-monitor
+	rm -f $(TARGET_DIR)/usr/sbin/wireguard-mobile-client
+	rm -f $(TARGET_DIR)/etc/init.d/wireguard
+	rm -f $(TARGET_DIR)/etc/wireguard
+	rm -f $(TARGET_DIR)/etc/runlevels/default/S31wireguard
+	rm -f $(TARGET_DIR)/etc/runlevels/default/K20wireguard
+endef
+
+$(eval $(call GENTARGETS,package,wireguard-tools))
--- package/wireguard/wireguard.mk
+++ package/wireguard/wireguard.mk
@@ -4,46 +4,29 @@
 #
 ################################################################################
 
-WIREGUARD_VERSION = 0.0.20191212
-WIREGUARD_SITE = https://git.zx2c4.com/WireGuard/snapshot
-WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VERSION).tar.xz
+WIREGUARD_VERSION = 0.0.20191226
+WIREGUARD_SITE = https://git.zx2c4.com/wireguard-linux-compat/snapshot
+WIREGUARD_SOURCE = wireguard-linux-compat-$(WIREGUARD_VERSION).tar.xz
 WIREGUARD_DEPENDENCIES = host-pkg-config linux libmnl
 
 WIREGUARD_MAKE_OPTS = \
 	CC=$(TARGET_CC) \
 	LD=$(TARGET_LD) \
-	WITH_BASHCOMPLETION=no \
-	WITH_WGQUICK=no \
-	WITH_SYSTEMDUNITS=no \
 	KERNELRELEASE=$(LINUX_VERSION_PROBED) \
 	KERNELDIR=$(LINUX_DIR)
 
 define WIREGUARD_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(WIREGUARD_MAKE_OPTS) \
-		-C $(@D)/src tools module
+		-C $(@D)/src module
 endef
 
 define WIREGUARD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/src/tools/wg $(TARGET_DIR)/usr/bin/wg
 	$(INSTALL) -m 0644 -D $(@D)/src/wireguard.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/wireguard/wireguard.ko
-	$(INSTALL) -m 0755 -D package/wireguard/wireguard-monitor $(TARGET_DIR)/usr/sbin/wireguard-monitor
-	$(INSTALL) -m 0755 -D package/wireguard/wireguard-mobile-client $(TARGET_DIR)/usr/sbin/wireguard-mobile-client
-	$(INSTALL) -m 0755 -D package/wireguard/wireguard.init $(TARGET_DIR)/etc/init.d/wireguard
-	ln -sf /tmp/etc/wireguard $(TARGET_DIR)/etc/wireguard
-	ln -sf ../../init.d/wireguard $(TARGET_DIR)/etc/runlevels/default/S31wireguard
-	ln -sf ../../init.d/wireguard $(TARGET_DIR)/etc/runlevels/default/K20wireguard
 	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
 endef
 
 define WIREGUARD_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/wg
 	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/wireguard/wireguard.ko
-	rm -f $(TARGET_DIR)/usr/sbin/wireguard-monitor
-	rm -f $(TARGET_DIR)/usr/sbin/wireguard-mobile-client
-	rm -f $(TARGET_DIR)/etc/init.d/wireguard
-	rm -f $(TARGET_DIR)/etc/wireguard
-	rm -f $(TARGET_DIR)/etc/runlevels/default/S31wireguard
-	rm -f $(TARGET_DIR)/etc/runlevels/default/K20wireguard
 	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) -r $(LINUX_VERSION_PROBED)
 endef
 
