a46b9dc621d458f59fccba7d4be4d6898255a5be
Lonnie Abelbeck <lonnie@abelbeck.com>
2019-12-27 10:09:46 -0600

    zabbix, version bump to 3.0.29

--- package/zabbix/zabbix.mk
+++ package/zabbix/zabbix.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ZABBIX_VERSION = 3.0.28
+ZABBIX_VERSION = 3.0.29
 ZABBIX_SOURCE = zabbix-$(ZABBIX_VERSION).tar.gz
 ZABBIX_SITE = http://downloads.sourceforge.net/sourceforge/zabbix
 
