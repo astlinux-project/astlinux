5c02f35cef5ede7b77562687c4f62ab427125cec
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-01-27 14:04:18 -0600

    php, version bump to 7.2.27 security fixes: CVE-2020-7060, CVE-2020-7059

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PHP_VERSION = 7.2.26
+PHP_VERSION = 7.2.27
 PHP_SITE = https://secure.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
