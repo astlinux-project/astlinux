4c6ce51478c74ec416be9ba6de71d724141c0b98
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-02-01 15:46:13 -0600

    sqlite, version bump to 3.31.1

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3300100
+SQLITE_VERSION = 3310100
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
-SQLITE_SITE = https://www.sqlite.org/2019
+SQLITE_SITE = https://www.sqlite.org/2020
 SQLITE_INSTALL_STAGING = YES
 
 # sqlite-0001-editline-configure-fix.patch
