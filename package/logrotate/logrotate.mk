944000b100a98764acc72b931f9c2973c1078608
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-03-05 15:11:36 -0600

    logrotate, version bump to 3.16.0

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LOGROTATE_VERSION = 3.15.1
+LOGROTATE_VERSION = 3.16.0
 LOGROTATE_SOURCE = logrotate-$(LOGROTATE_VERSION).tar.gz
 LOGROTATE_SITE = https://github.com/logrotate/logrotate/releases/download/$(LOGROTATE_VERSION)
 
