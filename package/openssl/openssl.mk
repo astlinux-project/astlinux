1c77a6a83938edc266fff976e1c77de7532191cd
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-03-19 17:29:47 -0500

    openssl, version bump to 1.1.1e Security fix: CVE-2019-1551

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.1.1d
+OPENSSL_VERSION = 1.1.1e
 OPENSSL_SITE = https://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
