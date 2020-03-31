c36f9b9413eb9f5c6249302e49ca56b46db7cec2
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-03-31 11:09:45 -0500

    wireguard, version bump to 1.0.20200330

--- package/wireguard/wireguard.mk
+++ package/wireguard/wireguard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_VERSION = 0.0.20200318
+WIREGUARD_VERSION = 1.0.20200330
 WIREGUARD_SITE = https://git.zx2c4.com/wireguard-linux-compat/snapshot
 WIREGUARD_SOURCE = wireguard-linux-compat-$(WIREGUARD_VERSION).tar.xz
 WIREGUARD_DEPENDENCIES = linux
