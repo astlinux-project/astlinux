2061424173dfb194ec3c595d32a1254339f363e6
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-04-20 16:31:45 -0500

    libyaml, version bump to 0.2.3

--- package/libyaml/libyaml.mk
+++ package/libyaml/libyaml.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBYAML_VERSION = 0.2.2
+LIBYAML_VERSION = 0.2.3
 LIBYAML_SOURCE = yaml-$(LIBYAML_VERSION).tar.gz
 LIBYAML_SITE = https://pyyaml.org/download/libyaml
 LIBYAML_INSTALL_STAGING = YES
