3670ab3605e33886b2184a557c6d145fe9508373
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-04-20 10:43:42 -0500

    chrony, version bump to 4.0-pre2

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHRONY_VERSION = 4.0-pre1
+CHRONY_VERSION = 4.0-pre2
 CHRONY_SITE = https://download.tuxfamily.org/chrony
 CHRONY_DEPENDENCIES = host-pkg-config libcap
 
