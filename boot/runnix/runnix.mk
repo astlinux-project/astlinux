ada7c7bdef1db84cecf0494f8dd85f9ea4d7771e
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-04-28 11:57:25 -0500

    runnix, version bump to 0.6.0, now on official repo

--- boot/runnix/runnix.mk
+++ boot/runnix/runnix.mk
@@ -4,10 +4,9 @@
 #
 #############################################################
 
-RUNNIX_VERSION = 0.6.0d
+RUNNIX_VERSION = 0.6.0
 RUNNIX_SOURCE = runnix-$(RUNNIX_VERSION).tar.gz
-RUNNIX_SITE = https://s3.amazonaws.com/mirror.astlinux-project/runnix5
-RUNNIX_SITE = https://abelbeck.com/lonnie/runnix6
+RUNNIX_SITE = https://s3.amazonaws.com/mirror.astlinux-project/runnix6
 RUNNIX_DEPENDENCIES = host-fdisk
 
 RUNFS_DIR = $(BUILD_DIR)/runfs
