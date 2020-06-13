8df4cc68ef6b883e9e75393a1c491474f7eed64b
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-06-13 10:40:26 -0500

    iperf3, profiling is disabled by default BTW using --disable-profiling will enable profiling

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -8,9 +8,6 @@ IPERF3_VERSION = 3.8.1
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION).tar.gz
 IPERF3_SITE = https://downloads.es.net/pub/iperf
 
-IPERF3_CONF_OPT = \
-	--disable-profiling
-
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 IPERF3_CONF_OPT += --with-openssl=$(STAGING_DIR)/usr
 IPERF3_DEPENDENCIES += openssl
