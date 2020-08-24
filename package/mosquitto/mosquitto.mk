3bd4a38c6b38f61926a85511e15574c5be4a96f2
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-08-24 13:43:39 -0500

    mosquitto, version bump to 1.6.12 (Custom Build Only)

--- package/mosquitto/mosquitto.mk
+++ package/mosquitto/mosquitto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOSQUITTO_VERSION = 1.6.11
+MOSQUITTO_VERSION = 1.6.12
 MOSQUITTO_SITE = https://mosquitto.org/files/source
 MOSQUITTO_INSTALL_STAGING = YES
 MOSQUITTO_DEPENDENCIES = host-pkg-config
