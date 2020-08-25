a93f5b896b7bc25af48913039b8a2b160c7536e5
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-08-25 14:18:11 -0500

    iptables, major version bump to 1.8.4

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -4,26 +4,31 @@
 #
 #############################################################
 
-IPTABLES_VERSION = 1.4.21
+IPTABLES_VERSION = 1.8.4
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
-IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
+IPTABLES_SITE = https://netfilter.org/projects/iptables/files
 IPTABLES_INSTALL_STAGING = YES
 IPTABLES_DEPENDENCIES = host-pkg-config
 
 IPTABLES_CONF_OPT = \
 	--libexecdir=/usr/lib \
-	--with-kernel=$(STAGING_DIR)/usr
+	--with-kernel=$(STAGING_DIR)/usr \
+	--with-xt-lock-name=/var/lock/xtables.lock \
+	--disable-nftables \
+	--disable-connlabel \
+	--disable-bpf-compiler \
+	--disable-nfsynproxy
 
 define IPTABLES_TARGET_SYMLINK_CREATE
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables-save
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables-restore
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/iptables
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/iptables-save
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/iptables-restore
 endef
 
 define IPTABLES_TARGET_IPV6_SYMLINK_CREATE
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables-save
-	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables-restore
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/ip6tables
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/ip6tables-save
+	ln -sf xtables-legacy-multi $(TARGET_DIR)/usr/sbin/ip6tables-restore
 endef
 
 define IPTABLES_TARGET_IPV6_REMOVE
@@ -47,7 +52,7 @@ IPTABLES_POST_INSTALL_TARGET_HOOKS += IPTABLES_TARGET_INSTALL
 define IPTABLES_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/iptables-xml
 	rm -f $(TARGET_DIR)/usr/sbin/iptables* $(TARGET_DIR)/usr/sbin/ip6tables*
-	rm -f $(TARGET_DIR)/usr/sbin/xtables-multi
+	rm -f $(TARGET_DIR)/usr/sbin/xtables-legacy-multi
 	rm -rf $(TARGET_DIR)/usr/lib/xtables
 endef
 
