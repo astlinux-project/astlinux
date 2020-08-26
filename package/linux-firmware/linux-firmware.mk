abfb5bbeef1646f8a09b80dd3f1269ca5a8a3e9e
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-08-26 13:07:27 -0500

    linux-firmware, new package to provide binary blobs for the r8169 and tg3 NIC drivers.
    Remove r8168 in the default rc.modules and map r8168 -> r8169.

---
+++ package/linux-firmware/linux-firmware.mk
@@ -0,0 +1,56 @@
+################################################################################
+#
+# linux-firmware
+#
+################################################################################
+
+LINUX_FIRMWARE_VERSION = 20200122
+LINUX_FIRMWARE_SOURCE = linux-firmware-$(LINUX_FIRMWARE_VERSION).tar.xz
+LINUX_FIRMWARE_SITE = $(BR2_KERNEL_MIRROR)/linux/kernel/firmware
+LINUX_FIRMWARE_DEPENDENCIES = linux
+
+##
+## Configured for Linux Kernel 4.19
+## Use 'modinfo' to determine the 'firmware:' entries
+##
+
+## RealTek RTL-8169 Gigabit Ethernet driver
+LINUX_FIRMWARE_FILES += \
+	rtl_nic/rtl8107e-2.fw \
+	rtl_nic/rtl8107e-1.fw \
+	rtl_nic/rtl8168h-2.fw \
+	rtl_nic/rtl8168h-1.fw \
+	rtl_nic/rtl8168g-3.fw \
+	rtl_nic/rtl8168g-2.fw \
+	rtl_nic/rtl8106e-2.fw \
+	rtl_nic/rtl8106e-1.fw \
+	rtl_nic/rtl8411-2.fw \
+	rtl_nic/rtl8411-1.fw \
+	rtl_nic/rtl8402-1.fw \
+	rtl_nic/rtl8168f-2.fw \
+	rtl_nic/rtl8168f-1.fw \
+	rtl_nic/rtl8105e-1.fw \
+	rtl_nic/rtl8168e-3.fw \
+	rtl_nic/rtl8168e-2.fw \
+	rtl_nic/rtl8168e-1.fw \
+	rtl_nic/rtl8168d-2.fw \
+	rtl_nic/rtl8168d-1.fw
+
+## Broadcom Tigon3 ethernet driver
+LINUX_FIRMWARE_FILES += \
+	tigon/tg3_tso5.bin \
+	tigon/tg3_tso.bin \
+	tigon/tg3.bin
+
+define LINUX_FIRMWARE_INSTALL_FILES
+	cd $(@D) && \
+		$(TAR) cf install.tar $(sort $(LINUX_FIRMWARE_FILES)) && \
+		$(TAR) xf install.tar -C $(TARGET_DIR)/lib/firmware
+endef
+
+define LINUX_FIRMWARE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/lib/firmware
+	$(LINUX_FIRMWARE_INSTALL_FILES)
+endef
+
+$(eval $(call GENTARGETS,package,linux-firmware))
