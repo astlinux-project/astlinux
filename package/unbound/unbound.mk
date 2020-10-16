6dffacf2f4b0fd169772d82445df17e7165294d5
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-10-16 14:46:14 -0500

    unbound, version bump to 1.12.0

--- package/unbound/unbound.mk
+++ package/unbound/unbound.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-UNBOUND_VERSION = 1.11.0
+UNBOUND_VERSION = 1.12.0
 UNBOUND_SITE = https://www.unbound.net/downloads
 UNBOUND_SOURCE = unbound-$(UNBOUND_VERSION).tar.gz
 UNBOUND_INSTALL_STAGING = YES
