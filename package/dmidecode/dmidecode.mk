5294ae8e487d036c4705ae4230eb2c2651ccd5e0
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-10-16 13:46:10 -0500

    dmidecode, version bump to 3.3 (Custom Build Only)

--- package/dmidecode/dmidecode.mk
+++ package/dmidecode/dmidecode.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-DMIDECODE_VERSION = 3.2
+DMIDECODE_VERSION = 3.3
 DMIDECODE_SITE = https://download.savannah.gnu.org/releases/dmidecode
 DMIDECODE_SOURCE = dmidecode-$(DMIDECODE_VERSION).tar.xz
 
