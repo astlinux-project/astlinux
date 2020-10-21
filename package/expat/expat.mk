f5a1dd4b8980fff90dd76f5fac7d78274ed3e510
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-10-21 10:35:32 -0500

    expat, version bump to 2.2.10

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-EXPAT_VERSION = 2.2.9
-EXPAT_SITE = https://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
+EXPAT_VERSION = 2.2.10
+EXPAT_SITE = https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$(EXPAT_VERSION))
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_INSTALL_STAGING = YES
 
@@ -14,7 +14,11 @@ EXPAT_DEPENDENCIES = host-pkg-config
 HOST_EXPAT_DEPENDENCIES = host-pkg-config
 
 EXPAT_CONF_OPT = \
+	--without-docbook \
 	--without-xmlwf
+
+HOST_EXPAT_CONF_OPT = \
+	--without-docbook
 
 $(eval $(call AUTOTARGETS,package,expat))
 $(eval $(call AUTOTARGETS,package,expat,host))
