093442b717be555e1015f3d19a2b17054863f3f0
Lonnie Abelbeck <lonnie@abelbeck.com>
2020-12-16 15:17:07 -0600

    usbutils, major version bump to 013
    While udev's hwdb is checked for manufacturer and product, the fallback
    is to use /sys/bus/usb/devices which seems fine without 6.5 MB of data.
    
    Much of the reorganization follow this Buildroot commit:
    https://git.buildroot.net/buildroot/commit/package/usbutils?id=7d925ffcbfd5d78707c5fc87bc4dc52df0b57e58

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,48 +4,23 @@
 #
 #############################################################
 
-USBUTILS_VERSION = 007
+USBUTILS_VERSION = 013
 USBUTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/usb/usbutils
-USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.gz
-USBUTILS_DEPENDENCIES = host-pkg-config libusb
-USBUTILS_INSTALL_STAGING = YES
-
-USBUTILS_CONF_OPT = --disable-zlib
+USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.xz
+USBUTILS_DEPENDENCIES = host-pkg-config libusb udev
+# Missing configure script
+USBUTILS_AUTORECONF = YES
 
 # Build after busybox since it's got a lightweight lsusb
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
-	USBUTILS_DEPENDENCIES += busybox
-endif
-
-# Nice lsusb.py script only if there's python
-ifeq ($(BR2_PACKAGE_PYTHON),)
-define USBUTILS_REMOVE_PYTHON
-	rm -f $(TARGET_DIR)/usr/bin/lsusb.py
-endef
-
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_PYTHON
+USBUTILS_DEPENDENCIES += busybox
 endif
 
 define USBUTILS_TARGET_CLEANUP
+	rm -f $(TARGET_DIR)/usr/bin/lsusb.py
 	rm -f $(TARGET_DIR)/usr/bin/usb-devices
-	rm -f $(TARGET_DIR)/usr/sbin/update-usbids.sh
-	rm -f $(TARGET_DIR)/usr/share/pkgconfig/usbutils.pc
 endef
 
 USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_TARGET_CLEANUP
-
-define USBUTILS_REMOVE_COMPRESSED_IDS
-	rm -f $(TARGET_DIR)/usr/share/usb.ids.gz
-endef
-
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_COMPRESSED_IDS
-
-define USBUTILS_REMOVE_DEVFILES
-	rm -f $(TARGET_DIR)/usr/bin/libusb-config
-endef
-
-ifneq ($(BR2_HAVE_DEVFILES),y)
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_DEVFILES
-endif
 
 $(eval $(call AUTOTARGETS,package,usbutils))
