45cfec948e0d849281d57124debef04647c624aa
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-01-06 15:44:32 -0600

    pppd, version bump to 2.4.9 Disable the new EAP-TLS support Now requires libpcap to build, so use it Remove the radius stuff to clean-up pppd.mk Remove the BR2_PACKAGE_PPPD_FILTER and BR2_PACKAGE_PPPD_RADIUS selectors
    Add minor tweaks to the pppoe-start script

--- package/pppd/pppd.mk
+++ package/pppd/pppd.mk
@@ -4,29 +4,18 @@
 #
 #############################################################
 
-PPPD_VERSION = 2.4.8
+PPPD_VERSION = 2.4.9
 PPPD_SOURCE = ppp-$(PPPD_VERSION).tar.gz
 PPPD_SITE = https://github.com/paulusmack/ppp/archive/ppp-$(PPPD_VERSION)
-PPPD_DEPENDENCIES = openssl
+PPPD_DEPENDENCIES = openssl libpcap
 
 PPPD_INSTALL_STAGING = YES
 PPPD_TARGET_BINS = chat pppd pppdump pppstats
-PPPD_RADIUS_CONF = dictionary dictionary.ascend dictionary.compat \
-			dictionary.merit dictionary.microsoft \
-			issue port-id-map realms servers radiusclient.conf
 
-ifeq ($(BR2_PACKAGE_PPPD_FILTER),y)
-PPPD_DEPENDENCIES += libpcap
-PPPD_MAKE_OPT += FILTER=y
-endif
-
-ifeq ($(BR2_INET_IPV6),y)
-PPPD_MAKE_OPT += HAVE_INET6=y
-endif
+PPPD_MAKE_OPT =
 
 define PPPD_CONFIGURE_CMDS
-	$(SED) 's/FILTER=y/#FILTER=y/' $(PPPD_DIR)/pppd/Makefile.linux
-	$(SED) 's/ifneq ($$(wildcard \/usr\/include\/pcap-bpf.h),)/ifdef FILTER/' $(PPPD_DIR)/*/Makefile.linux
+	$(SED) 's/^USE_EAPTLS=y/#USE_EAPTLS=y/' $(PPPD_DIR)/pppd/Makefile.linux
 	( cd $(@D); $(TARGET_MAKE_ENV) ./configure --prefix=/usr )
 endef
 
@@ -39,29 +28,7 @@ define PPPD_UNINSTALL_TARGET_CMDS
 	rm -f $(addprefix $(TARGET_DIR)/usr/sbin/, $(PPPD_TARGET_BINS))
 	rm -f $(TARGET_DIR)/usr/sbin/pppoe-*
 	rm -rf $(TARGET_DIR)/usr/lib/pppd
-	rm -rf $(TARGET_DIR)/etc/ppp/radius
 endef
-
-ifeq ($(BR2_PACKAGE_PPPD_RADIUS),y)
-define PPPD_INSTALL_RADIUS
-	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/radius/radattr.so \
-		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/radattr.so
-	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/radius/radius.so \
-		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/radius.so
-	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/radius/radrealms.so \
-		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/radrealms.so
-	for m in $(PPPD_RADIUS_CONF); do \
-		$(INSTALL) -m 644 -D $(PPPD_DIR)/pppd/plugins/radius/etc/$$m \
-			$(TARGET_DIR)/etc/ppp/radius/$$m; \
-	done
-	$(SED) 's:/usr/local/etc:/etc:' \
-		$(TARGET_DIR)/etc/ppp/radius/radiusclient.conf
-	$(SED) 's:/usr/local/sbin:/usr/sbin:' \
-		$(TARGET_DIR)/etc/ppp/radius/radiusclient.conf
-	$(SED) 's:/etc/radiusclient:/etc/ppp/radius:g' \
-		$(TARGET_DIR)/etc/ppp/radius/*
-endef
-endif
 
 define PPPD_INSTALL_TARGET_CMDS
 	for sbin in $(PPPD_TARGET_BINS); do \
@@ -82,9 +49,9 @@ define PPPD_INSTALL_TARGET_CMDS
 #	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/pppoatm/pppoatm.so \
 #		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/pppoatm.so
 ##
-	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/rp-pppoe/rp-pppoe.so \
+	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/pppoe/pppoe.so \
 		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/pppoe.so
-	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/rp-pppoe/pppoe-discovery \
+	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/pppoe/pppoe-discovery \
 		$(TARGET_DIR)/usr/sbin/pppoe-discovery
 	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/winbind.so \
 		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/winbind.so
@@ -92,7 +59,6 @@ define PPPD_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/openl2tp.so
 	$(INSTALL) -D $(PPPD_DIR)/pppd/plugins/pppol2tp/pppol2tp.so \
 		$(TARGET_DIR)/usr/lib/pppd/$(PPPD_VERSION)/pppol2tp.so
-	$(PPPD_INSTALL_RADIUS)
 	ln -snf /tmp/etc/ppp $(TARGET_DIR)/etc/ppp
 endef
 
