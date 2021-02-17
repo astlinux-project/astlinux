84669741731ba67e975aa19c105c77f3eeef5cf2
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-02-17 11:17:20 -0600

    openssl, version bump to 1.1.1j security fixes: CVE-2021-23840, CVE-2021-23841

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.1.1i
+OPENSSL_VERSION = 1.1.1j
 OPENSSL_SITE = https://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
