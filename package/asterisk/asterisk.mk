608f1c5de5a58d74bf0300fd10c3a34ebc5fded8
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-02-20 10:32:10 -0600

    asterisk, version bump to 13.38.2 and 16.16.1 add sRTP replay patch to 13se 13.29.2 version

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v13),y)
-ASTERISK_VERSION := 13.38.1
+ASTERISK_VERSION := 13.38.2
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v16),y)
-ASTERISK_VERSION := 16.16.0
+ASTERISK_VERSION := 16.16.1
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 13.29.2
