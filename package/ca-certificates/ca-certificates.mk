60784d0b4cdf10e44370458b2d02d68a9f078576
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-02-21 09:58:05 -0600

    ca-certificates, add symlink for common Debian ca-certificates.crt

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -14,14 +14,17 @@ endef
 
 define CA_CERTIFICATES_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/lib/ssl/certs
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/ssl/certs
 	$(INSTALL) -m 0444 -D $(@D)/cacert.pem $(TARGET_DIR)/usr/share/ca-certificates/ca-bundle.crt
 	ln -sf /usr/share/ca-certificates/ca-bundle.crt $(TARGET_DIR)/usr/lib/ssl/certs/ca-bundle.crt
 	ln -sf /usr/share/ca-certificates/ca-bundle.crt $(TARGET_DIR)/usr/lib/ssl/cert.pem
+	ln -sf /usr/share/ca-certificates/ca-bundle.crt $(TARGET_DIR)/etc/ssl/certs/ca-certificates.crt
 endef
 
 define CA_CERTIFICATES_UNINSTALL_TARGET_CMDS
 	rm -f  $(TARGET_DIR)/usr/lib/ssl/certs/ca-bundle.crt
 	rm -f  $(TARGET_DIR)/usr/lib/ssl/cert.pem
+	rm -f  $(TARGET_DIR)/etc/ssl/certs/ca-certificates.crt
 	rm -rf $(TARGET_DIR)/usr/share/ca-certificates
 endef
 
