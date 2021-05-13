4d51b28ec21b560c83d0e3ef090234748ac0f047
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-05-13 11:04:19 -0500

    chrony, version bump to 4.1

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHRONY_VERSION = 4.0
+CHRONY_VERSION = 4.1
 CHRONY_SITE = https://download.tuxfamily.org/chrony
 CHRONY_DEPENDENCIES = host-pkg-config libcap
 
