c8f50549f198c2be50e4f88a0340578cf80a4e66
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-05-13 14:58:09 -0500

    prosody, major version bump to 0.11.9 Security fixes: CVE-2021-32917, CVE-2021-32918, CVE-2021-32919, CVE-2021-32920, CVE-2021-32921
    Note: Requires much testing.

--- package/prosody/prosody.mk
+++ package/prosody/prosody.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PROSODY_VERSION = 0.10.3
+PROSODY_VERSION = 0.11.9
 PROSODY_SOURCE = prosody-$(PROSODY_VERSION).tar.gz
 PROSODY_SITE = https://prosody.im/downloads/source
 PROSODY_DEPENDENCIES = lua libidn openssl luafilesystem luaexpat luasocket luasec
