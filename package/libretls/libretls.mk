d5ff0c5fd12cae0c1bcec413b619fcbe91391edf
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-05-25 13:25:42 -0500

    libretls, version bump to 3.3.3p1 Now includes our missing 'wrap-compiler-for-flag-check' script fix.

--- package/libretls/libretls.mk
+++ package/libretls/libretls.mk
@@ -4,16 +4,11 @@
 #
 #############################################################
 
-LIBRETLS_VERSION = 3.3.3
+LIBRETLS_VERSION = 3.3.3p1
 LIBRETLS_SITE = https://causal.agency/libretls
 LIBRETLS_INSTALL_STAGING = YES
 
 LIBRETLS_DEPENDENCIES = host-pkg-config openssl
-
-define LIBRETLS_FIXUP_SCRIPTS
-	$(INSTALL) -m 0755 -D package/libretls/scripts/wrap-compiler-for-flag-check $(@D)/scripts/wrap-compiler-for-flag-check
-endef
-LIBRETLS_POST_EXTRACT_HOOKS = LIBRETLS_FIXUP_SCRIPTS
 
 LIBRETLS_CONF_OPT = \
 	--with-openssl=$(STAGING_DIR)/usr \
