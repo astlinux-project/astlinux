7e6d5283748639d42010fe5a92c60277076b8677
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-05-26 10:46:19 -0500

    zabbix, version bump to 4.0.31

--- package/zabbix/zabbix.mk
+++ package/zabbix/zabbix.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ZABBIX_VERSION = 4.0.30
+ZABBIX_VERSION = 4.0.31
 ZABBIX_SOURCE = zabbix-$(ZABBIX_VERSION).tar.gz
 ZABBIX_SITE = https://cdn.zabbix.com/zabbix/sources/stable/4.0
 
