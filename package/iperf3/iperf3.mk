905df554408bc4766706a56681ea7d5310edf265
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-05-28 14:35:51 -0500

    iperf3, version bump to 3.10

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.9
+IPERF3_VERSION = 3.10
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION).tar.gz
 IPERF3_SITE = https://downloads.es.net/pub/iperf
+# patch configure.ac
+IPERF3_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 IPERF3_CONF_OPT += --with-openssl=$(STAGING_DIR)/usr
