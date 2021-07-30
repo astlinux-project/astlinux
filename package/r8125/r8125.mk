6172b204deb67097736b2fe94236258fa1378cb6
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-07-30 14:18:50 -0500

    New r8125 package for Realtek RTL8125B NIC 'r8125' 2.5G network driver

---
+++ package/r8125/r8125.mk
@@ -0,0 +1,36 @@
+#############################################################
+#
+# r8125
+#
+#############################################################
+
+R8125_VERSION = 9.005.06
+R8125_SOURCE:=r8125-$(R8125_VERSION).tar.bz2
+R8125_SITE = https://s3.amazonaws.com/files.astlinux-project
+R8125_DEPENDENCIES = linux
+
+R8125_UNINSTALL_STAGING_OPT = --version
+
+R8125_MAKE_OPT += \
+	CC=$(TARGET_CC) \
+	LD=$(TARGET_LD) \
+	BASEDIR=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED) \
+	KERNELDIR=$(LINUX_DIR) \
+	KFLAG=2x \
+	modules
+
+define R8125_CONFIGURE_CMDS
+	@echo "No configure"
+endef
+
+define R8125_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -D $(@D)/src/r8125.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/ethernet/realtek/r8125.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
+endef
+
+define R8125_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/net/ethernet/realtek/r8125.ko
+	$(HOST_DIR)/usr/sbin/depmod -ae -F $(LINUX_DIR)/System.map -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
+endef
+
+$(eval $(call AUTOTARGETS,package,r8125))
