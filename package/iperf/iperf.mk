20f17f0bc12fb636aeec1bfbdc1786e35d280f34
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-08-29 14:08:32 -0500

    iperf, version bump to 2.1.4

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-IPERF_VERSION = 2.0.13
+IPERF_VERSION = 2.1.4
 IPERF_SOURCE = iperf-$(IPERF_VERSION).tar.gz
 IPERF_SITE = http://downloads.sourceforge.net/project/iperf2
+
+IPERF_AUTORECONF = YES
 
 IPERF_CONF_OPT = \
 	--disable-dependency-tracking \
