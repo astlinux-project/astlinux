6aaf078dc0a0bdf3853d988ff85d6f1bcdaac950
Lonnie Abelbeck <lonnie@abelbeck.com>
2021-09-15 14:51:58 -0500

    libedit, version bump to 20210910-3.1

--- package/libedit/libedit.mk
+++ package/libedit/libedit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEDIT_VERSION = 20210714-3.1
+LIBEDIT_VERSION = 20210910-3.1
 LIBEDIT_SITE = https://thrysoee.dk/editline
 LIBEDIT_INSTALL_STAGING = YES
 
