c36e5acd2633dec14267b7a4be5792ef04afc481
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-03-26 11:48:33 -0500

    asterisk, version bump to 16.25.0 and 18.11.0 pjsip 2.10, security fixes: CVE-2022-21723, CVE-2022-23608, CVE-2021-37706

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v16),y)
-ASTERISK_VERSION := 16.21.1
+ASTERISK_VERSION := 16.25.0
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v18),y)
-ASTERISK_VERSION := 18.10.0
+ASTERISK_VERSION := 18.11.0
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 13.38.3
