dedb581338a1a01d77cc6ec51a353dca1725344f
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-03-31 13:22:25 -0500

    ca-certificates, version bump to 2022-03-29 Only fixes an internal SHA256 hash value comment

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2022-03-18
+CA_CERTIFICATES_VERSION = 2022-03-29
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
