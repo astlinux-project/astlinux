9705f959899f9b2a1fa19445ff4f7d6e28b436a3
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-04-23 13:59:47 -0500

    libusb, version bump to 1.0.26

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -4,11 +4,14 @@
 #
 #############################################################
 
-LIBUSB_VERSION = 1.0.25
+LIBUSB_VERSION = 1.0.26
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = https://github.com/libusb/libusb/releases/download/v$(LIBUSB_VERSION)
 LIBUSB_DEPENDENCIES = host-pkg-config
 LIBUSB_INSTALL_STAGING = YES
+
+# patch configure.ac
+LIBUSB_AUTORECONF = YES
 
 ifeq ($(BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_UDEV),y)
 LIBUSB_DEPENDENCIES += udev
