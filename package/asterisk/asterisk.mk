9659432c83f722ba7d0e05ba0ee9babd347da89c
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-04-24 09:07:07 -0500

    asterisk, version bump to 16.25.2 and 18.11.2 Security fixes: (res_stir_shaken) CVE-2022-26498, CVE-2022-26499

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v16),y)
-ASTERISK_VERSION := 16.25.0
+ASTERISK_VERSION := 16.25.2
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v18),y)
-ASTERISK_VERSION := 18.11.0
+ASTERISK_VERSION := 18.11.2
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 13.38.3
