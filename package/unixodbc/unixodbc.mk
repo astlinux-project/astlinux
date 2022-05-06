a5511fe6ee970b97f9323ca45a94d86e764d6cf5
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-05-06 10:47:27 -0500

    unixodbc, version bump to 2.3.11

--- package/unixodbc/unixodbc.mk
+++ package/unixodbc/unixodbc.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-UNIXODBC_VERSION = 2.3.10
+UNIXODBC_VERSION = 2.3.11
 UNIXODBC_SOURCE = unixODBC-$(UNIXODBC_VERSION).tar.gz
 UNIXODBC_SITE = http://www.unixodbc.org
 UNIXODBC_DEPENDENCIES = host-bison host-flex libtool $(if $(BR2_PACKAGE_FLEX),flex)
