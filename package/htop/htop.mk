bdc87a20c64f0d8c81bfa9b76518829f94da2d95
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-06-03 14:18:18 -0500

    htop, version bump to 3.2.1 Now use the attached release .tar.xz download.
    Note: After version 3.0.5 it failed in configure finding ncurses with --disable-unicode
    The fix is to pass --disable-static to ./configure so -static is not added to the AC_CHECK_LIB test.

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -4,25 +4,31 @@
 #
 #############################################################
 
-HTOP_VERSION = 3.0.5
-HTOP_SOURCE = htop-$(HTOP_VERSION).tar.gz
-HTOP_SITE = https://github.com/htop-dev/htop/archive/$(HTOP_VERSION)
+HTOP_VERSION = 3.2.1
+HTOP_SOURCE = htop-$(HTOP_VERSION).tar.xz
+HTOP_SITE = https://github.com/htop-dev/htop/releases/download/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
-
-HTOP_AUTORECONF = YES
 
 # Prevent htop build system from searching the host paths
 HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/$(NCURSES_CONFIG_SCRIPTS)
 
 HTOP_CONF_OPT = \
+	--disable-static \
 	--disable-unicode \
-	--enable-linux-affinity
+	--enable-affinity
+
+ifeq ($(BR2_PACKAGE_LIBCAP),y)
+HTOP_DEPENDENCIES += libcap
+HTOP_CONF_OPT += --enable-capabilities
+else
+HTOP_CONF_OPT += --disable-capabilities
+endif
 
 ifeq ($(BR2_PACKAGE_LM_SENSORS),y)
 HTOP_DEPENDENCIES += lm-sensors
-HTOP_CONF_OPT += --with-sensors
+HTOP_CONF_OPT += --enable-sensors
 else
-HTOP_CONF_OPT += --without-sensors
+HTOP_CONF_OPT += --disable-sensors
 endif
 
 define HTOP_INSTALL_TARGET_CMDS
