6ff9c664abb2e9b68fc71516b9a084a5555cce69
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-06-26 13:19:02 -0500

    asterisk, version bump to 16.27.0 and 18.13.0

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v16),y)
-ASTERISK_VERSION := 16.26.1
+ASTERISK_VERSION := 16.27.0
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v18),y)
-ASTERISK_VERSION := 18.12.1
+ASTERISK_VERSION := 18.13.0
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 13.38.3
@@ -248,7 +248,7 @@ $(ASTERISK_DIR)/.configured: $(ASTERISK_DIR)/.patched | host-automake host-pkg-c
 		LIBS='$(ASTERISK_LIBS)' \
 	)
 	(cd $(ASTERISK_DIR)/menuselect; \
-		$(HOST_CONFIGURE_OPTS) SED=sed \
+		$(HOST_CONFIGURE_OPTS) \
 		./configure \
 	)
 	$(HOST_MAKE_ENV) LD_RUN_PATH="$(HOST_DIR)/usr/lib" \
