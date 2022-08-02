f685e63b69740846ad53babc9f56edceec361b44
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-08-02 08:18:21 -0500

    igb, version bump to 5.11.4

--- package/igb/igb.mk
+++ package/igb/igb.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-IGB_VERSION = 5.10.2
+IGB_VERSION = 5.11.4
 IGB_SOURCE:=igb-$(IGB_VERSION).tar.gz
 IGB_SITE = http://downloads.sourceforge.net/project/e1000/igb%20stable/$(IGB_VERSION)
 IGB_DEPENDENCIES = linux
