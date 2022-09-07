ea3ca9feb7d23c2e37ad21e1372084e48ffb3623
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-09-07 08:58:16 -0500

    stunnel, version bump to 5.65

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.63
+STUNNEL_VERSION = 5.65
 STUNNEL_SITE = https://www.stunnel.org/downloads
 STUNNEL_DEPENDENCIES = openssl
 
