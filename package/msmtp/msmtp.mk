d260f750768c2691ec719a8b25b2f218430dfd54
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-09-07 08:19:17 -0500

    msmtp, version bump to 1.8.22

--- package/msmtp/msmtp.mk
+++ package/msmtp/msmtp.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MSMTP_VERSION = 1.8.20
+MSMTP_VERSION = 1.8.22
 MSMTP_SOURCE = msmtp-$(MSMTP_VERSION).tar.xz
 MSMTP_SITE = https://marlam.de/msmtp/releases
 MSMTP_DEPENDENCIES = libretls
