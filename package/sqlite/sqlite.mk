69be9f06c7d739ea5dc32bf025a4a43753c35f28
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-10-01 09:31:45 -0500

    sqlite, version bump to 3.39.4

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3390300
+SQLITE_VERSION = 3390400
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2022
 SQLITE_INSTALL_STAGING = YES
