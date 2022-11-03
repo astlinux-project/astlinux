8b139341cc22018564cba8c5625c4dfbf3d58d63
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-11-03 08:38:28 -0500

    ethtool, version bump to 6.0

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 5.19
+ETHTOOL_VERSION = 6.0
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 
 ETHTOOL_DEPENDENCIES = host-pkg-config
