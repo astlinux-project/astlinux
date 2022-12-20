23a0ee6e9f920c3dbd076df26fb67a63010d4265
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-12-20 10:36:50 -0600

    runnix, version bump to 0.6.13

--- boot/runnix/runnix.mk
+++ boot/runnix/runnix.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RUNNIX_VERSION = 0.6.12
+RUNNIX_VERSION = 0.6.13
 RUNNIX_SOURCE = runnix-$(RUNNIX_VERSION).tar.gz
 RUNNIX_SITE = https://astlinux-project.org/mirror/runnix6
 RUNNIX_DEPENDENCIES = host-fdisk
