6c3f848858d4bde47396fb869f0f105407d67be6
Lonnie Abelbeck <lonnie@abelbeck.com>
2022-12-26 09:33:12 -0600

    tiff, version bump to 4.5.0 Security fixes: Many CVEs

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -4,13 +4,15 @@
 #
 #############################################################
 
-TIFF_VERSION = 4.4.0
+TIFF_VERSION = 4.5.0
 TIFF_SITE = https://download.osgeo.org/libtiff
 TIFF_SOURCE = tiff-$(TIFF_VERSION).tar.gz
 TIFF_INSTALL_STAGING = YES
 TIFF_CONF_OPT = \
 	--disable-cxx \
 	--without-x \
+	--disable-tests \
+	--disable-docs \
 	--disable-libdeflate
 
 TIFF_DEPENDENCIES = host-pkg-config zlib jpeg
