f871a85f3032d9d612a2625c07576954a397ad45
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-01-21 09:53:07 -0600

    asterisk, version bumps 16.30.0 and 18.16.0

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v16),y)
-ASTERISK_VERSION := 16.29.1
+ASTERISK_VERSION := 16.30.0
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v18),y)
-ASTERISK_VERSION := 18.15.1
+ASTERISK_VERSION := 18.16.0
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 13.38.3
