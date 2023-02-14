f0eb286f28bb9416375d4ecf5a3d22a35d636287
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-02-14 10:30:36 -0600

    monit, version bump to 5.33.0

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONIT_VERSION = 5.32.0
+MONIT_VERSION = 5.33.0
 MONIT_SITE = https://mmonit.com/monit/dist
 MONIT_DEPENDENCIES = host-bison host-flex
 #
