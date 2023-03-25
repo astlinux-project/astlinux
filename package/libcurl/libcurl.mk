9ea630bcf3585232c871040cf224aec141333d57
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-03-25 08:32:46 -0500

    libcurl, version bump to 8.0.1 security fixes: CVE-2023-27537, CVE-2023-27536, CVE-2023-27535, CVE-2023-27534, CVE-2023-27533
    Happy 25th anniversary

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBCURL_VERSION = 7.88.1
+LIBCURL_VERSION = 8.0.1
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.gz
 LIBCURL_SITE = https://curl.haxx.se/download
 LIBCURL_INSTALL_STAGING = YES
