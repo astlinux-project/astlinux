47a23cccd7fae8e8aeb922b9e1938e49e673b824
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-05-04 09:08:12 -0500

    whois, version bump to 5.5.17

--- package/whois/whois.mk
+++ package/whois/whois.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WHOIS_VERSION = 5.5.16
+WHOIS_VERSION = 5.5.17
 WHOIS_SITE = https://github.com/rfc1036/whois/archive/v$(WHOIS_VERSION)
 WHOIS_SOURCE = whois-$(WHOIS_VERSION).tar.gz
 
