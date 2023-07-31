8f6660ccb8f790a31649a7d3d4415df772727da9
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-07-31 09:20:19 -0500

    tinyproxy, enable for standard builds

--- package/tinyproxy/tinyproxy.mk
+++ package/tinyproxy/tinyproxy.mk
@@ -8,22 +8,16 @@ TINYPROXY_VERSION = 1.11.1
 TINYPROXY_SITE = https://github.com/tinyproxy/tinyproxy/releases/download/$(TINYPROXY_VERSION)
 TINYPROXY_SOURCE = tinyproxy-$(TINYPROXY_VERSION).tar.xz
 
-TINYPROXY_CONF_OPT = \
-        --enable-filter \
-        --enable-reverse \
-        --enable-transparent
-
-define TINYPROXY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/src/tinyproxy $(TARGET_DIR)/usr/bin/
-	-mkdir $(TARGET_DIR)/usr/share/tinyproxy
-	cp -a $(@D)/data/templates/*.html $(TARGET_DIR)/usr/share/tinyproxy/
-	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D)/data/templates install
-	$(INSTALL) -D -m 0644 $(@D)/etc/tinyproxy.conf $(TARGET_DIR)/stat/etc/
+define TINYPROXY_POST_INSTALL
+	$(INSTALL) -D -m 0644 $(TARGET_DIR)/etc/tinyproxy/tinyproxy.conf $(TARGET_DIR)/stat/etc/tinyproxy.conf
+	rm -rf $(TARGET_DIR)/etc/tinyproxy
 	$(INSTALL) -D -m 0755 package/tinyproxy/tinyproxy.init $(TARGET_DIR)/etc/init.d/tinyproxy
 	ln -sf /tmp/etc/tinyproxy.conf $(TARGET_DIR)/etc/tinyproxy.conf
 	ln -sf ../../init.d/tinyproxy $(TARGET_DIR)/etc/runlevels/default/S33tinyproxy
 	ln -sf ../../init.d/tinyproxy $(TARGET_DIR)/etc/runlevels/default/K25tinyproxy	
 endef
+
+TINYPROXY_POST_INSTALL_TARGET_HOOKS = TINYPROXY_POST_INSTALL
 
 define TINYPROXY_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/tinyproxy
