c47fb6328c562b171b1eb5183455a4a10832e16b
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-08-02 08:28:42 -0500

    openssl, version bump to 1.1.1v

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.1.1u
+OPENSSL_VERSION = 1.1.1v
 OPENSSL_SITE = https://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
