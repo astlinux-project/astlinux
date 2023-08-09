99564f95eeedffcc8450bc52969f3f94536c0878
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-08-09 14:28:38 -0500

    chrony, version bump to 4.4

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHRONY_VERSION = 4.4-pre2
+CHRONY_VERSION = 4.4
 CHRONY_SITE = https://chrony-project.org/releases
 CHRONY_DEPENDENCIES = host-pkg-config libcap
 
