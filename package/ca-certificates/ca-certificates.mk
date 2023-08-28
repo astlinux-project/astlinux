6b61887d9114bbcef6f439df1bb92d2ae82dd0bf
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-08-28 09:50:38 -0500

    ca-certificates, version bump to 2023-08-22

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2023-05-30
+CA_CERTIFICATES_VERSION = 2023-08-22
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
