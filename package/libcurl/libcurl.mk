301354091dd3151876b14a26c34e2dbaa67a3b0a
Lonnie Abelbeck <lonnie@abelbeck.com>
2023-09-19 10:08:31 -0500

    libcurl, version bump to 8.3.0 Security fix: CVE-2023-38039

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBCURL_VERSION = 8.2.1
+LIBCURL_VERSION = 8.3.0
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.gz
 LIBCURL_SITE = https://curl.haxx.se/download
 LIBCURL_INSTALL_STAGING = YES
