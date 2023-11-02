2e75be8041134393f9520920845861ec0a32ca23
abelbeck <lonnie@abelbeck.com>
2023-11-02 11:25:20 -0500

    iperf3, version bump to 3.15

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.14
+IPERF3_VERSION = 3.15
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION).tar.gz
 IPERF3_SITE = https://downloads.es.net/pub/iperf
 
