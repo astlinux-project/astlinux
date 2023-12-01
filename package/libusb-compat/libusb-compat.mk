4c6a3d24e8b3bd12ddb4bbe3d0ad889747d30021
abelbeck <lonnie@abelbeck.com>
2023-12-01 10:25:39 -0600

    libusb-compat, fix upstream nonsense Upstream libusb-compat-0.1 made autoconf changes and re-released the v0.1.8 tarballs without changing the version number. Solve by using a URL to the original v0.1.8 version. Ref: https://github.com/buildroot/buildroot/commit/d590c2c9398bce36efe042233054a2c6b75fcad0

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -6,7 +6,8 @@
 
 LIBUSB_COMPAT_VERSION = 0.1.8
 LIBUSB_COMPAT_SOURCE = libusb-compat-$(LIBUSB_COMPAT_VERSION).tar.bz2
-LIBUSB_COMPAT_SITE = https://github.com/libusb/libusb-compat-0.1/releases/download/v$(LIBUSB_COMPAT_VERSION)
+#LIBUSB_COMPAT_SITE = https://github.com/libusb/libusb-compat-0.1/releases/download/v$(LIBUSB_COMPAT_VERSION)
+LIBUSB_COMPAT_SITE = https://astlinux-project.org/files
 LIBUSB_COMPAT_DEPENDENCIES = host-pkg-config libusb
 LIBUSB_COMPAT_INSTALL_STAGING = YES
 
