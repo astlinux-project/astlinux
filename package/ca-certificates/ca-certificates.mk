6d024e4330eaa6ec5ac83d9281dea6affa78c5ec
abelbeck <lonnie@abelbeck.com>
2023-12-14 08:35:36 -0600

    ca-certificates, version bump to 2023-12-12 Authority diff: -Security Communication Root CA -Autoridad de Certificacion Firmaprofesional CIF A62634068
    +TrustAsia Global Root CA G3
    +TrustAsia Global Root CA G4
    
    +CommScope Public Trust ECC Root-01
    +CommScope Public Trust ECC Root-02
    +CommScope Public Trust RSA Root-01
    +CommScope Public Trust RSA Root-02

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2023-08-22
+CA_CERTIFICATES_VERSION = 2023-12-12
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
