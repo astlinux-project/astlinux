51e37f3602761592b83b5cb2017e46bf6bcfed83
abelbeck <lonnie@abelbeck.com>
2023-12-15 09:08:17 -0600

    asterisk, version bump to 18.20.1 and 20.5.1

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 ifeq ($(BR2_PACKAGE_ASTERISK_v18),y)
-ASTERISK_VERSION := 18.20.0
+ASTERISK_VERSION := 18.20.1
 ASTERISK_LABEL :=
 else
  ifeq ($(BR2_PACKAGE_ASTERISK_v20),y)
-ASTERISK_VERSION := 20.5.0
+ASTERISK_VERSION := 20.5.1
 ASTERISK_LABEL :=
  else
 ASTERISK_VERSION := 16.30.0
