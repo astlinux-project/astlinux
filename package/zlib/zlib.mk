ba4f6113fbceb9d04d72f755182ddad27df6f372
abelbeck <lonnie@abelbeck.com>
2024-02-05 10:08:00 -0600

    zlib, version bump to 1.3.1

--- package/zlib/zlib.mk
+++ package/zlib/zlib.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ZLIB_VERSION = 1.3
+ZLIB_VERSION = 1.3.1
 ZLIB_SOURCE = zlib-$(ZLIB_VERSION).tar.gz
 ZLIB_SITE = https://zlib.net
 ZLIB_INSTALL_STAGING = YES
