be91b283f4da3b851dcc4f0e0621fad53b73269c
abelbeck <lonnie@abelbeck.com>
2024-02-13 09:26:14 -0600

    stunnel, version bump to 5.72

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-STUNNEL_VERSION = 5.70
+STUNNEL_VERSION = 5.72
 STUNNEL_SITE = https://www.stunnel.org/downloads
 STUNNEL_DEPENDENCIES = openssl
 
