ef14f2a5a4654ea06de859a1bb34a0a6daa0f9b4
abelbeck <lonnie@abelbeck.com>
2024-04-11 09:21:53 -0500

    mosquitto, version bump to 1.6.15

--- package/mosquitto/mosquitto.mk
+++ package/mosquitto/mosquitto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOSQUITTO_VERSION = 1.6.13
+MOSQUITTO_VERSION = 1.6.15
 MOSQUITTO_SITE = https://mosquitto.org/files/source
 MOSQUITTO_INSTALL_STAGING = YES
 MOSQUITTO_DEPENDENCIES = host-pkg-config
