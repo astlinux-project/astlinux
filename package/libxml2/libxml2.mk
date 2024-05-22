1f092fb8f022f02bc5bc8af19f42ae7d52ac6461
abelbeck <lonnie@abelbeck.com>
2024-05-22 08:05:52 -0500

    libxml2, version bump to 2.11.8 Security fix: CVE-2024-34459

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBXML2_VERSION = 2.11.7
+LIBXML2_VERSION = 2.11.8
 LIBXML2_SITE = https://download.gnome.org/sources/libxml2/2.11
 LIBXML2_SOURCE = libxml2-$(LIBXML2_VERSION).tar.xz
 LIBXML2_INSTALL_STAGING = YES
