126d3e92fc78b95d00f435bd59d7f7e2e47a56e3
abelbeck <lonnie@abelbeck.com>
2024-07-11 08:14:02 -0500

    ca-certificates, version bump to 2024-07-02 Authority diff: -GLOBALTRUST 2020 +FIRMAPROFESIONAL CA ROOT-A WEB

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2024-03-11
+CA_CERTIFICATES_VERSION = 2024-07-02
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
