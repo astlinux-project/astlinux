016073619b4fe897f5f7b13fb79541467fe10837
abelbeck <lonnie@abelbeck.com>
2024-11-15 10:28:38 -0600

    iperf, version bump to 2.2.1 The --disable-web100 option has been removed

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -4,12 +4,11 @@
 #
 #############################################################
 
-IPERF_VERSION = 2.2.0
+IPERF_VERSION = 2.2.1
 IPERF_SOURCE = iperf-$(IPERF_VERSION).tar.gz
 IPERF_SITE = https://downloads.sourceforge.net/project/iperf2
 
 IPERF_CONF_OPT = \
-	--disable-dependency-tracking \
-	--disable-web100
+	--disable-dependency-tracking
 
 $(eval $(call AUTOTARGETS,package,iperf))
