1f6134412020a649674a5aeb5afa307db3155f83
abelbeck <lonnie@abelbeck.com>
2025-01-09 13:22:37 -0600

    kexec, version bump to 2.0.30

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-KEXEC_VERSION = 2.0.23
+KEXEC_VERSION = 2.0.30
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 
