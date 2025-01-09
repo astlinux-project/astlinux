95a904f6035d4def091dd92330dfada1bafe4454
abelbeck <lonnie@abelbeck.com>
2025-01-09 12:47:11 -0600

    fping, version bump to 5.3

--- package/fping/fping.mk
+++ package/fping/fping.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-FPING_VERSION = 5.2
+FPING_VERSION = 5.3
 FPING_SITE = https://fping.org/dist
 FPING_SOURCE = fping-$(FPING_VERSION).tar.gz
 
