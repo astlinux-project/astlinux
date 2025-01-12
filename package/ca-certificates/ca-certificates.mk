99ad06a3e37157d12e1f914f8ed3216d1e6d3bfd
abelbeck <lonnie@abelbeck.com>
2025-01-12 12:49:25 -0600

    ca-certificates, version bump to 2024-12-31 Authority diff: -SecureSign RootCA11 -Entrust Root Certification Authority - G4 -Security Communication RootCA3

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2024-11-26
+CA_CERTIFICATES_VERSION = 2024-12-31
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
