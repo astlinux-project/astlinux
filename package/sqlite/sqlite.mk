2f09a199ef69ac17f6e4efd55769fc0daaff177f
abelbeck <lonnie@abelbeck.com>
2025-02-18 10:42:25 -0600

    sqlite, version bump to 3.49.1 Removed libedit support as it no longer cross-compiles without hacks.
    Set configure build options to match the previous defaults.
    
    The sqlite3 shell no longer defines these options:
    SQLITE_ENABLE_EXPLAIN_COMMENTS
    SQLITE_DQS=0
    SQLITE_ENABLE_DBPAGE_VTAB
    SQLITE_ENABLE_STMTVTAB
    SQLITE_ENABLE_DBSTAT_VTAB

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,47 +4,63 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3470200
+SQLITE_VERSION = 3490100
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
-SQLITE_SITE = https://www.sqlite.org/2024
+SQLITE_SITE = https://www.sqlite.org/2025
 SQLITE_INSTALL_STAGING = YES
-
-# sqlite-0001-editline-configure-fix.patch
-SQLITE_AUTORECONF = YES
 
 SQLITE_DEPENDENCIES = zlib
 
 SQLITE_CFLAGS += -DSQLITE_ENABLE_COLUMN_METADATA
 
-SQLITE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) $(SQLITE_CFLAGS)"
-
 SQLITE_CONF_OPT = \
 	--disable-math \
+	--disable-fts3 \
+	--enable-fts4 \
 	--disable-fts5 \
+	--enable-geopoly \
+	--enable-rtree \
+	--disable-session \
+	--disable-static \
 	--disable-static-shell \
+	--disable-readline \
 	--enable-threadsafe \
 	--localstatedir=/var
 
-ifeq ($(BR2_PACKAGE_READLINE),y)
-SQLITE_DEPENDENCIES += readline
-SQLITE_CONF_OPT += --disable-editline --enable-readline
-else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
-SQLITE_DEPENDENCIES += libedit
-SQLITE_CONF_OPT += --enable-editline --disable-readline
-else
-SQLITE_CONF_OPT += --disable-editline --disable-readline
-endif
+SQLITE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) $(SQLITE_CFLAGS)"
+
+define SQLITE_CONFIGURE_CMDS
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) $(SQLITE_CONF_ENV) ./configure \
+		--prefix=/usr \
+		--host="$(GNU_TARGET_NAME)" \
+		--build="$(GNU_HOST_NAME)" \
+		--sysroot="$(STAGING_DIR)" \
+		$(SQLITE_CONF_OPT) \
+	)
+endef
+
+define SQLITE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define SQLITE_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(STAGING_DIR)" -C $(@D) install
+endef
+
+define SQLITE_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
+endef
 
 define SQLITE_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/sqlite3
-	rm -f $(TARGET_DIR)/usr/lib/libsqlite3*
+	rm -f $(TARGET_DIR)/usr/lib/libsqlite3.so*
 endef
 
 define SQLITE_UNINSTALL_STAGING_CMDS
 	rm -f $(STAGING_DIR)/usr/bin/sqlite3
-	rm -f $(STAGING_DIR)/usr/lib/libsqlite3*
+	rm -f $(STAGING_DIR)/usr/lib/libsqlite3.so*
 	rm -f $(STAGING_DIR)/usr/lib/pkgconfig/sqlite3.pc
 	rm -f $(STAGING_DIR)/usr/include/sqlite3*.h
 endef
 
-$(eval $(call AUTOTARGETS,package,sqlite))
+$(eval $(call GENTARGETS,package,sqlite))
