4715e4bbbdfe6be665872086f2c01632403055ac
abelbeck <lonnie@abelbeck.com>
2025-03-29 09:28:17 -0500

    libusb, version bump to 1.0.28

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBUSB_VERSION = 1.0.27
+LIBUSB_VERSION = 1.0.28
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = https://github.com/libusb/libusb/releases/download/v$(LIBUSB_VERSION)
 LIBUSB_DEPENDENCIES = host-pkg-config
