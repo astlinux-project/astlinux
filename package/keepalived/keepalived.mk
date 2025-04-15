c1d5b7ab4145702ae43a9f137880b1cb4478bdef
abelbeck <lonnie@abelbeck.com>
2025-04-15 10:40:13 -0500

    keepalived, version bump to 2.3.3

--- package/keepalived/keepalived.mk
+++ package/keepalived/keepalived.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEEPALIVED_VERSION = 2.3.2
+KEEPALIVED_VERSION = 2.3.3
 KEEPALIVED_SOURCE = keepalived-$(KEEPALIVED_VERSION).tar.gz
 KEEPALIVED_SITE = http://www.keepalived.org/software
 KEEPALIVED_DEPENDENCIES = host-pkg-config linux openssl
