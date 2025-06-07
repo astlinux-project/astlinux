0c43863a5df8aedc6d0cf065e92a9d309b14256d
abelbeck <lonnie@abelbeck.com>
2025-06-07 11:49:21 -0500

    libmad, version 0.15.1b Security fixes: CVE-2017-8372, CVE-2017-8373, CVE-2017-8374
    Ref: https://sources.debian.org/src/libmad/0.15.1b-10.1/debian/patches/

--- package/multimedia/sox/sox.mk
+++ package/multimedia/sox/sox.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 SOX_VERSION = 14.4.2
-SOX_SITE = http://downloads.sourceforge.net/project/sox/sox/$(SOX_VERSION)
+SOX_SITE = https://downloads.sourceforge.net/project/sox/sox/$(SOX_VERSION)
 SOX_SOURCE = sox-$(SOX_VERSION).tar.bz2
 SOX_INSTALL_STAGING = YES
 # patching Makefile.am
