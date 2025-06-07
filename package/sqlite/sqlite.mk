bf1c402302298e6b1593f99334aedb0945367567
abelbeck <lonnie@abelbeck.com>
2025-06-07 13:59:17 -0500

    sqlite, version bump to 3.50.1

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3500000
+SQLITE_VERSION = 3500100
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2025
 SQLITE_INSTALL_STAGING = YES
