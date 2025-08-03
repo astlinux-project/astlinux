550a2c6c5eb240752b7bc1f1b41e403fa079ba1e
abelbeck <lonnie@abelbeck.com>
2025-08-03 08:50:54 -0500

    ca-certificates, version bump to 2025-07-15
    Authority diff:
    No Change

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2025-05-20
+CA_CERTIFICATES_VERSION = 2025-07-15
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
