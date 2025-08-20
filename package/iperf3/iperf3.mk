fa5cc8fa94ad37cc71dd0e83997813546da4aebc
abelbeck <lonnie@abelbeck.com>
2025-08-20 10:06:15 -0500

    iperf3, version bump to 3.19.1

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.19
+IPERF3_VERSION = 3.19.1
 IPERF3_SOURCE = iperf-$(IPERF3_VERSION).tar.gz
 IPERF3_SITE = https://downloads.es.net/pub/iperf
 
