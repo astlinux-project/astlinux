8a37ef80d6b57b2aeb63e87daa8e299dd074e7a7
abelbeck <lonnie@abelbeck.com>
2025-09-21 09:43:26 -0500

    lldpd, version bump to 1.0.20 (Custom Build Only)

--- package/lldpd/lldpd.mk
+++ package/lldpd/lldpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LLDPD_VERSION = 1.0.19
+LLDPD_VERSION = 1.0.20
 LLDPD_SITE = https://github.com/lldpd/lldpd/releases/download/$(LLDPD_VERSION)
 LLDPD_DEPENDENCIES = host-pkg-config $(if $(BR2_PACKAGE_LIBCAP),libcap)
 
