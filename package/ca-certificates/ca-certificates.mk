31882ece914e32c42fff178f603d8d529a2a9148
abelbeck <lonnie@abelbeck.com>
2025-11-07 07:08:09 -0600

    ca-certificates, version bump to 2025-11-04
    Authority diff:
    +OISTE Server Root ECC G1
    +OISTE Server Root RSA G1

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2025-09-09
+CA_CERTIFICATES_VERSION = 2025-11-04
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
