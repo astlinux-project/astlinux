483d1b1a34c5eb0997d5770230e4ecfe01db2b26
abelbeck <lonnie@abelbeck.com>
2026-01-16 08:51:49 -0600

    libsodium, version bump to 1.0.21 Security fix

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBSODIUM_VERSION = 1.0.20
+LIBSODIUM_VERSION = 1.0.21
 LIBSODIUM_SOURCE = libsodium-$(LIBSODIUM_VERSION).tar.gz
 LIBSODIUM_SITE = https://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)-RELEASE
 
