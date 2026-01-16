cef51c466f4864d6b1da0e4f6f3a83dd7bd9a77d
abelbeck <lonnie@abelbeck.com>
2026-01-16 08:40:06 -0600

    libpcap, version bump to 1.10.6 Security fix: CVE-2025-11961

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBPCAP_VERSION = 1.10.5
+LIBPCAP_VERSION = 1.10.6
 LIBPCAP_SITE = https://www.tcpdump.org/release
 LIBPCAP_SOURCE = libpcap-$(LIBPCAP_VERSION).tar.gz
 LIBPCAP_INSTALL_STAGING = YES
