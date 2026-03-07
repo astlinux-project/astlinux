5bb421cf04ac805176f323d91feb892c21f3747e
abelbeck <lonnie@abelbeck.com>
2026-03-07 08:25:50 -0600

    sqlite, version bump to 3.52.0 Fixes the rare WAL-reset database corruption bug. Ref: https://www.sqlite.org/wal.html#walresetbug

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3510200
+SQLITE_VERSION = 3520000
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2026
 SQLITE_INSTALL_STAGING = YES
