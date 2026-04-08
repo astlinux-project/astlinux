4a5aa5a2deff807bc423521ca6b8bd8e4ba08ae0
abelbeck <lonnie@abelbeck.com>
2026-04-08 09:02:49 -0500

    iprange, version bump to 2.0.0

--- package/iprange/iprange.mk
+++ package/iprange/iprange.mk
@@ -4,8 +4,11 @@
 #
 ################################################################################
 
-IPRANGE_VERSION = 1.0.4
+IPRANGE_VERSION = 2.0.0
 IPRANGE_SOURCE = iprange-$(IPRANGE_VERSION).tar.gz
 IPRANGE_SITE = https://github.com/firehol/iprange/releases/download/v$(IPRANGE_VERSION)
+
+# patch configure.ac
+IPRANGE_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS,package,iprange))
