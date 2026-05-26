c8119708208ce90409444b03fb7ec0ac1ed7f6c0
abelbeck <lonnie@abelbeck.com>
2026-05-26 09:15:33 -0500

    sipgrep, version bump to 2.2.0

--- package/sipgrep/sipgrep.mk
+++ package/sipgrep/sipgrep.mk
@@ -4,21 +4,19 @@
 #
 #############################################################
 
-SIPGREP_VERSION = 2.1.0
+SIPGREP_VERSION = 2.2.0
 SIPGREP_SOURCE = sipgrep-$(SIPGREP_VERSION).tar.gz
-SIPGREP_SITE = https://astlinux-project.org/files
-#SIPGREP_SITE = https://github.com/adubovikov/sipgrep/releases
-SIPGREP_DEPENDENCIES = libpcap pcre
+SIPGREP_SITE = https://github.com/sipcapture/sipgrep/archive/$(SIPGREP_VERSION)
+SIPGREP_DEPENDENCIES = libpcap pcre2
+SIPGREP_AUTORECONF = YES
 
 SIPGREP_UNINSTALL_STAGING_OPT = --version
 
 SIPGREP_CONF_OPT += \
-	--disable-pcap-restart \
-	--with-pcap-includes=$(STAGING_DIR)/usr/include/pcap \
 	--enable-ipv6
 
 define SIPGREP_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/sipgrep $(TARGET_DIR)/usr/bin/sipgrep
+	$(INSTALL) -m 0755 -D $(@D)/src/sipgrep $(TARGET_DIR)/usr/bin/sipgrep
 endef
 
 define SIPGREP_UNINSTALL_TARGET_CMDS
