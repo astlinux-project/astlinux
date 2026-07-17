fdbf1014fd9344ae9efbe758af7922991dd5bf12
abelbeck <lonnie@abelbeck.com>
2026-07-17 07:49:41 -0500

    ca-certificates, version bump to 2026-07-16 Authority diff: -Entrust Root Certification Authority -SecureSign Root CA12

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CA_CERTIFICATES_VERSION = 2026-05-14
+CA_CERTIFICATES_VERSION = 2026-07-16
 CA_CERTIFICATES_SOURCE = cacert-$(CA_CERTIFICATES_VERSION).pem
 CA_CERTIFICATES_SITE = https://curl.haxx.se/ca
 
