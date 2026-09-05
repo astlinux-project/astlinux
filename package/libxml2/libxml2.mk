1dd55ca61a70c611bfe5f71747bc374d26702e59
abelbeck <lonnie@abelbeck.com>
2026-09-05 08:51:24 -0500

    libxml2, version bump to 2.15.4 security fixes

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBXML2_VERSION = 2.15.3
+LIBXML2_VERSION = 2.15.4
 LIBXML2_SITE = https://download.gnome.org/sources/libxml2/2.15
 LIBXML2_SOURCE = libxml2-$(LIBXML2_VERSION).tar.xz
 LIBXML2_INSTALL_STAGING = YES
